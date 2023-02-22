1fcdd995d9f83b463cb4622a3b1b1be9ba3b077c
Summer498 <subore02@gmail.com>
2023-02-22 14:44:07 +0900

    add utility: reanalyze all

--- analyzeAll.sh
+++ analyzeAll.sh
@@ -3,6 +3,8 @@ defcol=[39m
 
 for file in resources/*; do
     echo $green$file$defcol
+    #./mimicopy.sh "$file" 0 0 --roman_reanalyze > /dev/null
+    #./manalyze.sh "$file" 0 0 --melody_reanalyze > /dev/null
     ./mimicopy.sh "$file" > /dev/null
     ./manalyze.sh "$file" > /dev/null
 done
