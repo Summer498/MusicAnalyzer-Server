4a708ddfb6f12beddc918f34052e3c0cc14b5d81
Summer498 <subore02@gmail.com>
2024-02-24 16:16:49 +0900

    refactoring

--- packages/Color/index.ts
+++ packages/Color/index.ts
@@ -1,22 +1,12 @@
-import { mod, vAdd, vMul } from "../Math";
-
-const abs = Math.abs;
-const vFloor = (a: number[]) => a.map(e => Math.floor(e));
+import { mod } from "../Math";
 
 // 0 <= h < 360; 0 <= s <= 1; 0 <= b <= 1
+// h |-> [red, yellow, green, cyan, blue, magenta]
 export const hsv2rgb = (h: number, s: number, v: number) => {
-  const c = v * s;
-  const x = c * (1 - abs(mod(h / 60, 2) - 1));
-  const m = v - c;
-  const rgb =
-    h < 60? [c, x, 0]: h < 120? [x, c, 0]: h < 180? [0, c, x]: h < 240? [0, x, c]: h < 300? [x, 0, c]: [c, 0, x];
-  return vFloor(vMul(vAdd(rgb, m), 255));
+  const H = mod(h, 360) / 60;
+  const x = s * Math.abs(mod(H + 1, 2) - 1);
+  const rgb = [[s, x, 0], [x, s, 0], [0, s, x], [0, x, s], [x, 0, s], [s, 0, x]][Math.floor(H)];
+  return rgb.map(e => Math.floor((e + 1 - s) * v * 255));
 };
 
-export const rgbToString = (rgb: number[]) => {
-  let ret = "#";
-  rgb.forEach(e => {
-    ret = `${ret}${e < 16 ? `0` : ``}${e.toString(16)}`;
-  });
-  return ret;
-};
+export const rgbToString = (rgb: number[]) => '#' + rgb.map(e => ('0' + e.toString(16)).slice(-2)).join('');
