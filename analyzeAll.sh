bb0e60019b596a658bedf8a9f6e5449e3cb418b1
Summer498 <subore02@gmail.com>
2024-06-02 17:03:06 +0900

    modify updated directory structure (long ago)

--- analyzeAll.sh
+++ analyzeAll.sh
@@ -1,10 +1,10 @@
 green=[32m
 defcol=[39m
 
-for file in resources/*; do
-    echo $green$file$defcol
+for file in resources/*/*.*; do
+    echo ${green}analyze $file${defcol}
     #./ranalyze.sh "$file" 0 0 --roman_reanalyze > /dev/null
     #./manalyze.sh "$file" 0 0 --melody_reanalyze > /dev/null
-    ./ranalyze.sh "$file" > /dev/null
-    ./manalyze.sh "$file" > /dev/null
+    ./ranalyze.sh "$file" -q > /dev/null
+    ./manalyze.sh "$file" -q > /dev/null
 done
