8c565a84acc76a118cef8f568f94c9e6bacd337c
Summer498 <subore02@gmail.com>
2024-09-30 11:21:37 +0900

    refactoring: separate responsibility of model, view and controller

--- html/hierarchical-analysis-sample/view/src/song-manager.ts
+++ html/hierarchical-analysis-sample/view/src/song-manager.ts
@@ -5,7 +5,7 @@ import { getChordKeysSVG, getChordNamesSVG, getChordNotesSVG, getChordRomansSVG 
 import { DMelodySwitcher, GravitySwitcher, HierarchyLevel, MelodyBeepSwitcher, MelodyBeepVolume, MelodyColorSelector, TimeRangeSlider } from "@music-analyzer/controllers";
 import { HTML, SVG } from "@music-analyzer/html";
 import { TimeAndMelodyAnalysis } from "@music-analyzer/melody-analyze";
-import { getDMelodyControllers, getHierarchicalChordGravitySVGs, getHierarchicalIRSymbolSVGs, getHierarchicalMelodySVGs, getHierarchicalScaleGravitySVGs, getTSR_SVGs } from "@music-analyzer/melody-view";
+import { getDMelodyControllers, getHierarchicalChordGravitySVGs, getHierarchicalIRSymbolSVGs, getHierarchicalMelodyControllers, getHierarchicalScaleGravitySVGs, getTSR_SVGs } from "@music-analyzer/melody-view";
 import { getBlackBGs, getBlackKeys, getCurrentTimeLine, getOctaveBGs, getOctaveKeys, getWhiteBGs, getWhiteKeys, PianoRoll } from "@music-analyzer/svg-objects";
 import { Assertion, _throw } from "@music-analyzer/stdlib";
 import { PianoRollTimeLength } from "@music-analyzer/view-parameters";
@@ -70,7 +70,7 @@ export const appendPianoRoll = (piano_roll_place: HTMLDivElement, song_manager: 
   piano_roll.svg.appendChild(getChordRomansSVG(song_manager.romans).group);
   piano_roll.svg.appendChild(getChordKeysSVG(song_manager.romans).group);
   piano_roll.svg.appendChild(getDMelodyControllers(song_manager.d_melodies, d_melody_switcher).group);
-  getHierarchicalMelodySVGs(song_manager.hierarchical_melody, hierarchy_level, melody_beep_switcher, melody_beep_volume)
+  getHierarchicalMelodyControllers(song_manager.hierarchical_melody, hierarchy_level, melody_beep_switcher, melody_beep_volume)
     .forEach(e => piano_roll.svg.appendChild(e.group));
   getHierarchicalIRSymbolSVGs(song_manager.hierarchical_melody, hierarchy_level)
     .forEach(e => piano_roll.svg.appendChild(e.group));
--- packages/melody-view/index.ts
+++ packages/melody-view/index.ts
@@ -77,68 +77,111 @@ export const getDMelodyControllers = (detected_melodies: TimeAndMelodyAnalysis[]
   detected_melodies.map(e => new DMelodyController(e, d_melody_switcher))
 );
 
-class MelodySVG implements Updatable {
-  svg: SVGRectElement;
+class MelodyModel {
   begin: number;
   end: number;
   note: number;
   layer: number;
-  y: number;
-  w: number;
-  h: number;
+  constructor(melody: TimeAndMelodyAnalysis, layer: number) {
+    this.begin = melody.begin;
+    this.end = melody.end;
+    this.note = melody.note;
+    this.layer = layer;
+  }
+}
+
+class MelodyView {
+  svg: SVGRectElement;
   sound_reserved: boolean;
+  constructor(color: string) {
+    this.svg = SVG.rect();
+    this.svg.setAttribute("name", "melody-note");
+    this.svg.setAttribute("fill", color);
+    this.svg.setAttribute("stroke", "#444");
+    this.sound_reserved = false;
+  }
+  set x(value: number) { this.svg.setAttribute("x", `${value}`); }
+  set y(value: number) { this.svg.setAttribute("y", `${value}`); }
+  set width(value: number) { this.svg.setAttribute("width", `${value}`); }
+  set height(value: number) { this.svg.setAttribute("height", `${value}`); }
+  get visibility() { 
+    const visibility = this.svg.getAttribute("visibility");
+    if (visibility === "visible" || visibility === "hidden"){return visibility;}
+    else {throw new TypeError(`Illegal string received. Expected is "visible" or "hidden" but reserved is ${visibility}`);}
+  }
+  set visibility(value: "visible" | "hidden") { this.svg.setAttribute("visibility", value); }
+  set onclick(value: () => void) { this.svg.onclick = value; }
+}
+
+class MelodyController implements Updatable {
+  model: MelodyModel;
+  view: MelodyView;
   hierarchy_level: HierarchyLevel;
   melody_beep_switcher: MelodyBeepSwitcher;
   melody_beep_volume: MelodyBeepVolume;
 
   constructor(melody: TimeAndMelodyAnalysis, hierarchy_level: HierarchyLevel, melody_beep_switcher: MelodyBeepSwitcher, melody_beep_volume: MelodyBeepVolume, layer?: number) {
-    this.svg = SVG.rect();
-    this.svg.setAttribute("name", "melody-note");
-    this.svg.setAttribute("fill", fifthChromaToColor(melody.note, 0.75, 0.9));
-    this.svg.setAttribute("stroke", "#444");
-    this.begin = melody.begin;
-    this.end = melody.end;
-    this.note = melody.note;
-    this.layer = layer || 0;
-    this.y = (piano_roll_begin - melody.note) * black_key_prm.height;
-    this.w = (melody.end - melody.begin) * 15 / 16;  // 最初と最後がくっつくのを防ぐために少し短くする
-    this.h = black_key_prm.height;
-    this.sound_reserved = false;
+    this.model = new MelodyModel(melody, layer || 0);
+    this.view = new MelodyView(fifthChromaToColor(melody.note, 0.75, 0.9));
+    this.updateY();
+    this.updateWidth();
+    this.updateHeight();
     this.hierarchy_level = hierarchy_level;
     this.melody_beep_switcher = melody_beep_switcher;
     this.melody_beep_volume = melody_beep_volume;
+    CurrentTimeX.onUpdate.push(this.updateX.bind(this));
+    NowAt.onUpdate.push(this.updateX.bind(this));
+    NoteSize.onUpdate.push(this.updateX.bind(this));
+    NoteSize.onUpdate.push(this.updateWidth.bind(this));
+    this.onUpdate();
+  }
+  updateX() { this.view.x = CurrentTimeX.value + (this.model.begin - NowAt.value) * NoteSize.value; }
+  updateY() { this.view.y = (piano_roll_begin - this.model.note) * black_key_prm.height; }
+  updateWidth() { this.view.width = (this.model.end - this.model.begin) * 15 / 16 * NoteSize.value; }
+  updateHeight() { this.view.height = black_key_prm.height; }
+  updateVisibility() {
+    const is_visible = this.hierarchy_level.range.value === `${this.model.layer}`;
+    this.view.visibility = is_visible ? "visible" : "hidden";
   }
 
-  beepMelody = (volume: number) => {
+  beepMelody = () => {
     const now_at = NowAt.value;
-    if (now_at <= this.begin && this.begin < now_at + reservation_range) {
-      if (this.sound_reserved === false) {
-        play([440 * Math.pow(2, (this.note - 69) / 12)], this.begin - now_at, this.end - this.begin, volume);
-        this.sound_reserved = true;
-        setTimeout(() => { this.sound_reserved = false; }, reservation_range * 1000);
+
+    if (now_at <= this.model.begin && this.model.begin < now_at + reservation_range) {
+      console.log("this");
+      console.log(this);
+      console.log("this.view");
+      console.log(this.view);
+      if (this.view.sound_reserved === false) {
+        console.log("this.view.sound_reserved === false");
+        const volume = Number(this.melody_beep_volume.range.value) / 400;
+        const pitch = [440 * Math.pow(2, (this.model.note - 69) / 12)];
+        const begin_sec = this.model.begin - now_at;
+        const length_sec = this.model.end - this.model.begin;
+        console.log(`pitch: ${pitch}`);
+        play(pitch, begin_sec, length_sec, volume);
+        this.view.sound_reserved = true;
+        setTimeout(() => { this.view.sound_reserved = false; }, reservation_range * 1000);
       }
     }
   };
 
   onUpdate() {
-    const is_visible = this.hierarchy_level.range.value === String(this.layer);
-    this.svg.setAttribute("x", `${CurrentTimeX.value + (this.begin - NowAt.value) * NoteSize.value}`);
-    this.svg.setAttribute("y", `${this.y}`);
-    this.svg.setAttribute("width", `${this.w * NoteSize.value}`);
-    this.svg.setAttribute("height", `${this.h}`);
-    this.svg.setAttribute("visibility", is_visible ? "visible" : "hidden");
-    this.svg.onclick = deleteMelody;
-    if (this.melody_beep_switcher.checkbox.checked && is_visible) {
-      this.beepMelody(Number(this.melody_beep_volume.range.value) / 400);
+    this.updateVisibility();
+    this.view.onclick = deleteMelody;
+    console.log(this.melody_beep_switcher.checkbox.checked);
+    console.log(this.view.visibility);
+    if (this.melody_beep_switcher.checkbox.checked && this.view.visibility === "visible") {
+      this.beepMelody();
     }
   }
 }
 
-export const getHierarchicalMelodySVGs = (hierarchical_melodies: TimeAndMelodyAnalysis[][], hierarchy_level: HierarchyLevel, melody_beep_switcher: MelodyBeepSwitcher, melody_beep_volume: MelodyBeepVolume) =>
+export const getHierarchicalMelodyControllers = (hierarchical_melodies: TimeAndMelodyAnalysis[][], hierarchy_level: HierarchyLevel, melody_beep_switcher: MelodyBeepSwitcher, melody_beep_volume: MelodyBeepVolume) =>
   hierarchical_melodies.map((e, l) =>
-    new SvgCollection(
+    new SvgCollection2(
       `layer-${l}`,
-      e.map(e => new MelodySVG(e, hierarchy_level, melody_beep_switcher, melody_beep_volume, l))
+      e.map(e => new MelodyController(e, hierarchy_level, melody_beep_switcher, melody_beep_volume, l))
     )
   );
 
