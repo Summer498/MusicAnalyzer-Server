ac90a683da30b64fe43493e175d21962811cca85
Summer498 <subore02@gmail.com>
2025-02-05 15:00:02 +0900

    Comment out changed to unreachable code

--- packages/melody-view/src/melody/melody-view.ts
+++ packages/melody-view/src/melody/melody-view.ts
@@ -13,9 +13,11 @@ export class MelodyView {
     this.svg = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     this.svg.id = "melody-note";
     this.svg.style.fill = "#0c0";
-    // this.svg.style.fill = get_color_on_parametric_scale(this.model.melody_analysis.implication_realization);
-    // this.svg.style.fill = get_color_of_Narmour_concept(this.model.melody_analysis.implication_realization);
-    // this.svg.style.fill = this.model.note ? fifthChromaToColor(this.model.note, 0.75, 0.9) : "#000";
+    if (0) {
+      this.svg.style.fill = get_color_on_parametric_scale(this.model.melody_analysis.implication_realization);
+      this.svg.style.fill = get_color_of_Narmour_concept(this.model.melody_analysis.implication_realization);
+      this.svg.style.fill = this.model.note ? fifthChromaToColor(this.model.note, 0.75, 0.9) : "#000";
+    }
     this.svg.style.stroke = "#444";
     this.sound_reserved = false;
     this.updateY();
