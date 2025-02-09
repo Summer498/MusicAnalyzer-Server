cde849d3628c30fbb72480fa64f644eade7cf69e
Summer498 <subore02@gmail.com>
2025-02-09 10:47:46 +0900

    little bit time-optimization

--- packages/view/src/svg-collection.ts
+++ packages/view/src/svg-collection.ts
@@ -14,25 +14,34 @@ export abstract class TimeAndMVCController extends MVCController {
 
 export abstract class SvgCollection implements AccompanyToAudio {
   readonly children: TimeAndMVCController[];
-  readonly show: TimeAndMVCController[];
+  readonly children_model: TimeAndMVCModel[];
+  #show: TimeAndMVCController[];
+  get show() { return this.#show; };
   readonly svg: SVGGElement;
   constructor(children: TimeAndMVCController[]) {
     this.children = children;
-    this.show = [];
+    this.children_model = this.children.map(e => e.model);
+    this.#show = [];
     this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
     this.show.map(e => this.svg.appendChild(e.view.svg));
   }
   updateShow(begin: number, end: number) {
-    // const remain = search_items_in_range(this.show, begin, end);
-    // this.show.splice(0, remain.begin_index).forEach(e=>this.svg.removeChild(e.svg));  // 左側にはみ出したものを消す
-    // this.show.splice(remain.end_index, this.show.length - remain.end_index).forEach(e=>this.group.removeChild(e.svg));  // 右側にはみ出したものを消す
-    this.show.splice(0, this.show.length);  // 全部消す
-    this.svg.childNodes.forEach(e => this.svg.removeChild(e));  // 全部消す
-    const append = search_items_overlaps_range(this.children.map(e => e.model), begin - 5, end + 5);  // melodic gravity の矢印を隠すために ±5 のマージンを取る
+    /*
+    const remain = search_items_overlaps_range(this.show.map(e=>e.model), begin, end);
+    this.show.splice(remain.end_index, this.show.length - remain.end_index).forEach(e=>e.view.svg.parentNode?.removeChild(e.view.svg));  // 右側にはみ出したものを消す
+    this.show.splice(0, remain.begin_index).forEach(e=>e.view.svg.parentNode?.removeChild(e.view.svg));  // 左側にはみ出したものを消す
+    */
+
+    this.#show = []; //.splice(0, this.show.length);  // 全部消す
+    this.svg.textContent = "";  // 全部消す
+
+    const append = search_items_overlaps_range(this.children_model, begin - 5, end + 5);  // melodic gravity の矢印を隠すために ±5 のマージンを取る
+    const fragment = document.createDocumentFragment();
     this.children.slice(append.begin_index, append.end_index).forEach(e => {
       this.show.push(e);
-      this.svg.appendChild(e.view.svg);
+      fragment.appendChild(e.view.svg);
     });  // 必要分全部追加する
+    this.svg.appendChild(fragment);
   }
   onAudioUpdate() {
     this.updateShow(
