3ab55c05d626c4a4419edbaa68af779105e70b97
Summer498 <subore02@gmail.com>
2025-02-10 11:52:30 +0900

    insert initial value to avoid NaN

--- packages/view-parameters/src/piano-roll-ratio.ts
+++ packages/view-parameters/src/piano-roll-ratio.ts
@@ -1,5 +1,5 @@
 export class PianoRollRatio {
-  static #value: number;
+  static #value: number = 1;
   static get value() { return this.#value; }
   static set value(value: number) { this.#value = value; }
 }
--- packages/view-parameters/src/song-length.ts
+++ packages/view-parameters/src/song-length.ts
@@ -1,5 +1,5 @@
 export class SongLength {
-  static #value: number;
+  static #value: number = 0;
   static get value() { return this.#value; }
   static set value(value: number) { this.#value = value; }
 }
