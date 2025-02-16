8a2a38e996a04e851a2e7dfe738b295021133d0c
Summer498 <subore02@gmail.com>
2025-02-16 17:29:56 +0900

    separate initializer into one file

--- html/analyze/index.ts
+++ html/analyze/index.ts
@@ -1,13 +1,8 @@
-import { TimeAndRomanAnalysis } from "@music-analyzer/chord-to-roman";
-import { IMelodyModel } from "@music-analyzer/melody-analyze";
-import { calcTempo } from "@music-analyzer/beat-estimation";
-import { bracket_hight, NowAt, PianoRollBegin, PianoRollEnd } from "@music-analyzer/view-parameters";
-import { SongManager } from "@music-analyzer/piano-roll";
-import { setupUI } from "./src/song-manager";
+import { NowAt } from "@music-analyzer/view-parameters";
 import { MusicAnalyzerWindow } from "./src/MusicAnalyzerWindow";
-import { loadMusicAnalysis } from "./src/MusicAnalysisLoader";
 import { updateTitle } from "./src/UIManager";
 import { AccompanyToAudioRegistry, WindowReflectableRegistry } from "@music-analyzer/view";
+import { initializeApplication } from "./src/initialize-application";
 
 
 declare const window: MusicAnalyzerWindow;
@@ -16,31 +11,6 @@ declare const piano_roll_place: HTMLDivElement;
 declare const controllers: HTMLDivElement;
 declare const title: HTMLHeadingElement;
 type Mode = "TSR" | "PR" | "";
-
-const initializeApplication = async (tune_id: string, mode: Mode) => {
-  window.MusicAnalyzer = await loadMusicAnalysis(tune_id, mode);
-
-  const d_romans: TimeAndRomanAnalysis[] = window.MusicAnalyzer.roman.map(e => e);
-  const d_melodies: IMelodyModel[] = window.MusicAnalyzer.melody.map(e => ({
-    ...e,
-    begin: e.begin - 0.16,  // ズレ補正
-    end: e.end - 0.16,
-  }));
-  const romans = d_romans.map(e => e);
-  const melodies = d_melodies.map(e => e)
-    .filter((e, i) => i + 1 >= d_melodies.length || 60 / (d_melodies[i + 1].begin - d_melodies[i].begin) < 300 * 4);
-
-  // テンポの計算
-  const beat_info = calcTempo(melodies, romans);
-
-  // SVG -->
-  const highest_pitch = window.MusicAnalyzer.melody.reduce((p, c) => isNaN(p.note) ? c : isNaN(c.note) ? p : p.note > c.note ? p : c).note || 0;
-  const lowest_pitch = window.MusicAnalyzer.melody.reduce((p, c) => isNaN(p.note) ? c : isNaN(c.note) ? p : p.note < c.note ? p : c).note || 0;
-  PianoRollBegin.value = highest_pitch + Math.floor(window.MusicAnalyzer.hierarchical_melody.length * bracket_hight / 12) * 12 + 12;
-  PianoRollEnd.value = lowest_pitch - 3;
-  const song_manager = new SongManager(beat_info, romans, window.MusicAnalyzer.hierarchical_melody, d_melodies);
-  setupUI(song_manager, piano_roll_place, controllers);
-};
 
 const setupFPSMonitor = () => {
   const fps_element = document.createElement("p");
@@ -77,7 +47,7 @@ const main = async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const tune_id = urlParams.get("tune") || "";
   const mode: Mode = urlParams.has("pr") ? "PR" : urlParams.has("tsr") ? "TSR" : "";
-  await initializeApplication(tune_id, mode);
+  await initializeApplication(tune_id, mode, window, piano_roll_place, controllers);
   const fps_monitor = setupFPSMonitor();
   updateTitle(title, tune_id, mode);
 
---
+++ html/analyze/src/initialize-application.ts
@@ -0,0 +1,41 @@
+import { TimeAndRomanAnalysis } from "@music-analyzer/chord-to-roman";
+import { loadMusicAnalysis } from "./MusicAnalysisLoader";
+import { IMelodyModel } from "@music-analyzer/melody-analyze";
+import { calcTempo } from "@music-analyzer/beat-estimation";
+import { bracket_hight, PianoRollBegin, PianoRollEnd } from "@music-analyzer/view-parameters";
+import { SongManager } from "@music-analyzer/piano-roll";
+import { setupUI } from "./song-manager";
+import { MusicAnalyzerWindow } from "./MusicAnalyzerWindow";
+
+type Mode = "TSR" | "PR" | "";
+
+export const initializeApplication = async (
+  tune_id: string,
+  mode: Mode,
+  window: MusicAnalyzerWindow,
+  piano_roll_place: HTMLDivElement,
+  controllers: HTMLDivElement
+) => {
+  window.MusicAnalyzer = await loadMusicAnalysis(tune_id, mode);
+
+  const d_romans: TimeAndRomanAnalysis[] = window.MusicAnalyzer.roman.map(e => e);
+  const d_melodies: IMelodyModel[] = window.MusicAnalyzer.melody.map(e => ({
+    ...e,
+    begin: e.begin,  // ズレ補正
+    end: e.end,
+  }));
+  const romans = d_romans.map(e => e);
+  const melodies = d_melodies.map(e => e)
+    .filter((e, i) => i + 1 >= d_melodies.length || 60 / (d_melodies[i + 1].begin - d_melodies[i].begin) < 300 * 4);
+
+  // テンポの計算
+  const beat_info = calcTempo(melodies, romans);
+
+  // SVG -->
+  const highest_pitch = window.MusicAnalyzer.melody.reduce((p, c) => isNaN(p.note) ? c : isNaN(c.note) ? p : p.note > c.note ? p : c).note || 0;
+  const lowest_pitch = window.MusicAnalyzer.melody.reduce((p, c) => isNaN(p.note) ? c : isNaN(c.note) ? p : p.note < c.note ? p : c).note || 0;
+  PianoRollBegin.value = highest_pitch + Math.floor(window.MusicAnalyzer.hierarchical_melody.length * bracket_hight / 12) * 12 + 12;
+  PianoRollEnd.value = lowest_pitch - 3;
+  const song_manager = new SongManager(beat_info, romans, window.MusicAnalyzer.hierarchical_melody, d_melodies);
+  setupUI(song_manager, piano_roll_place, controllers);
+};
