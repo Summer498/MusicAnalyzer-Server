4d4f12e5b568c3f5fcee09289d19087dd67636b9
Summer498 <subore02@gmail.com>
2025-02-16 15:47:03 +0900

    amend: create abstract class Slide

--- packages/controllers/index.ts
+++ packages/controllers/index.ts
@@ -1,6 +1,4 @@
 export { Controller } from "./src/controller";
 export { Switcher } from "./src/switcher";
-export { HierarchyLevel } from "./src/hierarchy-level";
-export { MelodyBeepVolume } from "./src/melody-beep-volume";
 export { MelodyColorSelector } from "./src/melody-color-selector";
-export { TimeRangeSlider } from "./src/time-range-slider";
+export { HierarchyLevel, MelodyBeepVolume, Slider, TimeRangeSlider } from "./src/slider";
