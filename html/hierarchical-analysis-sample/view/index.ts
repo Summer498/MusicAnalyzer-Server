10cea01a16a6e185a83cebb63cdf398bb1e45d3c
Summer498 <subore02@gmail.com>
2025-02-16 16:53:43 +0900

    remove dependency to piano roll controller

--- html/hierarchical-analysis-sample/view/index.ts
+++ html/hierarchical-analysis-sample/view/index.ts
@@ -2,11 +2,12 @@ import { TimeAndRomanAnalysis } from "@music-analyzer/chord-to-roman";
 import { IMelodyModel } from "@music-analyzer/melody-analyze";
 import { calcTempo } from "@music-analyzer/beat-estimation";
 import { bracket_hight, NowAt, PianoRollBegin, PianoRollEnd } from "@music-analyzer/view-parameters";
-import { PianoRollController, SongManager } from "@music-analyzer/piano-roll";
+import { SongManager } from "@music-analyzer/piano-roll";
 import { setupUI } from "./src/song-manager";
 import { MusicAnalyzerWindow } from "./src/MusicAnalyzerWindow";
 import { loadMusicAnalysis } from "./src/MusicAnalysisLoader";
 import { updateTitle } from "./src/UIManager";
+import { AccompanyToAudioRegistry, WindowReflectableRegistry } from "@music-analyzer/view";
 
 
 declare const window: MusicAnalyzerWindow;
@@ -36,8 +37,7 @@ const initializeApplication = async (tune_id: string, mode: Mode) => {
   PianoRollBegin.value = highest_pitch + Math.floor(window.MusicAnalyzer.hierarchical_melody.length * bracket_hight / 12) * 12 + 12;
   PianoRollEnd.value = lowest_pitch - 3;
   const song_manager = new SongManager(beat_info, romans, window.MusicAnalyzer.hierarchical_melody, d_melodies);
-  const piano_roll = setupUI(song_manager, piano_roll_place, controllers);
-  return piano_roll;
+  setupUI(song_manager, piano_roll_place, controllers);
 };
 
 const setupFPSMonitor = () => {
@@ -49,18 +49,18 @@ const setupFPSMonitor = () => {
 };
 
 let last_audio_time = Number.MIN_SAFE_INTEGER;
-const audioUpdate = (piano_roll: PianoRollController) => {
+const audioUpdate = (accompany_to_audio_registry: AccompanyToAudioRegistry) => {
   // --> audio 関連処理
   const now_at = audio_player.currentTime;
   if (audio_player.paused && now_at === last_audio_time) { return; }
   last_audio_time = now_at;
   NowAt.value = now_at;
-  piano_roll.accompany_to_audio_registry.onAudioUpdate();
+  accompany_to_audio_registry.onAudioUpdate();
   // <-- audio 関連処理
 };
 
 let old_time = Date.now();
-const onUpdate = (piano_roll: PianoRollController, fps_element: HTMLParagraphElement) => {
+const onUpdate = (accompany_to_audio_registry: AccompanyToAudioRegistry, fps_element: HTMLParagraphElement) => {
   // fps 関連処理 -->
   const now = Date.now();
   const fps = Math.floor(1000 / (now - old_time));
@@ -68,24 +68,24 @@ const onUpdate = (piano_roll: PianoRollController, fps_element: HTMLParagraphEle
   fps_element.style.color = fps < 30 ? "red" : fps < 60 ? "yellow" : "lime";
   old_time = now;
 
-  audioUpdate(piano_roll);
+  audioUpdate(accompany_to_audio_registry);
 };
 
 const main = async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const tune_id = urlParams.get("tune") || "";
   const mode: Mode = urlParams.has("pr") ? "PR" : urlParams.has("tsr") ? "TSR" : "";
-  const piano_roll = await initializeApplication(tune_id, mode);
+  await initializeApplication(tune_id, mode);
   const fps_monitor = setupFPSMonitor();
   updateTitle(title, tune_id, mode);
 
   const update = () => {
-    onUpdate(piano_roll, fps_monitor);
+    onUpdate(AccompanyToAudioRegistry.instance, fps_monitor);
     requestAnimationFrame(update);
   };
 
-  window.onresize = e => piano_roll.window_reflectable_registry.onWindowResized();
-  piano_roll.window_reflectable_registry.onWindowResized();
+  window.onresize = e => WindowReflectableRegistry.instance.onWindowResized();
+  WindowReflectableRegistry.instance.onWindowResized();
   update();
 };
 window.onload = main;
