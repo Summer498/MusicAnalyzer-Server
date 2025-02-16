1d72ad1f0814d245d9c7e5b861bc4c6e2ef6bc01
Summer498 <subore02@gmail.com>
2025-02-16 18:25:52 +0900

    create EventLoop

--- html/analyze/index.ts
+++ html/analyze/index.ts
@@ -1,9 +1,8 @@
-import { NowAt } from "@music-analyzer/view-parameters";
 import { MusicAnalyzerWindow } from "./src/MusicAnalyzerWindow";
 import { updateTitle } from "./src/UIManager";
 import { AccompanyToAudioRegistry, WindowReflectableRegistry } from "@music-analyzer/view";
 import { initializeApplication } from "./src/initialize-application";
-
+import { EventLoop } from "./src/EventLoop";
 
 declare const window: MusicAnalyzerWindow;
 declare const audio_player: HTMLAudioElement | HTMLVideoElement;
@@ -12,52 +11,15 @@ declare const controllers: HTMLDivElement;
 declare const title: HTMLHeadingElement;
 type Mode = "TSR" | "PR" | "";
 
-const setupFPSMonitor = () => {
-  const fps_element = document.createElement("p");
-  fps_element.id = "fps";
-  fps_element.textContent = `fps:${0}`;
-  document.body.insertAdjacentElement("beforeend", fps_element);
-  return fps_element;
-};
-
-let last_audio_time = Number.MIN_SAFE_INTEGER;
-const audioUpdate = (accompany_to_audio_registry: AccompanyToAudioRegistry) => {
-  // --> audio 関連処理
-  const now_at = audio_player.currentTime;
-  if (audio_player.paused && now_at === last_audio_time) { return; }
-  last_audio_time = now_at;
-  NowAt.value = now_at;
-  accompany_to_audio_registry.onAudioUpdate();
-  // <-- audio 関連処理
-};
-
-let old_time = Date.now();
-const onUpdate = (accompany_to_audio_registry: AccompanyToAudioRegistry, fps_element: HTMLParagraphElement) => {
-  // fps 関連処理 -->
-  const now = Date.now();
-  const fps = Math.floor(1000 / (now - old_time));
-  fps_element.textContent = `fps:${(" " + fps).slice(-3)}`;
-  fps_element.style.color = fps < 30 ? "red" : fps < 60 ? "yellow" : "lime";
-  old_time = now;
-
-  audioUpdate(accompany_to_audio_registry);
-};
-
-const main = async () => {
+const main = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const tune_id = urlParams.get("tune") || "";
   const mode: Mode = urlParams.has("pr") ? "PR" : urlParams.has("tsr") ? "TSR" : "";
-  await initializeApplication(tune_id, mode, window, piano_roll_place, controllers);
-  const fps_monitor = setupFPSMonitor();
   updateTitle(title, tune_id, mode);
-
-  const update = () => {
-    onUpdate(AccompanyToAudioRegistry.instance, fps_monitor);
-    requestAnimationFrame(update);
-  };
-
+  initializeApplication(tune_id, mode, window, piano_roll_place, controllers)
+    .then(e => WindowReflectableRegistry.instance.onWindowResized());
+  new EventLoop(AccompanyToAudioRegistry.instance, audio_player).update();
   window.onresize = e => WindowReflectableRegistry.instance.onWindowResized();
   WindowReflectableRegistry.instance.onWindowResized();
-  update();
 };
-window.onload = main;
+main();
---
+++ html/analyze/src/EventLoop.ts
@@ -0,0 +1,42 @@
+import { AccompanyToAudioRegistry } from "@music-analyzer/view";
+import { NowAt } from "@music-analyzer/view-parameters";
+
+export class EventLoop {
+  readonly registry: AccompanyToAudioRegistry;
+  readonly audio_player: HTMLAudioElement | HTMLVideoElement;
+  readonly fps_element: HTMLParagraphElement;
+  private last_audio_time = Number.MIN_SAFE_INTEGER;
+  private old_time: number;
+  constructor(
+    registry: AccompanyToAudioRegistry,
+    audio_player: HTMLAudioElement | HTMLVideoElement
+  ) {
+    this.old_time = Date.now();
+    this.registry = registry;
+    this.audio_player = audio_player;
+    this.fps_element = document.createElement("p");
+    this.fps_element.id = "fps";
+    this.fps_element.textContent = `fps:${0}`;
+    document.body.insertAdjacentElement("beforeend", this.fps_element);
+  }
+  audioUpdate() {
+    const now_at = this.audio_player.currentTime;
+    if (this.audio_player.paused && now_at === this.last_audio_time) { return; }
+    this.last_audio_time = now_at;
+    NowAt.value = now_at;
+    this.registry.onAudioUpdate();
+  };
+  onUpdate() {
+    const now = Date.now();
+    const fps = Math.floor(1000 / (now - this.old_time));
+    this.fps_element.textContent = `fps:${(" " + fps).slice(-3)}`;
+    this.fps_element.style.color = fps < 30 ? "red" : fps < 60 ? "yellow" : "lime";
+    this.old_time = now;
+
+    this.audioUpdate();
+  };
+  update() {
+    this.onUpdate();
+    requestAnimationFrame(this.update.bind(this));
+  }
+}
