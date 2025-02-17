252ea931d503105c6af5f7fae226e6aa698cca60
Summer498 <subore02@gmail.com>
2025-02-18 05:59:07 +0900

    refactoring: rename file

--- ServerProgram/src/routing/index.ts
+++ ServerProgram/src/routing/index.ts
@@ -1,6 +1,6 @@
 export { getDirectoryContents } from "./get-directory-contents";
 export { handlePostRequest } from "./handle-post-request";
-export { listUpGTTMExample } from "./listup-gttm-example";
+export { listUpGTTMExample } from "./list-up-gttm-example";
 export { send301Redirect } from "./send-301-redirect";
 export { send404HTML } from "./send-404-html";
 export { send404NotFound } from "./send-404-not-found";
