b134f88d943fc0ae41730daf6e8b515033e4a99d
Summer498 <subore02@gmail.com>
2025-02-19 11:10:06 +0900

    remove unnecessary parameters

--- packages/svg-objects/src/octave-bg.ts
+++ packages/svg-objects/src/octave-bg.ts
@@ -1,4 +1,4 @@
-import { octave_height, OctaveCount, PianoRollWidth } from "@music-analyzer/view-parameters";
+import { OctaveCount } from "@music-analyzer/view-parameters";
 import { SvgAndParam } from "./svg-and-param";
 import { OctaveBlackBG } from "./black-bg";
 import { OctaveWhiteBG } from "./white-bg";
@@ -6,30 +6,20 @@ import { WindowReflectableRegistry } from "@music-analyzer/view";
 
 export class OctaveBG extends SvgAndParam {
   readonly svg: SVGGElement;
-  readonly y: number;
-  readonly oct: number;
-
-  readonly height: number;
   readonly white_BGs: OctaveWhiteBG;
   readonly black_BGs: OctaveBlackBG;
+  readonly oct: number;
   constructor(oct: number) {
     super();
-    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    this.svg.id = `octave-BG-${oct}`;
+    this.oct = oct;
     this.white_BGs = new OctaveWhiteBG(oct);
     this.black_BGs = new OctaveBlackBG(oct);
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    this.svg.id = `octave-BG-${oct}`;
     this.svg.appendChild(this.white_BGs.svg);
     this.svg.appendChild(this.black_BGs.svg);
-    this.y = octave_height * oct;
-    
-    this.height = octave_height;
-    this.oct = oct;
-  }
+    }
   onWindowResized() {
-    this.svg.style.x = String(0);
-    this.svg.style.y = String(this.y);
-    this.svg.style.width = String(PianoRollWidth.value);
-    this.svg.style.height = String(this.height);
     this.white_BGs.onWindowResized();
     this.black_BGs.onWindowResized();
   }
--- packages/svg-objects/src/octave-keys.ts
+++ packages/svg-objects/src/octave-keys.ts
@@ -1,4 +1,4 @@
-import { octave_height, OctaveCount, white_key_prm } from "@music-analyzer/view-parameters";
+import { OctaveCount } from "@music-analyzer/view-parameters";
 import { SvgAndParam } from "./svg-and-param";
 import { OctaveBlackKey } from "./black-key";
 import { OctaveWhiteKey } from "./white-key";
@@ -6,30 +6,20 @@ import { WindowReflectableRegistry } from "@music-analyzer/view";
 
 export class OctaveKey extends SvgAndParam {
   readonly svg: SVGGElement;
-  readonly y: number;
-  readonly oct: number;
-  readonly width: number;
-  readonly height: number;
   readonly white_keys: OctaveWhiteKey;
   readonly black_keys: OctaveBlackKey;
+  readonly oct: number;
   constructor(oct: number) {
     super();
-    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    this.svg.id = `octave-key-${oct}`;
+    this.oct = oct;
     this.white_keys = new OctaveWhiteKey(oct);
     this.black_keys = new OctaveBlackKey(oct);
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    this.svg.id = `octave-key-${oct}`;
     this.svg.appendChild(this.white_keys.svg);
     this.svg.appendChild(this.black_keys.svg);
-    this.y = octave_height * oct;
-    this.width = white_key_prm.width;
-    this.height = octave_height;
-    this.oct = oct;
   }
   onWindowResized() {
-    this.svg.style.x = String(0);
-    this.svg.style.y = String(this.y);
-    this.svg.style.width = String(this.width);
-    this.svg.style.height = String(this.height);
     this.white_keys.onWindowResized();
     this.black_keys.onWindowResized();
   }
