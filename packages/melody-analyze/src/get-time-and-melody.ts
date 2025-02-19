e102a4dc98c0f55623edcfc987e71702d2204e03
Summer498 <subore02@gmail.com>
2025-02-19 13:54:49 +0900

    readable array handling

--- packages/melody-analyze/src/get-time-and-melody.ts
+++ packages/melody-analyze/src/get-time-and-melody.ts
@@ -6,18 +6,14 @@ const freqToMidi = (freq: number) => (Math.log2(freq) - Math.log2(440)) * 12 + 6
 export const getTimeAndMelody = (melody_data: number[], sampling_rate: number) => {
   const melody = melody_data.map(e => e || Math.round(freqToMidi(e)));
   const comp_melody = compress(melody);
-  const non_null_melody = (() => {
-    const res: TimeAndMelody[] = [];
-    comp_melody.forEach(e => e.item || res.push({
+  const non_null_melody = comp_melody.map(e => ({
+    begin: e.begin / sampling_rate,
+    end: e.end / sampling_rate,
+    head: {
       begin: e.begin / sampling_rate,
       end: e.end / sampling_rate,
-      head: {
-        begin: e.begin / sampling_rate,
-        end: e.end / sampling_rate,
-      },
-      note: e.item
-    })); // value が null の場合は time ごと除く
-    return res;
-  })();
+    },
+    note: e.item
+  } as TimeAndMelody)).filter(e => e.note);
   return non_null_melody;
 };
