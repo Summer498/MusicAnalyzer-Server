8afa5bac10060829f88f6c4cb9d3b7187a922f7f
Summer498 <subore02@gmail.com>
2025-02-21 21:59:51 +0900

    organize code

--- packages/melody-analyze/src/analyze-melody.ts
+++ packages/melody-analyze/src/analyze-melody.ts
@@ -14,23 +14,26 @@ export const analyzeMelody = (
   melodies: TimeAndMelody[],
   romans: TimeAndRomanAnalysis[],
 ): IMelodyModel[] => {
-//  const prev_prev = [undefined, undefined, ...melodies];
+  //  const prev_prev = [undefined, undefined, ...melodies];
   const prev = [undefined, ...melodies];
   const curr = [...melodies];
   const next = [...melodies.slice(1), undefined];
   return curr.map((e, i) => {
     const roman = romans.find(roman => roman.begin <= e.end && e.begin < roman.end); // TODO: 治す. 現状はとりあえずコードとメロディを大きめに重ならせてみているだけ
-    return {
-      begin: e.begin,
-      end: e.end,
-      note: e.note,
-      head: e.head,
+    const time_and_melody = {
+      ...e,
       roman_name: roman?.roman || "",
-      melody_analysis: {
-        scale_gravity: registerGravity(roman && _Scale.get(roman.scale), prev[i]?.note, curr[i]?.note),
-        chord_gravity: registerGravity(roman && _Chord.get(roman.chord), prev[i]?.note, curr[i]?.note),
-        implication_realization: getArchetype(prev[i]?.note, curr[i]?.note, next[i]?.note)
-      } as MelodyAnalysis
+    } as TimeAndMelody;
+
+    const melody_analysis = {
+      scale_gravity: registerGravity(roman && _Scale.get(roman.scale), prev[i]?.note, curr[i]?.note),
+      chord_gravity: registerGravity(roman && _Chord.get(roman.chord), prev[i]?.note, curr[i]?.note),
+      implication_realization: getArchetype(prev[i]?.note, curr[i]?.note, next[i]?.note)
+    } as MelodyAnalysis;
+
+    return {
+      ...time_and_melody,
+      melody_analysis: melody_analysis,
     } as IMelodyModel;
   });
 };
