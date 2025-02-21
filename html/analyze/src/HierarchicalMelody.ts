32f7c85267a7d700bd40f953a3650179110c64fe
Summer498 <subore02@gmail.com>
2025-02-21 23:52:31 +0900

    purge processes into unctions

--- html/analyze/src/HierarchicalMelody.ts
+++ html/analyze/src/HierarchicalMelody.ts
@@ -1,7 +1,42 @@
-import { analyzeMelody } from "@music-analyzer/melody-analyze";
+import { analyzeMelody, IMelodyModel, TimeAndMelody } from "@music-analyzer/melody-analyze";
 import { MusicXML, ReductionElement, TimeSpan } from "@music-analyzer/gttm";
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
 import { getTimeAndMelodyFromTS } from "./TimeSpanMapping";
+
+const getTimeAndMelody = (
+  reduction_element: ReductionElement,
+  matrix: TimeSpan[][],
+  musicxml: MusicXML,
+) => {
+  const melody_notes = getTimeAndMelodyFromTS(reduction_element, matrix, musicxml);
+  const ret = {
+    ...melody_notes,
+    head: {
+      begin: matrix[reduction_element.measure][reduction_element.note].leftend,
+      end: matrix[reduction_element.measure][reduction_element.note].rightend,
+    },
+  } as TimeAndMelody;
+  return ret;
+};
+
+const scaleAndTranslate = (e: TimeAndMelody, w: number, b: number) => {
+  return {
+    begin: e.begin * w + b,
+    end: e.end * w + b,
+    head: {
+      begin: e.head.begin * w + b,
+      end: e.head.end * w + b,
+    },
+    note: e.note,
+  } as TimeAndMelody;
+};
+
+const appendIR = (e: IMelodyModel) => {
+  return {
+    ...e,
+    IR: e.melody_analysis.implication_realization.symbol,
+  } as IMelodyModel & { IR: string };
+};
 
 export const getHierarchicalMelody = (
   measure: number,
@@ -10,25 +45,18 @@ export const getHierarchicalMelody = (
   musicxml: MusicXML,
   roman: TimeAndRomanAnalysis[]
 ) => {
-  // 全階層分の IR 分析
-  const hierarchical_time_and_melodies = [...Array(reduction.getDepthCount())].map((_, i) => reduction.getArrayOfLayer(i).map(element => {
-    return {
-      ...getTimeAndMelodyFromTS(element, matrix, musicxml),
-      head: {
-        begin: matrix[element.measure][element.note].leftend,
-        end: matrix[element.measure][element.note].rightend
-      }
-    };
-  }));
-
-  hierarchical_time_and_melodies.forEach(layer => layer.forEach(ts_element => {
-    const w = measure / 8;  // NOTE: 1 measure = 3.5
-    const b = 0;
-    ts_element.begin = ts_element.begin * w + b;
-    ts_element.end = ts_element.end * w + b;
-    ts_element.head.begin = ts_element.head.begin * w + b;
-    ts_element.head.end = ts_element.head.end * w + b;
-    ts_element.note;
-  }));
-  return hierarchical_time_and_melodies.map(time_and_melody => analyzeMelody(time_and_melody, roman).map(e => ({ IR: e.melody_analysis.implication_realization.symbol, ...e })));
+  return [...Array(reduction.getDepthCount())]
+    .map((_, layer) => {
+      return analyzeMelody(
+        reduction.getArrayOfLayer(layer)
+          .map(element => {
+            const w = measure / 8;  // NOTE: 1 measure = 3.5
+            const b = 0;
+            const melody = getTimeAndMelody(element, matrix, musicxml);
+            const scaled_melody = scaleAndTranslate(melody, w, b);
+            return scaled_melody;
+          }),
+        roman)
+        .map(e => appendIR(e));
+    });
 };
