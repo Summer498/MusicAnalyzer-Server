495464a8db21689d1b98991f4802501965421b37
Summer498 <subore02@gmail.com>
2025-02-22 07:45:37 +0900

    purge code chunk into other class

--- packages/irm/src/MelodicArchetype.ts
+++ packages/irm/src/MelodicArchetype.ts
@@ -42,6 +42,59 @@ const remove_minus = (src: string) => {
   return src;
 };
 
+class Archetype3 {
+  readonly _symbol: _ArchetypeSymbol;
+  readonly symbol: ArchetypeSymbol;
+  readonly retrospective: boolean;
+  readonly registral_return_form: RegistralReturnForm;
+  readonly notes: NoteLiteral[];
+  readonly intervals: IntervalName[];
+  readonly melody_motion: MelodyMotion;
+  readonly color: string;
+  constructor(prev: NoteLiteral, curr: NoteLiteral, next: NoteLiteral) {
+    this.notes = [prev, curr, next].map(e => e || "");
+    this.intervals = [
+      _Interval.distance(prev, curr),
+      _Interval.distance(curr, next),
+    ];
+
+    this.color = get_color_on_intervallic_angle(this.notes);
+    const initial = _Interval.get(this.intervals[0]);
+    const follow = _Interval.get(this.intervals[1]);
+    this.melody_motion = new MelodyMotion(initial, follow);
+    const i_dir = this.melody_motion.intervallic.direction.name;
+    const i_mgn = this.melody_motion.intervallic.magnitude.name;
+    const v_dir = this.melody_motion.registral.direction.name;
+    const v_mgn = this.melody_motion.registral.magnitude.name;
+    this.registral_return_form = new RegistralReturnForm(this.notes);
+
+    const initial_magnitude = Math.abs(initial.num) < 5 ? "aa" : "ab";
+
+    // Reverse
+    if ((v_dir === "mL" || i_dir === "mL") && i_mgn === "AB") {
+      this.retrospective = initial_magnitude === "aa";
+      if (i_dir !== "mL") { this._symbol = "VR"; }
+      else if (v_dir !== "mL") { this._symbol = "IR"; }
+      else { this._symbol = "R"; }
+    }
+    // Process / Duplicate
+    else {
+      this.retrospective = initial_magnitude === "ab";
+      if (i_mgn !== "AA") { this._symbol = "VP"; }
+      else if (v_mgn !== "AA") {
+        if (i_dir === "mN") { this._symbol = "ID"; }
+        else { this._symbol = "IP"; }
+      }
+      else if (v_dir === "mN") { this._symbol = "D"; }
+      else { this._symbol = "P"; }
+    }
+    if (this.retrospective) {
+      this._symbol = retrospectiveSymbol(this._symbol);
+    }
+    this.symbol = this._symbol;
+  }
+}
+
 export class Archetype {
   readonly _symbol: _ArchetypeSymbol;
   readonly symbol: ArchetypeSymbol;
@@ -55,47 +108,14 @@ export class Archetype {
   constructor(prev?: NoteLiteral, curr?: NoteLiteral, next?: NoteLiteral) {
     this.notes = [prev, curr, next].map(e => e || "");
     if (prev && curr && next) {
-      this.intervals = [
-        _Interval.distance(prev, curr),
-        _Interval.distance(curr, next),
-      ];
-
-      this.color = get_color_on_intervallic_angle(this.notes);
-      const initial = _Interval.get(this.intervals[0]);
-      const follow = _Interval.get(this.intervals[1]);
-      this.melody_motion = new MelodyMotion(initial, follow);
-      const i_dir = this.melody_motion.intervallic.direction.name;
-      const i_mgn = this.melody_motion.intervallic.magnitude.name;
-      const v_dir = this.melody_motion.registral.direction.name;
-      const v_mgn = this.melody_motion.registral.magnitude.name;
-      this.registral_return_form = new RegistralReturnForm(this.notes);
-
-      const initial_magnitude = Math.abs(initial.num) < 5 ? "aa" : "ab";
-
-      // Reverse
-      if (i_mgn === "AB" && (i_dir === "mL" || v_dir === "mL")) {
-        this.retrospective = initial_magnitude === "aa";
-        if (i_dir === "mR") { this._symbol = "VR"; }
-        else if (v_dir === "mR") { this._symbol = "IR"; }
-        else { this._symbol = "R"; }
-      }
-      // Duplicate
-      else if (i_dir === "mN" && v_dir !== "mR") {
-        this.retrospective = initial_magnitude === "ab";
-        if (v_dir !== "mN") { this._symbol = "ID"; }
-        else { this._symbol = "D"; }
-      }
-      // Process
-      else {
-        this.retrospective = initial_magnitude === "ab";
-        if (i_mgn === "AB") { this._symbol = "VP"; }
-        else if (v_mgn === "AB") { this._symbol = "IP"; }
-        else { this._symbol = "P"; }
-      }
-      if (this.retrospective) {
-        this._symbol = retrospectiveSymbol(this._symbol);
-      }
-      this.symbol = this._symbol;
+      const e = new Archetype3(prev, curr, next);
+      this._symbol = e._symbol;
+      this.symbol = e.symbol;
+      this.retrospective = e.retrospective;
+      this.registral_return_form = e.registral_return_form;
+      this.intervals = e.intervals;
+      this.melody_motion = e.melody_motion;
+      this.color = e.color;
     }
     else {
       this.retrospective = false;
