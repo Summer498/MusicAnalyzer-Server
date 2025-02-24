4d6405b4a69b0781efc2bb94d732a12d5979a53b
Summer498 <subore02@gmail.com>
2025-02-25 08:11:15 +0900

    purge children of reduction view into each class

--- packages/melody-view/src/tsr-tree/tsr-tree-view.ts
+++ packages/melody-view/src/tsr-tree/tsr-tree-view.ts
@@ -1,104 +1,37 @@
 import { Archetype, get_color_of_Narmour_concept, get_color_on_digital_intervallic_scale, get_color_on_digital_parametric_scale } from "@music-analyzer/irm";
 import { BlackKeyPrm, bracket_hight, NoteSize } from "@music-analyzer/view-parameters";
-import { MVCView } from "@music-analyzer/view";
+import { MVCModel, MVCView } from "@music-analyzer/view";
 import { ReductionModel } from "./tsr-tree-model";
 import { get_color_of_implication_realization } from "@music-analyzer/irm/src/colors.ts";
 
-export class ReductionView extends MVCView {
-  readonly svg: SVGGElement;
-  readonly bracket: SVGPathElement;
-  readonly circle: SVGCircleElement;
-  readonly ir_symbol: SVGTextElement;
+class ReductionViewModel extends MVCModel {
   #x: number;
   #w: number;
   #cx: number;
   #cw: number;
+  #strong: boolean;
   readonly y: number;
   readonly h: number;
-  #strong: boolean;
+  get x() { return this.#x; }
+  get w() { return this.#w; }
+  get cx() { return this.#cx; }
+  get cw() { return this.#cw; }
+  get strong() { return this.#strong; }
+  set strong(value: boolean) { this.#strong = value; }
   constructor(
-    protected readonly model: TSRModel,
-    readonly archetype: Archetype,
+    readonly model: ReductionModel,
   ) {
     super();
-    this.bracket = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    this.bracket.id = "group";
-    this.bracket.style.stroke = "rgb(0, 0, 64)";
-    this.bracket.style.strokeWidth = String(3);
-    this.bracket.style.fill = "rgb(242, 242, 242)";
-    this.circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    this.circle.id = "head";
-    this.circle.style.stroke = "rgb(192, 0, 0)";
-    this.circle.style.fill = "rgb(192, 0, 0)";
-
-    this.ir_symbol = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    this.ir_symbol.textContent = archetype.symbol;
-    this.ir_symbol.id = "I-R Symbol";
-    this.ir_symbol.style.fontFamily = "Times New Roman";
-    this.ir_symbol.style.fontSize = `${bracket_hight}em`;
-    this.ir_symbol.style.textAnchor = "middle";
-
     this.#x = this.getViewX(this.model.begin);
     this.#w = this.getViewW(this.model.duration);
     this.#cw = this.getViewW(this.model.head.duration);
     this.#cx = this.getViewX(this.model.head.begin) + this.#cw / 2;
     this.y = (2 + this.model.layer) * BlackKeyPrm.height * bracket_hight;
     this.h = BlackKeyPrm.height * bracket_hight;
-
-    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    this.svg.id = "time-span-node";
-    this.svg.appendChild(this.bracket);
-    //    this.svg.appendChild(this.circle);
-    this.svg.appendChild(this.ir_symbol);
     this.#strong = false;
-    this.updateBracket(this.#x, this.y, this.#w, this.h);
-    this.updateCircle(this.#cx, this.y - this.h);
-    this.updateIRSymbol(this.#cx, this.y, this.#w, this.h);
   }
   getViewX(x: number) { return x * NoteSize.value; }
   getViewW(w: number) { return w * NoteSize.value; }
-  updateBracket(x: number, y: number, w: number, h: number) {
-    const begin = { x: x + w * 0 / 10 + h * 0 / 2, y: y - h * 0 / 10 };
-    const ctrl11 = { x: x + w * 0 / 10 + h * 0 / 2, y: y - h * 6 / 10 };
-    const ctrl12 = { x: x + w * 0 / 10 + Math.min(w * 0.1, h * 1 / 2), y: y - h * 10 / 10 };
-    const corner1 = { x: x + w * 0 / 10 + Math.min(w * 0.2, h * 2 / 2), y: y - h * 10 / 10 };
-    const corner2 = { x: x + w * 10 / 10 - Math.min(w * 0.2, h * 2 / 2), y: y - h * 10 / 10 };
-    const ctrl21 = { x: x + w * 10 / 10 - Math.min(w * 0.1, h * 1 / 2), y: y - h * 10 / 10 };
-    const ctrl22 = { x: x + w * 10 / 10 - h * 0 / 2, y: y - h * 6 / 10 };
-    const end = { x: x + w * 10 / 10 - h * 0 / 2, y: y - h * 0 / 10 };
-    this.bracket.setAttribute("d",
-      `M${begin.x} ${begin.y}`
-      + `C${ctrl11.x} ${ctrl11.y}`
-      + ` ${ctrl12.x} ${ctrl12.y}`
-      + ` ${corner1.x} ${corner1.y}`
-      + `L${corner2.x} ${corner2.y}`
-      + `C${ctrl21.x} ${ctrl21.y}`
-      + ` ${ctrl22.x} ${ctrl22.y}`
-      + ` ${end.x} ${end.y}`
-    );
-  }
-  updateCircle(cx: number, cy: number) {
-    this.circle.style.cx = String(cx);
-    this.circle.style.cy = String(cy);
-  }
-  updateIRSymbol(cx: number, y: number, w: number, h: number) {
-    this.ir_symbol.setAttribute("x", String(cx));
-    this.ir_symbol.setAttribute("y", String(y));
-    this.ir_symbol.style.fontSize = `${Math.min(w / h, bracket_hight)}em`;
-    this.ir_symbol.style.fill = get_color_of_Narmour_concept(this.archetype) || "rgb(0, 0, 0)";
-    if (false) {
-      this.ir_symbol.style.fill = get_color_of_implication_realization(this.archetype) || "rgb(0, 0, 0)";
-      this.ir_symbol.style.fill = get_color_on_digital_parametric_scale(this.archetype) || "rgb(0, 0, 0)";
-      this.ir_symbol.style.fill = get_color_on_digital_intervallic_scale(this.archetype) || "rgb(0, 0, 0)";
-      this.ir_symbol.style.fill = this.archetype.color || "rgb(0, 0, 0)";
-    }
-  }
-  get strong() { return this.#strong; }
-  set strong(value: boolean) {
-    this.#strong = value;
-    this.bracket.style.strokeWidth = this.#strong ? "3" : "1";
-    this.circle.style.r = String(this.#strong ? 5 : 3);
-  }
   updateX() {
     this.#x = this.getViewX(this.model.begin);
     this.#cx = this.getViewX(this.model.head.begin) + this.#cw / 2;
@@ -110,8 +43,141 @@ export class ReductionView extends MVCView {
   onWindowResized() {
     this.updateX();
     this.updateWidth();
-    this.updateBracket(this.#x, this.y, this.#w, this.h);
-    this.updateCircle(this.#cx, this.y - this.h);
-    this.updateIRSymbol(this.#cx, this.y, this.#w, this.h);
+  }
+}
+
+export class ReductionBracket extends MVCView {
+  readonly svg: SVGPathElement;
+  constructor(
+    readonly model: ReductionViewModel,
+  ) {
+    super();
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    this.svg.id = "group";
+    this.svg.style.stroke = "rgb(0, 0, 64)";
+    this.svg.style.strokeWidth = String(3);
+    this.svg.style.fill = "rgb(242, 242, 242)";
+  }
+  updateStrong() {
+    this.svg.style.strokeWidth = this.model.strong ? "3" : "1";
+  }
+  update(x: number, y: number, w: number, h: number) {
+    const begin = { x: x + w * 0 / 10 + h * 0 / 2, y: y - h * 0 / 10 };
+    const ctrl11 = { x: x + w * 0 / 10 + h * 0 / 2, y: y - h * 6 / 10 };
+    const ctrl12 = { x: x + w * 0 / 10 + Math.min(w * 0.1, h * 1 / 2), y: y - h * 10 / 10 };
+    const corner1 = { x: x + w * 0 / 10 + Math.min(w * 0.2, h * 2 / 2), y: y - h * 10 / 10 };
+    const corner2 = { x: x + w * 10 / 10 - Math.min(w * 0.2, h * 2 / 2), y: y - h * 10 / 10 };
+    const ctrl21 = { x: x + w * 10 / 10 - Math.min(w * 0.1, h * 1 / 2), y: y - h * 10 / 10 };
+    const ctrl22 = { x: x + w * 10 / 10 - h * 0 / 2, y: y - h * 6 / 10 };
+    const end = { x: x + w * 10 / 10 - h * 0 / 2, y: y - h * 0 / 10 };
+    this.svg.setAttribute("d",
+      `M${begin.x} ${begin.y}`
+      + `C${ctrl11.x} ${ctrl11.y}`
+      + ` ${ctrl12.x} ${ctrl12.y}`
+      + ` ${corner1.x} ${corner1.y}`
+      + `L${corner2.x} ${corner2.y}`
+      + `C${ctrl21.x} ${ctrl21.y}`
+      + ` ${ctrl22.x} ${ctrl22.y}`
+      + ` ${end.x} ${end.y}`
+    );
+  }
+  onWindowResized() {
+    this.update(this.model.x, this.model.y, this.model.w, this.model.h);
+  }
+}
+
+export class ReductionDot extends MVCView {
+  readonly svg: SVGCircleElement;
+  constructor(
+    readonly model: ReductionViewModel,
+  ) {
+    super();
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    this.svg.id = "head";
+    this.svg.style.stroke = "rgb(192, 0, 0)";
+    this.svg.style.fill = "rgb(192, 0, 0)";
+  }
+  updateStrong() {
+    this.svg.style.r = String(this.model.strong ? 5 : 3);
+  }
+  update(cx: number, cy: number) {
+    this.svg.style.cx = String(cx);
+    this.svg.style.cy = String(cy);
+  }
+  onWindowResized() {
+    this.update(this.model.cx, this.model.y - this.model.h);
+  }
+}
+
+export class IRMSymbolOnReduction extends MVCView {
+  readonly svg: SVGTextElement;
+  constructor(
+    readonly model: ReductionViewModel,
+    readonly archetype: Archetype,
+  ) {
+    super();
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    this.svg.textContent = archetype.symbol;
+    this.svg.id = "I-R Symbol";
+    this.svg.style.fontFamily = "Times New Roman";
+    this.svg.style.fontSize = `${bracket_hight}em`;
+    this.svg.style.textAnchor = "middle";
+  }
+  updateColor() {
+    this.svg.style.fill = get_color_of_Narmour_concept(this.archetype) || "rgb(0, 0, 0)";
+    if (false) {
+      this.svg.style.fill = get_color_of_implication_realization(this.archetype) || "rgb(0, 0, 0)";
+      this.svg.style.fill = get_color_on_digital_parametric_scale(this.archetype) || "rgb(0, 0, 0)";
+      this.svg.style.fill = get_color_on_digital_intervallic_scale(this.archetype) || "rgb(0, 0, 0)";
+      this.svg.style.fill = this.archetype.color || "rgb(0, 0, 0)";
+    }
+  }
+  update(cx: number, y: number, w: number, h: number) {
+    this.svg.setAttribute("x", String(cx));
+    this.svg.setAttribute("y", String(y));
+    this.svg.style.fontSize = `${Math.min(w / h, bracket_hight)}em`;
+    this.updateColor();
+  }
+  onWindowResized() {
+    this.update(this.model.cx, this.model.y, this.model.w, this.model.h);
+  }
+}
+
+export class ReductionView extends MVCView {
+  readonly svg: SVGGElement;
+  readonly model: ReductionViewModel;
+  readonly bracket: ReductionBracket;
+  readonly dot: ReductionDot;
+  readonly ir_symbol: IRMSymbolOnReduction;
+  constructor(
+    model: ReductionModel,
+    readonly archetype: Archetype,
+  ) {
+    super();
+    this.model = new ReductionViewModel(model);
+    this.bracket = new ReductionBracket(this.model);
+    this.dot = new ReductionDot(this.model);
+    this.ir_symbol = new IRMSymbolOnReduction(this.model, archetype);
+
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    this.svg.id = "time-span-node";
+    this.svg.appendChild(this.bracket.svg);
+    if (false) { this.svg.appendChild(this.dot.svg); }
+    this.svg.appendChild(this.ir_symbol.svg);
+  }
+  get strong() { return this.model.strong; }
+  set strong(value: boolean) {
+    this.model.strong = value;
+    this.bracket.updateStrong();
+    this.dot.updateStrong();
+  }
+  updateColor() {
+    this.ir_symbol.updateColor();
+  }
+  onWindowResized() {
+    this.model.onWindowResized();
+    this.bracket.onWindowResized();
+    this.dot.onWindowResized();
+    this.ir_symbol.onWindowResized();
   }
 }
