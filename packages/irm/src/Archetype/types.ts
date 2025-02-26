84d91c92197230fd9dad32f3a20cfe60a03a64c2
Summer498 <subore02@gmail.com>
2025-02-26 09:02:10 +0900

    create subdivided I-R symbol converter

--- packages/irm/src/Archetype/types.ts
+++ packages/irm/src/Archetype/types.ts
@@ -5,12 +5,35 @@ export type TrigramProspectiveSymbol =
   | "R" | "IR" | "VR"
   | "D" | "ID"
 
+export type TrigramProspectiveDirectionalSymbol =
+  | "uP" | "uIP" | "uVP"
+  | "uR" | "uIR" | "uVR"
+  | "uID"
+  | "lIP" | "lR" | "lD"
+  | "dP" | "dIP" | "dVP"
+  | "dR" | "dIR" | "dVR"
+  | "dID"
+
 export type RetrospectiveSymbol =
   | "(P)" | "(IP)" | "(VP)"
   | "(R)" | "(IR)" | "(VR)"
   | "(D)" | "(ID)"
 
+export type RetrospectiveDirectionalSymbol =
+| "(uP)" | "(uIP)" | "(uVP)"
+| "(uR)" | "(uIR)" | "(uVR)"
+| "(uID)"
+| "(lIP)" | "(lR)" | "(lD)"
+| "(dP)" | "(dIP)" | "(dVP)"
+| "(dR)" | "(dIR)" | "(dVR)"
+| "(dID)"
+
 export type _ProspectiveSymbol = TrigramProspectiveSymbol | "M" | "dyad"
-export type _ArchetypeSymbol = "" | _ProspectiveSymbol | RetrospectiveSymbol
 export type ProspectiveSymbol = TrigramProspectiveSymbol | "M" | IntervalName
+export type _ProspectiveDirectionalSymbol = TrigramProspectiveDirectionalSymbol | "M" | "uDyad" | "dDyad"
+export type ProspectiveDirectionalSymbol = TrigramProspectiveDirectionalSymbol | "M" | IntervalName
+
+export type _ArchetypeSymbol = "" | _ProspectiveSymbol | RetrospectiveSymbol
 export type ArchetypeSymbol = "" | ProspectiveSymbol | RetrospectiveSymbol
+export type _ArchetypeDirectionalSymbol = "" | _ProspectiveDirectionalSymbol | RetrospectiveDirectionalSymbol
+export type ArchetypeDirectionalSymbol = "" | ProspectiveDirectionalSymbol | RetrospectiveDirectionalSymbol
