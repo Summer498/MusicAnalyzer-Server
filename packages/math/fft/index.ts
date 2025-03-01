676812b3e5b04556dca9f25eafe12adc5e688559
Summer498 <subore02@gmail.com>
2025-03-02 07:57:54 +0900

    refactoring for extension

--- packages/math/fft/index.ts
+++ packages/math/fft/index.ts
@@ -22,14 +22,48 @@ export class Complex<T extends number> {
   }
 };
 
-export class fftUtil {
-  private static readonly cache: Complex<number>[][] = [];
-  static exponent(k: number, N: number) {
-    const x = -2 * Math.PI * (k / N);
-    this.cache[N] ||= [];
-    this.cache[N][k] ||= new Complex(Math.cos(x), Math.sin(x));
-    return this.cache[N][k];
+export class RootOfUnity {
+  private readonly exponent_cache: Complex<number>[][];
+  private readonly modulo_cache: number[][];
+  constructor() {
+    this.exponent_cache = [];
+    this.modulo_cache = [];
   }
+  exponent(k: number, N: number) {
+    const x = -2 * Math.PI * (k / N);
+    this.exponent_cache[N] ||= [];
+    this.exponent_cache[N][k] ||= new Complex(Math.cos(x), Math.sin(x));
+    return this.exponent_cache[N][k];
+  }
+  modulo(k: number, N: number, modulo: number) {
+    const root = modulo - 1;
+    this.modulo_cache[N] ||= [];
+    this.modulo_cache[N][k] ||= Math.pow(root, k * N) % modulo;
+    return this.exponent_cache[N][k];
+
+  }
+};
+
+
+const fft_core = <T extends number>(
+  seq: Complex<number>[],
+  root_of_unity: RootOfUnity,
+): Complex<number>[] => {
+  const N = seq.length;
+  const res: Complex<number>[] = [];
+
+  if (N == 1) { return seq; }
+
+  const X_evens = fft_core(seq.filter((_, i) => i % 2 === 0), root_of_unity);
+  const X_odds = fft_core(seq.filter((_, i) => i % 2 == 1), root_of_unity);
+
+  for (let k = 0; k < N / 2; k++) {
+    const evens = X_evens[k];
+    const rotated_odds = X_odds[k].mlt(root_of_unity.exponent(k, N));
+    res[k] = evens.add(rotated_odds);
+    res[k + N / 2] = evens.sub(rotated_odds);
+  }
+  return res;
 };
 
 // real number fft
@@ -40,21 +74,7 @@ export const fft = <T extends number>(seq: Complex<number>[]): Complex<number>[]
   while (seq.length < N) {
     seq.push(new Complex<number>(0, 0));
   }
-  const res: Complex<number>[] = [];
-
-  // expected real number
-  if (N == 1) { return seq; }
-
-  const X_evens = fft(seq.filter((_, i) => i % 2 === 0));
-  const X_odds = fft(seq.filter((_, i) => i % 2 == 1));
-
-  for (let k = 0; k < N / 2; k++) {
-    const t = X_evens[k];
-    const e = fftUtil.exponent(k, N).mlt(X_odds[k]);
-    res[k] = t.add(e);
-    res[k + N / 2] = t.sub(e);
-  }
-  return res;
+  return fft_core(seq, new RootOfUnity());
 };
 
 export const ifft = <T extends number>(seq: Complex<T>[]) => {
