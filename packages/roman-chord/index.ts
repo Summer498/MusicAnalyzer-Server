510267ffb257c9c1d339faedffa1368d4afe7a73
Summer498 <subore02@gmail.com>
2025-03-02 08:24:35 +0900

    fix sharp flat bug

--- packages/roman-chord/index.ts
+++ packages/roman-chord/index.ts
@@ -1,15 +1,14 @@
 import { _RomanNumeral, _Chord, _Interval, _Note, _Scale, Chord, Scale } from "@music-analyzer/tonal-objects";
 
 const get_roman = (scale: Scale, chord: Chord) => {
-  // TODO: fix: Vb が V として出力される
   // chord.tonic || _throw(TypeError("chord.tonic should not be null"));  // NOTE: chord.tonic を null にするテストケースを思いつかないので(=無さそうなので)コメントアウト
-  const tonic = chord.tonic!;
-  // const true_tonic = scale.notes.find(e => _Note.chroma(e) === _Note.chroma(tonic));
-  // const interval = _Interval.distance(NN(scale.tonic), NN(true_tonic));
-  if (scale.tonic == null) {  // NOTE: scale が空の場合, ローマ数字分析ができないのでとりあえずコードをそのまま返す
+  if (chord.tonic === null || scale.tonic === null) {  // NOTE: tonic が空の場合, ローマ数字分析ができないのでとりあえずコードをそのまま返す
     return chord.name;
   }
-  const interval = _Interval.distance(scale.tonic, tonic);
+  const tonic = chord.tonic;
+  const true_tonic = scale.notes.find(e => _Note.chroma(e) === _Note.chroma(tonic));
+  const interval = _Interval.distance(scale.tonic, true_tonic!);
+  // const interval = _Interval.distance(scale.tonic, tonic);
   const roman = _RomanNumeral.get(_Interval.get(interval));
   return roman.roman + " " + chord.type;
 };
