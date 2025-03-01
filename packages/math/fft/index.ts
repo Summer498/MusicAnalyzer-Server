6fb18360861e37479681e4b93184821d358757a1
Summer498 <subore02@gmail.com>
2025-03-01 20:08:53 +0900

    refactoring: reduce space of Complex class

--- packages/math/fft/index.ts
+++ packages/math/fft/index.ts
@@ -3,30 +3,10 @@ export class Complex<T extends number> {
     readonly re: T,
     readonly im: T,
   ) { }
-  add(right: Complex<T>) {
-    return new Complex(
-      this.re + right.re,
-      this.im + right.im,
-    );
-  }
-  sub(right: Complex<T>) {
-    return new Complex(
-      this.re - right.re,
-      this.im - right.im,
-    );
-  }
-  scale(right: number){
-    return new Complex(
-      this.re * right,
-      this.im * right,
-    );
-  }
-  divScaler(right: number){
-    return new Complex(
-      this.re / right,
-      this.im / right,
-    );
-  }
+  add(right: Complex<T>) { return new Complex(this.re + right.re, this.im + right.im,); }
+  sub(right: Complex<T>) { return new Complex(this.re - right.re, this.im - right.im,); }
+  scale(right: number) { return new Complex(this.re * right, this.im * right,); }
+  divScaler(right: number) { return new Complex(this.re / right, this.im / right,); }
   mlt(right: Complex<T>) {
     return new Complex(
       this.re * right.re - this.im * right.im,
