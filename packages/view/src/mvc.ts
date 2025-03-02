7c701d816af5db0b8f03f55f7bcb7fb612637de5
Summer498 <subore02@gmail.com>
2025-03-02 20:56:31 +0900

    create interface &  use generics

--- packages/view/src/mvc.ts
+++ packages/view/src/mvc.ts
@@ -2,15 +2,23 @@ import { WindowReflectable } from "./window-reflectable";
 
 export abstract class MVVM_Model { }
 
-export abstract class MVVM_View implements WindowReflectable {
-  readonly abstract svg: SVGElement
-  protected readonly abstract model: MVVM_Model;
+export interface I_MVVM_View extends WindowReflectable {
+  readonly svg: SVGElement
+}
+export abstract class MVVM_View<M extends MVVM_Model, K extends keyof SVGElementTagNameMap> implements I_MVVM_View {
+  readonly svg: SVGElementTagNameMap[K];
+  constructor(
+    protected readonly model: M,
+    svg_tagname: K
+  ) {
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", svg_tagname);
+  }
   abstract onWindowResized(): void;
 }
 
 export abstract class MVVM_ViewModel implements WindowReflectable {
   readonly abstract model: MVVM_Model;
-  readonly abstract view: MVVM_View
+  readonly abstract view: I_MVVM_View;
   onWindowResized() {
     this.view.onWindowResized();
   }
