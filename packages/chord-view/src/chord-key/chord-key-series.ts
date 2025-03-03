4e94c08698f5c8289da7845457132fdb193bdd2d
Summer498 <subore02@gmail.com>
2025-03-03 12:10:17 +0900

    prepare to customize motion of key

--- packages/chord-view/src/chord-key/chord-key-series.ts
+++ packages/chord-view/src/chord-key/chord-key-series.ts
@@ -1,9 +1,15 @@
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
-import { ReflectableTimeAndMVCControllerCollection } from "@music-analyzer/view";
 import { ChordKeyVM } from "./chord-key-view-model";
+import { PianoRollTranslateX } from "@music-analyzer/view-parameters";
+import { MVVM_Collection } from "@music-analyzer/view/src/mvc";
 
-export class ChordKeySeries extends ReflectableTimeAndMVCControllerCollection<ChordKeyVM> {
+export class ChordKeySeries extends MVVM_Collection<ChordKeyVM> {
+  readonly remaining: ChordKeyVM | undefined;
   constructor(romans: TimeAndRomanAnalysis[]) {
     super("key-names", romans.map(e => new ChordKeyVM(e)));
+  }
+
+  onAudioUpdate() {
+    this.svg.setAttribute("transform", `translate(${PianoRollTranslateX.value})`);
   }
 }
--- packages/chord-view/src/chord-note/chord-note-view-model.ts
+++ packages/chord-view/src/chord-note/chord-note-view-model.ts
@@ -1,10 +1,12 @@
 import { _Note, Chord } from "@music-analyzer/tonal-objects";
-import { MVVM_ViewModel } from "@music-analyzer/view";
+import { AudioReflectable, MVVM_ViewModel } from "@music-analyzer/view";
 import { ChordNoteModel } from "./chord-note-model";
 import { ChordNoteView } from "./chord-note-view";
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
 
-export class ChordNoteVM extends MVVM_ViewModel<ChordNoteModel, ChordNoteView> {
+export class ChordNoteVM
+  extends MVVM_ViewModel<ChordNoteModel, ChordNoteView>
+  implements AudioReflectable {
   constructor(
     e: TimeAndRomanAnalysis,
     chord: Chord,
@@ -13,5 +15,8 @@ export class ChordNoteVM extends MVVM_ViewModel<ChordNoteModel, ChordNoteView> {
   ) {
     const model = new ChordNoteModel(e, chord, note, oct);
     super(model, new ChordNoteView(model));
+  }
+  onAudioUpdate() {
+    this.view.onWindowResized();
   }
 }
