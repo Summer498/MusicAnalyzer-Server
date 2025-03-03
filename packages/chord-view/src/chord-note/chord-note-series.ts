48809566d0d11172d6911876690a10a222f132da
Summer498 <subore02@gmail.com>
2025-03-03 12:02:57 +0900

    purge chord note elements into each class

--- packages/chord-view/src/chord-note/chord-note-series.ts
+++ packages/chord-view/src/chord-note/chord-note-series.ts
@@ -1,19 +1,25 @@
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
-import { getRange } from "@music-analyzer/math";
-import { _Chord } from "@music-analyzer/tonal-objects";
+import { _Chord, Chord } from "@music-analyzer/tonal-objects";
 import { ReflectableTimeAndMVCControllerCollection } from "@music-analyzer/view";
 import { OctaveCount } from "@music-analyzer/view-parameters";
 import { ChordNoteVM } from "./chord-note-view-model";
+import { MVVM_Collection } from "@music-analyzer/view/src/mvc";
 
-export class ChordNotesSeries extends ReflectableTimeAndMVCControllerCollection<ChordNoteVM> {
+export class ChordNotesInOctave extends MVVM_Collection<ChordNoteVM> {
+  constructor(roman: TimeAndRomanAnalysis, chord: Chord, oct: number) {
+    super(`${chord.name}-${oct}`, chord.notes.map(note => new ChordNoteVM(roman, chord, note, oct)));
+  }
+}
+
+export class ChordNotes extends MVVM_Collection<ChordNotesInOctave> {
+  constructor(readonly model: TimeAndRomanAnalysis) {
+    const chord = _Chord.get(model.chord);
+    super(chord.name, [...Array(OctaveCount.value)].map((_, oct) => new ChordNotesInOctave(model, chord, oct)));
+  }
+}
+
+export class ChordNotesSeries extends ReflectableTimeAndMVCControllerCollection<ChordNotes> {
   constructor(romans: TimeAndRomanAnalysis[]) {
-    super("chords", romans.map(roman => {
-      const chord = _Chord.get(roman.chord);
-      return getRange(0, OctaveCount.value)
-        .map(oct => chord.notes
-          .map(note => new ChordNoteVM(roman, chord, note, oct)
-          )
-        );
-    }).flat(2));
+    super("chords", romans.map(roman => new ChordNotes(roman)));
   }
 }
