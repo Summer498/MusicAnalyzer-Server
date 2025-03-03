7415862603ee75850b6891bb3f702ca9b1925dcc
Summer498 <subore02@gmail.com>
2025-03-03 18:24:09 +0900

    purge classes into each files

---
+++ packages/spectrogram/src/spectrogram-viewer.ts
@@ -0,0 +1,36 @@
+import { AudioReflectable } from "@music-analyzer/view";
+import { AudioAnalyzer } from "./audio-analyzer";
+
+export class spectrogramViewer implements AudioReflectable {
+  private readonly path: SVGPathElement;
+  readonly svg: SVGSVGElement;
+  constructor(
+    private readonly analyser: AudioAnalyzer,
+  ) {
+    this.path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    this.path.setAttribute("stroke", "red");
+    this.path.setAttribute("fill", "none");
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    this.svg.appendChild(this.path);
+    this.svg.id = "spectrum";
+    this.svg.setAttribute("width", String(800));
+    this.svg.setAttribute("height", String(450));
+  }
+
+  onAudioUpdate() {
+    const freqData = this.analyser.getByteFrequencyData();
+    const fftSize = freqData.length;
+    const width = this.svg.clientWidth;
+    const height = this.svg.clientHeight;
+    let pathData = "";
+
+    for (let i = 0; i < fftSize; i++) {
+      if (isNaN(freqData[i] * 0)) { continue; }
+      const x = i / (fftSize - 1) * width;
+      const y = (1 - freqData[i] / 255) * height;
+      // const y = (1 - Math.log2(1 + freqData[i]) / 8) * height;
+      pathData += `L ${x},${y}`;
+    }
+    this.path.setAttribute("d", "M" + pathData.slice(1));
+  }
+}
