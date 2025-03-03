c2ec964c71cca19937addb6e8dee4bd089af304d
Summer498 <subore02@gmail.com>
2025-03-03 20:06:34 +0900

    use transform than direct calculation

--- packages/melody-view/src/gravity/gravity-view.ts
+++ packages/melody-view/src/gravity/gravity-view.ts
@@ -2,19 +2,13 @@ import { NoteSize, BlackKeyPrm, PianoRollBegin } from "@music-analyzer/view-para
 import { MVVM_View } from "@music-analyzer/view";
 import { GravityModel } from "./gravity-model";
 
-const triangle_width = 5;
+const triangle_width = 4;
 const triangle_height = 5;
-
-type Vector2D = {
-  readonly x: number;
-  readonly y: number;
-}
 
 export class GravityView extends MVVM_View<GravityModel, "g"> {
   readonly triangle: SVGPolygonElement;
   readonly line: SVGLineElement;
-  readonly src: Vector2D;
-  readonly dst: Vector2D;
+  readonly line_seed: { readonly x1: number, readonly x2: number, readonly y1: number, readonly y2: number };
   constructor(model: GravityModel) {
     super(model, "g");
     this.triangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
@@ -32,57 +26,45 @@ export class GravityView extends MVVM_View<GravityModel, "g"> {
     this.svg.id = "gravity";
     this.svg.appendChild(this.triangle);
     this.svg.appendChild(this.line);
-    this.src = {
-      x: this.model.begin + this.model.duration / 2,
-      y: isNaN(this.model.note) ? -99 : (PianoRollBegin.value + 0.5 - this.model.note) * BlackKeyPrm.height
+    this.line_seed = {
+      x1: this.model.begin + this.model.duration / 2,
+      x2: this.model.next.begin,
+      y1: isNaN(this.model.note) ? -99 : (PianoRollBegin.value + 0.5 - this.model.note) * BlackKeyPrm.height,
+      y2: isNaN(this.model.note) ? -99 : (PianoRollBegin.value + 0.5 - this.model.gravity.destination!) * BlackKeyPrm.height,
     };
-    this.dst = {
-      x: this.model.next.begin,
-      y: (PianoRollBegin.value + 0.5 - this.model.gravity.destination!) * BlackKeyPrm.height
-    };
-    const view_pos = this.getViewPositions();
-    this.triangle.setAttribute("points", view_pos.p.join(","));
-    this.line.setAttribute("x1", String(view_pos.src.x));
-    this.line.setAttribute("x2", String(view_pos.dst.x));
-    this.line.setAttribute("y1", String(view_pos.src.y));
-    this.line.setAttribute("y2", String(view_pos.dst.y));
+    const view_pos = this.getLinePos();
+    this.triangle.setAttribute("points", this.getInitPos().join(","));
+    this.line.setAttribute("x1", String(view_pos.x1));
+    this.line.setAttribute("x2", String(view_pos.x2));
+    this.line.setAttribute("y1", String(view_pos.y1));
+    this.line.setAttribute("y2", String(view_pos.y2));
   }
-  getViewX(x: number) { return x * NoteSize.value; }
-  getViewPositions() {
-    const src: Vector2D = {
-      x: this.getViewX(this.src.x),
-      y: this.src.y
+  getInitPos() { return [0, 0, - triangle_width, + triangle_height, + triangle_width, + triangle_height,]; }
+  getLinePos() {
+    return {
+      x1: this.line_seed.x1 * NoteSize.value,
+      x2: this.line_seed.x2 * NoteSize.value,
+      y1: this.line_seed.y1,
+      y2: this.line_seed.y2,
     };
-    const dst: Vector2D = {
-      x: this.getViewX(this.dst.x),
-      y: this.dst.y
-    };
-
-    const w = dst.x - src.x;
-    const h = dst.y - src.y;
-    const r = Math.sqrt(w * w + h * h);
-    const cos = -h / r;
-    const sin = w / r;
-    const p = [
-      dst.x,
-      dst.y,
-      dst.x + cos * triangle_width - sin * triangle_height,
-      dst.y + sin * triangle_width + cos * triangle_height,
-      dst.x + cos * -triangle_width - sin * triangle_height,
-      dst.y + sin * -triangle_width + cos * triangle_height
-    ];
-    return { p, src, dst } as ({ readonly p: number[], readonly src: Vector2D, readonly dst: Vector2D });
+  }
+  getAngle() {
+    const line_pos = this.getLinePos();
+    const w = line_pos.x2 - line_pos.x1;
+    const h = line_pos.y2 - line_pos.y1;
+    return Math.atan2(h, w) * 180 / Math.PI + 90;
   }
   updateWidth() { this.svg.setAttribute("width", String(this.model.duration * NoteSize.value)); }
   updateHeight() { this.svg.setAttribute("height", String(BlackKeyPrm.height)); }
   onWindowResized() {
     this.updateWidth();
     this.updateHeight();
-    const view_pos = this.getViewPositions();
-    this.triangle.setAttribute("points", view_pos.p.join(","));
-    this.line.setAttribute("x1", String(view_pos.src.x));
-    this.line.setAttribute("x2", String(view_pos.dst.x));
-    this.line.setAttribute("y1", String(view_pos.src.y));
-    this.line.setAttribute("y2", String(view_pos.dst.y));
+    const angle = this.getAngle();
+    const line_pos = this.getLinePos();
+    this.triangle.setAttribute("transform", `translate(${line_pos.x2},${line_pos.y2}) rotate(${angle})`);
+    this.line.setAttribute("x1", String(line_pos.x1));
+    this.line.setAttribute("x2", String(line_pos.x2));
+    this.line.setAttribute("y1", String(line_pos.y1));
+    this.line.setAttribute("y2", String(line_pos.y2));
   }
 }
