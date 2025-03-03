ec36ea59ae405fa9a7b0c89bb9e887b76cf0b9cd
Summer498 <subore02@gmail.com>
2025-03-03 12:08:50 +0900

    create one letter key description

--- packages/chord-view/src/shorten.ts
+++ packages/chord-view/src/shorten.ts
@@ -18,3 +18,13 @@ export const shortenKey = (key: Scale) => {
   else if (type === "major") { return getCapitalCase(`${tonic}-dur`); }
   else { return key.name; }
 };
+
+export const oneLetterKey = (key: Scale) => {
+  const tonic = key.tonic || "";
+  const type = key.type;
+  if (type === "aeolian") { return getLowerCase(tonic); }
+  else if (type === "ionian") { return getCapitalCase(tonic); }
+  else if (type === "minor") { return getCapitalCase(tonic); }
+  else if (type === "major") { return getCapitalCase(tonic); }
+  else { return key.name; }
+};
