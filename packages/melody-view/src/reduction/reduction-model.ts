57ea04c8940bd026d647bbe9e96a4efb936cbd51
Summer498 <subore02@gmail.com>
2025-03-03 09:25:06 +0900

    push archetype on ReductionModel

--- packages/melody-view/src/reduction/reduction-model.ts
+++ packages/melody-view/src/reduction/reduction-model.ts
@@ -7,6 +7,7 @@ export class ReductionModel extends MVVM_Model {
   readonly end: number;
   readonly duration: number;
   readonly head: { readonly begin: number, readonly end: number, readonly duration: number };
+  readonly archetype: Archetype;
   constructor(
     readonly layer: number,
     e: TimeAndAnalyzedMelody,
@@ -20,5 +21,6 @@ export class ReductionModel extends MVVM_Model {
       ...e.head,
       duration: e.head.end - e.head.begin
     };
+    this.archetype = e.melody_analysis.implication_realization;
   }
 }
