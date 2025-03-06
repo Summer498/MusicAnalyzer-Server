d15663a2180d83b8e2d98d49ec58477f64ec7099
Summer498 <subore02@gmail.com>
2025-03-07 07:43:02 +0900

    bugfix: load roman analysis

--- ServerProgram/src/register-url-handlers.ts
+++ ServerProgram/src/register-url-handlers.ts
@@ -9,7 +9,7 @@ export const registerURLHandlers = (app: ReturnType<typeof express>) => {
   const upload = multer({ dest: POST_DATA_PATH });  // multer が POST_DATA_PATH にファイルを作成
 
   // URLの部分が一致するもののうち一番上にある関数の処理をする
-  app.get("/analyzed/chord/roman.json", loadRomanAnalysis);
+  app.get("/MusicAnalyzer-server/resources/**/analyzed/chord/roman.json", loadRomanAnalysis);
   app.get("/MusicAnalyzer-server/api/gttm-example/", listUpGTTMExample);
   app.post("/*", upload.single("upload"), renameFile, handlePostRequest);
   app.get("/*", sendRequestedFile);
