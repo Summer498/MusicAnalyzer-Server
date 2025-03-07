966124fe74988c93017db3cbd82a02c4eef91ab5
Summer498 <subore02@gmail.com>
2025-03-07 10:39:38 +0900

    remove unnecessary import

--- ServerProgram/src/routing/list-up-gttm-example.ts
+++ ServerProgram/src/routing/list-up-gttm-example.ts
@@ -1,19 +1,16 @@
 import { Request, Response } from "express";
-import { _throw, } from "../stdlib";
 import { getDirectoryContents } from "./get-directory-contents";
-import { HOME, ROOT_DIR } from "../constants";
 
 export const listUpGTTMExample = (req: Request, res: Response) => {
-  req.url || _throw(TypeError(`requested URL is null`));
-  req.url === "/MusicAnalyzer-server/api/gttm-example/"
-    || _throw(EvalError(`function handleHierarchicalAnalysisSample requires the url ends with '/'`));
+  if (req.url !== "/MusicAnalyzer-server/api/gttm-example/") {
+    throw new EvalError(`function handleHierarchicalAnalysisSample requires the url ends with '/'`);
+  }
 
   // json にリストを載せて返す
-  const read_dir = `${ROOT_DIR}/${HOME}/resources/gttm-example/`;
+  const read_dir = `resources/gttm-example/`;
   try {
-    getDirectoryContents(read_dir).then(
-      files => res.json(files)
-    );
+    getDirectoryContents(read_dir)
+      .then(files => res.json(files));
   } catch (err) {
     res.status(500).json({ error: `Failed to read directory contents of ${read_dir}` });
   }
