22dc73970eec28c65233fa93542cb3f37eff0c23
Summer498 <subore02@gmail.com>
2025-03-07 14:44:02 +0900

    bugfix: fix type

--- html/analyze/src/DataFetcher.ts
+++ html/analyze/src/DataFetcher.ts
@@ -3,7 +3,7 @@ import { xml_parser } from "./XMLParser";
 export const keyLength = (obj: object) => Object.keys(obj).length;
 export const getJSON = <T extends object>(url: string) => {
   return fetch(url)
-    .then(res => res.json() as T)
+    .then(res => res.json() as Promise<T>)
     .catch(e => { console.error(e); return undefined; });
 };
 export const getJSONfromXML = <T extends object>(url: string) => {
