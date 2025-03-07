d86cbd1ebbba3c6630249ea6b27006f9d51460b8
Summer498 <subore02@gmail.com>
2025-03-07 10:36:31 +0900

    use result of command to branch

--- ServerProgram/src/handle-analyzed-data/load-analysis.ts/call-program.ts
+++ ServerProgram/src/handle-analyzed-data/load-analysis.ts/call-program.ts
@@ -3,63 +3,72 @@ import { detectFile } from "../detect-file";
 import { runProcessWithCache } from "../run-process-with-cache";
 import { rename } from "./util";
 
+export const demucs = (force: boolean, directories: Directories, separate_dir: string) => {
+  const e = directories;
+  if (detectFile(e.src)) {
+    if (runProcessWithCache(false, separate_dir, `python -m demucs -d cuda "${e.src}" >&2"`)) {
+      rename(separate_dir, e.dst);
+    }
+  }
+};
+
 export const chordExtract = (force: boolean, directories: Directories) => {
   const e = directories;
-  detectFile(e.src);
-  runProcessWithCache(force, e.dst, `python -m chordExtract "${e.src}" "${e.dst}"`);
+  if (detectFile(e.src)) {
+    runProcessWithCache(force, e.dst, `python -m chordExtract "${e.src}" "${e.dst}"`);
+  }
 };
 
 export const chordToRoman = (force: boolean, directories: Directories) => {
   const e = directories;
-  detectFile(e.src);
-  runProcessWithCache(true, e.dst, `node ./packages/chord-analyze-cli < "${e.src}" > "${e.dst}"`);
+  if (detectFile(e.src)) {
+    runProcessWithCache(true, e.dst, `node ./packages/chord-analyze-cli < "${e.src}" > "${e.dst}"`);
+  }
 };
 
 export const crepe = (force: boolean, directories: Directories, tmp: string) => {
   const e = directories;
-  detectFile(e.src);
-  runProcessWithCache(force, e.dst, `"python -m crepe "${e.src}" >&2"`);
-  rename(e.dst, tmp);
+  if (detectFile(e.src)) {
+    if (runProcessWithCache(force, e.dst, `"python -m crepe "${e.src}" >&2"`)) {
+      rename(e.dst, tmp);
+    }
+  }
 };
 
 export const postCrepe = (force: boolean, directories: Directories) => {
   const e = directories;
-  detectFile("$post_crepe.src");
-  runProcessWithCache(force, e.dst, `python -m post-crepe "${e.src}" -o "${e.dst}"`);
+  if (detectFile(e.src)) {
+    runProcessWithCache(force, e.dst, `python -m post-crepe "${e.src}" -o "${e.dst}"`);
+  }
 };
 
 export const analyzeMelodyFromCrepeF0 = (force: boolean, directories: Directories, chord_src: string) => {
   const e = directories;
-  detectFile(e.src);
-  detectFile(chord_src);
-  runProcessWithCache(force, e.dst, `node ./packages/melody-analyze-cli -m "${e.src}" -r "${chord_src}" -o "${e.dst}" --sampling_rate 100`);
+  if (detectFile(e.src) && detectFile(chord_src)) {
+    runProcessWithCache(force, e.dst, `node ./packages/melody-analyze-cli -m "${e.src}" -r "${chord_src}" -o "${e.dst}" --sampling_rate 100`);
+  }
 };
 
 export const pyin = (force: boolean, directories: Directories, img_dir: Directories) => {
   const e = directories;
-  detectFile(e.src);
-  runProcessWithCache(force, e.dst, `python -m pyin "${e.src}" --fmin 128 --fmax 1024 -o "${e.dst}"`);
-  runProcessWithCache(force, img_dir.dst, `python -m pyin2img "${img_dir.src}" --audio_file "${e.src}" -o "${img_dir.dst}"`);
-
+  if (detectFile(e.src)) {
+    if (runProcessWithCache(force, e.dst, `python -m pyin "${e.src}" --fmin 128 --fmax 1024 -o "${e.dst}"`)) {
+      runProcessWithCache(force, img_dir.dst, `python -m pyin2img "${img_dir.src}" --audio_file "${e.src}" -o "${img_dir.dst}"`);
+    }
+  }
 };
 
 export const postPYIN = (force: boolean, directories: Directories, post_pyin_dir: string) => {
   const e = directories;
-  detectFile(e.src);
-  runProcessWithCache(force, e.dst, `node ./packages/post-pyin "${e.src}" "${post_pyin_dir}"`);
+  if (detectFile(e.src)) {
+    runProcessWithCache(force, e.dst, `node ./packages/post-pyin "${e.src}" "${post_pyin_dir}"`);
+  }
 };
 
 export const analyzeMelodyFromPYINf0 = (force: boolean, directories: Directories, chord_src: string) => {
   const e = directories;
-  detectFile(e.src);
-  detectFile(chord_src);
-  const sr = 22050 / 1024;
-  runProcessWithCache(force, e.dst, `node ./packages/melody-analyze-cli -m "${e.src}" -r "${chord_src}" -o "${e.dst}" --sampling_rate ${sr}`);
-};
-
-export const demucs = (force_reanalyze: boolean, directories: Directories, separate_dir: string) => {
-  const e = directories;
-  detectFile(e.src);
-  runProcessWithCache(false, separate_dir, `python -m demucs -d cuda "${e.src}" >&2"`);
-  rename(separate_dir, e.dst);
+  if (detectFile(e.src) && detectFile(chord_src)) {
+    const sr = 22050 / 1024;
+    runProcessWithCache(force, e.dst, `node ./packages/melody-analyze-cli -m "${e.src}" -r "${chord_src}" -o "${e.dst}" --sampling_rate ${sr}`);
+  }
 };
