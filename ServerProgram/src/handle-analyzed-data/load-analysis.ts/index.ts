790af1872ffdeffb4f8f541e049a8cc406853121
Summer498 <subore02@gmail.com>
2025-03-07 14:44:37 +0900

    use update flag

--- ServerProgram/src/handle-analyzed-data/load-analysis.ts/index.ts
+++ ServerProgram/src/handle-analyzed-data/load-analysis.ts/index.ts
@@ -1,4 +1,4 @@
-import { default as url } from "url";
+import { default as URL } from "url";
 import { existsSync } from "fs";
 import { basename } from "path";
 import { sendFile } from "../../routing";
@@ -7,71 +7,85 @@ import { ROOT } from "../../constants";
 import { DataDirectories } from "../data-directories";
 import { analyzeMelodyFromCrepeF0, analyzeMelodyFromPYINf0, chordExtract, chordToRoman, crepe, demucs, postCrepe, postPYIN, pyin } from "./call-program";
 
-const _loadRomanAnalysis = (song_name: string, file_path: string) => {
-  const force_reanalyze = false;
+const _loadRomanAnalysis = (update: boolean, song_name: string, file_path: string) => {
+  const force = false;
   const e = new DataDirectories(song_name, file_path);
 
-  chordExtract(force_reanalyze, e.chord_ext);
-  chordToRoman(force_reanalyze, e.chord_to_roman);
+  chordExtract(force, e.chord_ext);
+  chordToRoman(update, e.chord_to_roman);
 };
 
-const _loadAnalysisFromCREPE = (song_name: string, file_path: string) => {
-  const force_reanalyze = false;
+const _loadAnalysisFromCREPE = (update: boolean, song_name: string, file_path: string) => {
+  const force = false;
   const e = new DataDirectories(song_name, file_path);
-  _loadRomanAnalysis(song_name, file_path);
+  chordExtract(false, e.chord_ext);
+  chordToRoman(false, e.chord_to_roman);
 
-  demucs(force_reanalyze, e.demucs, e.separate_dir);
-  crepe(force_reanalyze, e.crepe, e.crepe_tmp);
-  postCrepe(force_reanalyze, e.post_crepe);
-  analyzeMelodyFromCrepeF0(force_reanalyze, e.melody_analyze_crepe, e.chord_to_roman.dst);
+  demucs(force, e.demucs, e.separate_dir);
+  crepe(force, e.crepe, e.crepe_tmp);
+  postCrepe(force, e.post_crepe);
+  analyzeMelodyFromCrepeF0(update, e.melody_analyze_crepe, e.chord_to_roman.dst);
 };
 
-const _loadAnalysisFromPYIN = (song_name: string, file_path: string) => {
-  const force_reanalyze = false;
+const _loadAnalysisFromPYIN = (update: boolean, song_name: string, file_path: string) => {
+  const force = false;
   const e = new DataDirectories(song_name, file_path);
-  _loadRomanAnalysis(song_name, file_path);
+  chordExtract(false, e.chord_ext);
+  chordToRoman(false, e.chord_to_roman);
 
-  demucs(force_reanalyze, e.demucs, e.separate_dir);
-  pyin(force_reanalyze, e.pyin, e.pyin_img);
-  postPYIN(force_reanalyze, e.post_pyin, e.post_pyin_dir);
-  analyzeMelodyFromPYINf0(force_reanalyze, e.melody_analyze_pyin, e.chord_to_roman.dst);
+  demucs(force, e.demucs, e.separate_dir);
+  pyin(force, e.pyin, e.pyin_img);
+  postPYIN(force, e.post_pyin, e.post_pyin_dir);
+  analyzeMelodyFromPYINf0(update, e.melody_analyze_pyin, e.chord_to_roman.dst);
 };
 
 export const loadRomanAnalysis = (req: Request, res: Response) => {
   console.log("/analyzed/chord/roman.json");
-  const pathname = url.parse(req.url, true, true).pathname;
+  const url = URL.parse(req.url, true, true);
+  const search = new URLSearchParams(url.search || "");
+  const update = search.has("update");
+  console.log(search);
+  const pathname = url.pathname;
   if (pathname === null) { throw new Error(`pathname was null`); };
   const song_dir = pathname.replace("/analyzed/chord/roman.json", "");
   const song_name = basename(song_dir);
 
   const extensions: ["wav", "mp3", "mp4", "m4a"] = ["wav", "mp3", "mp4", "m4a"];
   const ext = extensions.find(e => existsSync(decodeURI(`${ROOT}${song_dir}/${song_name}.${e}`)));
-  if (ext) { _loadRomanAnalysis(song_name, ""); }
+  if (ext) { _loadRomanAnalysis(update, song_name, ""); }
   sendFile(req, res, `${ROOT}${pathname}`);
 };
 
 export const loadAnalysisFromCrepe = (req: Request, res: Response) => {
   console.log("/analyzed/melody/crepe/manalyze.json");
-  const pathname = url.parse(req.url, true, true).pathname;
+  const url = URL.parse(req.url, true, true);
+  const search = new URLSearchParams(url.search || "");
+  const update = search.has("update");
+  console.log(search);
+  const pathname = url.pathname;
   if (pathname === null) { throw new Error(`pathname was null`); };
   const song_dir = pathname.replace("/analyzed/melody/crepe/manalyze.json", "");
   const song_name = basename(song_dir);
 
   const extensions: ["wav", "mp3", "mp4", "m4a"] = ["wav", "mp3", "mp4", "m4a"];
   const ext = extensions.find(e => existsSync(decodeURI(`${ROOT}${song_dir}/${song_name}.${e}`)));
-  if (ext) { _loadAnalysisFromCREPE(song_name, ""); }
+  if (ext) { _loadAnalysisFromCREPE(update, song_name, ""); }
   sendFile(req, res, `${ROOT}${pathname}`);
 };
 
 export const loadAnalysisFromPYIN = (req: Request, res: Response) => {
   console.log("/analyzed/melody/pyin/manalyze.json");
-  const pathname = url.parse(req.url, true, true).pathname;
+  const url = URL.parse(req.url, true, true);
+  const search = new URLSearchParams(url.search || "");
+  const update = search.has("update");
+  console.log(search);
+  const pathname = url.pathname;
   if (pathname === null) { throw new Error(`pathname was null`); };
   const song_dir = pathname.replace("/analyzed/melody/pyin/manalyze.json", "");
   const song_name = basename(song_dir);
 
   const extensions: ["wav", "mp3", "mp4", "m4a"] = ["wav", "mp3", "mp4", "m4a"];
   const ext = extensions.find(e => existsSync(decodeURI(`${ROOT}${song_dir}/${song_name}.${e}`)));
-  if (ext) { _loadAnalysisFromPYIN(song_name, ""); }
+  if (ext) { _loadAnalysisFromPYIN(update, song_name, ""); }
   sendFile(req, res, `${ROOT}${pathname}`);
 };
