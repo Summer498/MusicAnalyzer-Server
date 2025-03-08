8eb59855a0122eae1bc82920e5fc271ed4fca1ee
Summer498 <subore02@gmail.com>
2025-03-08 21:22:37 +0900

    create instantiation function as method or constructor

--- packages/chord-analyze/src/chord-analyze/time-and-roman-analysis.ts
+++ packages/chord-analyze/src/chord-analyze/time-and-roman-analysis.ts
@@ -1,11 +1,42 @@
+type TimeAndRomanAnalysis_Arg = [number, number, string, string, string];
+const getArgsOfTimeAndRomanAnalysis = (
+  args
+    : TimeAndRomanAnalysis_Arg
+    | [TimeAndRomanAnalysis]
+) => {
+  if (args.length === 1) {
+    const [e] = args;
+    return [e.begin, e.end, e.chord, e.scale, e.roman] as TimeAndRomanAnalysis_Arg
+  }
+  return args;
+}
+
 export class TimeAndRomanAnalysis {
+  readonly begin: number
+  readonly end: number
+  readonly chord: string
+  readonly scale: string
+  readonly roman: string
+  constructor(e: TimeAndRomanAnalysis);
   constructor(
-    readonly begin: number,
-    readonly end: number,
-    readonly chord: string,
-    readonly scale: string,
-    readonly roman: string,
-  ) { }
+    begin: number,
+    end: number,
+    chord: string,
+    scale: string,
+    roman: string,
+  );
+  constructor(
+    ...args
+      : TimeAndRomanAnalysis_Arg
+      | [TimeAndRomanAnalysis]
+  ) {
+    const [begin, end, chord, scale, roman] = getArgsOfTimeAndRomanAnalysis(args);
+    this.begin = begin
+    this.end = end;
+    this.chord = chord;
+    this.scale = scale;
+    this.roman = roman;
+  }
 }
 
 const v = "25.03.07.10.50";
@@ -17,8 +48,9 @@ export class RomanAnalysisData {
   static checkVersion(e: RomanAnalysisData) {
     return e.version === v;
   }
+  // required by the class with the constructor which has 1 argument
   static instantiate(e: RomanAnalysisData) {
-    return new RomanAnalysisData(e.body.map(e => new TimeAndRomanAnalysis(e.begin, e.end, e.chord, e.scale, e.roman)))
+    return new RomanAnalysisData(e.body.map(e => new TimeAndRomanAnalysis(e)))
   }
 }
 
