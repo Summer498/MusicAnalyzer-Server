e528829b011d7e321c48a6a43477c40940d7464f
Summer498 <subore02@gmail.com>
2025-03-08 17:09:16 +0900

    create a self-instantiating constructor

--- packages/time-and/src/time.ts
+++ packages/time-and/src/time.ts
@@ -1,9 +1,32 @@
+const getArgs = (
+  ...args:
+    [Time]
+    | [number, number]
+) => {
+  if (args.length === 1) {
+    const [e] = args;
+    return [e.begin, e.end];
+  }
+  else {
+    return args
+  }
+}
+
 export class Time {
+  readonly begin: number
+  readonly end: number
   get duration() { return this.end - this.begin }
+  constructor(time: Time);
+  constructor(begin: number, end: number);
   constructor(
-    readonly begin: number,
-    readonly end: number,
-  ) { }
+    ...args:
+      [Time]
+      | [number, number]
+  ) {
+    const e = getArgs(...args);
+    this.begin = e[0];
+    this.end = e[1];
+  }
   map(func: (e: number) => number) {
     return new Time(func(this.begin), func(this.end));
   }
