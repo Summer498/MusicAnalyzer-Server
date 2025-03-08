27441b8744ed5a0500810a0d3a262722c1a110d9
Summer498 <subore02@gmail.com>
2025-03-08 15:27:31 +0900

    use Time

--- packages/chord-view/src/chord-roman/chord-roman-model.ts
+++ packages/chord-view/src/chord-roman/chord-roman-model.ts
@@ -1,16 +1,15 @@
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
+import { Time } from "@music-analyzer/time-and";
 import { _Chord } from "@music-analyzer/tonal-objects";
 import { MVVM_Model } from "@music-analyzer/view";
 
 export class ChordRomanModel extends MVVM_Model {
-  readonly begin: number;
-  readonly end: number;
+  readonly time: Time;
   readonly tonic: string;
   readonly roman: string;
-  constructor(e: TimeAndRomanAnalysis){
+  constructor(e: TimeAndRomanAnalysis) {
     super();
-    this.begin = e.begin;
-    this.end = e.end;
+    this.time = new Time(e.begin, e.end);
     this.roman = e.roman;
     this.tonic = _Chord.get(e.chord).tonic || "";
   }
