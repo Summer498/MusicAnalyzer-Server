4d99c4d3b80ae6e40261da46a23dabe679cd4a82
Summer498 <subore02@gmail.com>
2025-03-08 13:44:55 +0900

    song_name should be decoded

--- ServerProgram/src/handle-analyzed-data/load-analysis.ts/index.ts
+++ ServerProgram/src/handle-analyzed-data/load-analysis.ts/index.ts
@@ -46,11 +46,11 @@ export const loadRomanAnalysis = (req: Request, res: Response) => {
   const update = search.has("update");
   const pathname = url.pathname;
   if (pathname === null) { throw new Error(`pathname was null`); };
-  const song_dir = pathname.replace("/analyzed/chord/roman.json", "");
+  const song_dir = decodeURI(pathname.replace("/analyzed/chord/roman.json", ""));
   const song_name = basename(song_dir);
 
   const extensions: ["wav", "mp3", "mp4", "m4a"] = ["wav", "mp3", "mp4", "m4a"];
-  const ext = extensions.find(e => existsSync(decodeURI(`${ROOT}${song_dir}/${song_name}.${e}`)));
+  const ext = extensions.find(e => existsSync((`${ROOT}${song_dir}/${song_name}.${e}`)));
   if (ext) { _loadRomanAnalysis(update, song_name, `${ROOT}${song_dir}/${song_name}.${ext}`); }
   sendFile(req, res, `${ROOT}${pathname}`);
 };
@@ -62,11 +62,11 @@ export const loadAnalysisFromCrepe = (req: Request, res: Response) => {
   const update = search.has("update");
   const pathname = url.pathname;
   if (pathname === null) { throw new Error(`pathname was null`); };
-  const song_dir = pathname.replace("/analyzed/melody/crepe/manalyze.json", "");
+  const song_dir = decodeURI(pathname.replace("/analyzed/melody/crepe/manalyze.json", ""));
   const song_name = basename(song_dir);
 
   const extensions: ["wav", "mp3", "mp4", "m4a"] = ["wav", "mp3", "mp4", "m4a"];
-  const ext = extensions.find(e => existsSync(decodeURI(`${ROOT}${song_dir}/${song_name}.${e}`)));
+  const ext = extensions.find(e => existsSync((`${ROOT}${song_dir}/${song_name}.${e}`)));
   if (ext) { _loadAnalysisFromCREPE(update, song_name, `${ROOT}${song_dir}/${song_name}.${ext}`); }
   sendFile(req, res, `${ROOT}${pathname}`);
 };
@@ -78,11 +78,11 @@ export const loadAnalysisFromPYIN = (req: Request, res: Response) => {
   const update = search.has("update");
   const pathname = url.pathname;
   if (pathname === null) { throw new Error(`pathname was null`); };
-  const song_dir = pathname.replace("/analyzed/melody/pyin/manalyze.json", "");
+  const song_dir = decodeURI(pathname.replace("/analyzed/melody/pyin/manalyze.json", ""));
   const song_name = basename(song_dir);
 
   const extensions: ["wav", "mp3", "mp4", "m4a"] = ["wav", "mp3", "mp4", "m4a"];
-  const ext = extensions.find(e => existsSync(decodeURI(`${ROOT}${song_dir}/${song_name}.${e}`)));
+  const ext = extensions.find(e => existsSync((`${ROOT}${song_dir}/${song_name}.${e}`)));
   if (ext) { _loadAnalysisFromPYIN(update, song_name, `${ROOT}${song_dir}/${song_name}.${ext}`); }
   sendFile(req, res, `${ROOT}${pathname}`);
 };
