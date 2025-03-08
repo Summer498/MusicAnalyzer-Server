69faa13b93abb6fd2a31e87cbafab5c610ed3820
Summer498 <subore02@gmail.com>
2025-03-08 16:07:13 +0900

    compress type

--- packages/melody-view/src/melody/melody-model.ts
+++ packages/melody-view/src/melody/melody-model.ts
@@ -5,13 +5,13 @@ import { MVVM_Model } from "@music-analyzer/view";
 export class MelodyModel extends MVVM_Model {
   readonly time: Time;
   readonly note: number;
-  readonly head: { begin: number, end: number };
+  readonly head: Time;
   readonly melody_analysis: MelodyAnalysis;
   constructor(e: TimeAndAnalyzedMelody) {
     super();
     this.time = new Time(e.begin, e.end);
     this.note = e.note;
-    this.head = e.head;
+    this.head = new Time(e.head.begin, e.head.end);
     this.melody_analysis = e.melody_analysis;
   }
 }
