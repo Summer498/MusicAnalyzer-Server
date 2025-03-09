f0b2beea73186443e9c4106485b64150e68af9bb
Summer498 <subore02@gmail.com>
2025-03-09 21:45:37 +0900

    accept triad

--- packages/irm/src/colors.ts/analog-parametric-scale.ts
+++ packages/irm/src/colors.ts/analog-parametric-scale.ts
@@ -1,15 +1,19 @@
 import { hsv2rgb, rgbToString } from "@music-analyzer/color";
-import { Archetype } from "../archetype";
+import { Archetype, Triad } from "../archetype";
 
 const get_grb_on_parametric_scale = (archetype: Archetype) => {
-  const s = archetype.intervallic.direction.name === "mL" ? -1 : 0;
-  const v = archetype.intervallic.direction.name === "mR" ? -1 : 0;
-  const scale = archetype.intervallic.magnitude.name === "AA" ? 0.25 : 0.5;
+  const s = archetype.intervallic?.direction.name === "mL" ? -1 : 0;
+  const v = archetype.intervallic?.direction.name === "mR" ? -1 : 0;
+  const scale = archetype.intervallic?.magnitude.name === "AA" ? 0.25 : 0.5;
   const B = 120;
-  switch (archetype.registral.direction.name) {
+  switch (archetype.registral?.direction.name) {
     case "mL": return hsv2rgb(-120 + B, 1 + s * scale, 1 + v * scale);
     case "mN": return hsv2rgb(0 + B, 1 + s * scale, 1 + v * scale);
     case "mR": return hsv2rgb(120 + B, 1 + s * scale, 1 + v * scale);
   }
+  return[64,64,64]
 };
-export const get_color_on_parametric_scale = (archetype: Archetype) => rgbToString(get_grb_on_parametric_scale(archetype));
+
+export function get_color_on_parametric_scale(archetype: Archetype | Triad) {
+  return rgbToString(get_grb_on_parametric_scale(archetype))
+}
