56f2277e5510ad7b4b0a28d40891158da9bb7977
Summer498 <subore02@gmail.com>
2025-03-11 05:54:28 +0900

    order code

--- packages/tonal-objects/index.ts
+++ packages/tonal-objects/index.ts
@@ -1,12 +1,12 @@
 export { default as _Chord } from "@tonaljs/chord";
 export { default as _Scale } from "@tonaljs/scale";
-export { type Chord } from '@tonaljs/chord';
-export { type Scale } from '@tonaljs/scale';
 export { default as _RomanNumeral } from "@tonaljs/roman-numeral";
 export { default as _Note } from "@tonaljs/note";
 export { default as _Interval } from "@tonaljs/interval";
 export { default as _Key } from "@tonaljs/key";
 export { ChordDictionary as _ChordDictionary } from "tonal";
+export { type Chord } from '@tonaljs/chord';
+export { type Scale } from '@tonaljs/scale';
 export { type IntervalLiteral, type IntervalName } from '@tonaljs/pitch-interval';
 export { NoteLiteral } from "@tonaljs/pitch-note";
 
