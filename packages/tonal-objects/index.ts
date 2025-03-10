f49ba29abe7e3e81dd0ec4792471d202c4cc1213
Summer498 <subore02@gmail.com>
2025-03-11 06:09:09 +0900

    remove unused export

--- packages/tonal-objects/index.ts
+++ packages/tonal-objects/index.ts
@@ -4,7 +4,6 @@ export { default as _RomanNumeral } from "@tonaljs/roman-numeral";
 export { default as _Note } from "@tonaljs/note";
 export { default as _Interval } from "@tonaljs/interval";
 export { default as _Key } from "@tonaljs/key";
-export { ChordDictionary as _ChordDictionary } from "tonal";
 export { type Chord } from '@tonaljs/chord';
 export { type Scale } from '@tonaljs/scale';
 export { type IntervalLiteral, type IntervalName } from '@tonaljs/pitch-interval';
