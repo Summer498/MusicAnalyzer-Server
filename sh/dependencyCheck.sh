df57f5d0eb2f24e976e5649f5195adcb0709043b
Summer498 <subore02@gmail.com>
2025-03-13 07:06:31 +0900

    introduce depcheck

---
+++ sh/dependencyCheck.sh
@@ -0,0 +1,10 @@
+green=[32m
+defcol=[39m
+
+if find packages/**/package.json -type f > /dev/null 2>&1; then
+    for dist in packages/**/package.json; do
+        echo npx depcheck $(dirname $dist)
+        npx depcheck $(dirname $dist)
+    done
+fi
+
