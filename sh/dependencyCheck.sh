90862e8dd3c455705584ccc65d6757e2f71e9e8a
Summer498 <subore02@gmail.com>
2025-03-13 09:08:42 +0900

    analyze html directory too

--- sh/dependencyCheck.sh
+++ sh/dependencyCheck.sh
@@ -1,6 +1,19 @@
 green=[32m
 defcol=[39m
 
+
+echo ./html
+npx depcheck ./html
+echo ""
+
+echo ./html/analyze
+npx depcheck ./html/analyze
+echo ""
+
+echo ./html/hierarchical-analysis-sample
+npx depcheck ./html/hierarchical-analysis-sample
+echo ""
+
 if find packages/**/package.json -type f > /dev/null 2>&1; then
     for dist in packages/**/package.json; do
         echo $(dirname $dist)
