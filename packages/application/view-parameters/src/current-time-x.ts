21450a56bc37c419367f33123067dd719d5a71a1
Summer498 <subore02@gmail.com>
2025-03-13 19:30:04 +0900

    remove value

--- packages/application/view-parameters/src/current-time-x.ts
+++ packages/application/view-parameters/src/current-time-x.ts
@@ -2,9 +2,7 @@ import { CurrentTimeRatio } from "./current-time-ratio";
 import { PianoRollWidth } from "./piano-roll/piano-roll-width";
 
 export class CurrentTimeX {
-  static #value: number;
   static get() {
-    this.#value = PianoRollWidth.get() * CurrentTimeRatio.get();
-    return this.#value;
+    return PianoRollWidth.get() * CurrentTimeRatio.get();
   }
 }
