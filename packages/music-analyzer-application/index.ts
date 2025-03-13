45668eac7e7550dc513beb89b3a19bcf8f92d746
Summer498 <subore02@gmail.com>
2025-03-13 11:04:37 +0900

    use export from other package

--- packages/music-analyzer-application/index.ts
+++ packages/music-analyzer-application/index.ts
@@ -3,5 +3,6 @@ export { ControllerUIs } from "./src/controller-uis";
 export { loadMusicAnalysis, setAudioPlayer } from "./src/data-loader";
 export { initializeApplication } from "./src/initialize-application";
 export { MusicAnalyzerWindow, GTTMData, AnalyzedMusicData } from "./src/MusicAnalyzerWindow";
-export { AudioReflectable, AudioReflectableRegistry, WindowReflectable, WindowReflectableRegistry } from "./src/reflectable";
 export { setupUI } from "./src/setup-ui";
+
+export { type AudioReflectable, AudioReflectableRegistry, type WindowReflectable, WindowReflectableRegistry } from "@music-analyzer/view";
--- packages/view/index.ts
+++ packages/view/index.ts
@@ -1,4 +1,5 @@
 export { CollectionLayer } from "./src/collection-layer";
 export { CollectionHierarchy } from "./src/collection-hierarchy";
 export { MVVM_Collection, MVVM_Model, MVVM_View, MVVM_ViewModel } from "./src/mvc";
+export { AudioReflectable, AudioReflectableRegistry, WindowReflectable, WindowReflectableRegistry } from "./src/reflectable";
 export { ReflectableTimeAndMVCControllerCollection } from "./src/svg-collection";
