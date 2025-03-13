155f74f4e313d22c95b350807aabb148a8bbf916
Summer498 <subore02@gmail.com>
2025-03-13 21:17:00 +0900

    move function into application package

--- packages/application/music-analyzer-application/index.ts
+++ packages/application/music-analyzer-application/index.ts
@@ -2,6 +2,7 @@ export { AnalyzedDataContainer } from "./src/analyzed-data-container";
 export { ControllerUIs } from "./src/controller-uis";
 export { loadMusicAnalysis, setAudioPlayer } from "./src/data-loader";
 export { EventLoop } from "./src/EventLoop";
+export { handleGttmExampleIDs } from "./src/handle-gttm-example-IDs";
 export { initializeApplication } from "./src/initialize-application";
 export { MusicAnalyzerWindow, GTTMData, AnalyzedMusicData } from "./src/MusicAnalyzerWindow";
 export { setupUI } from "./src/setup-ui";
