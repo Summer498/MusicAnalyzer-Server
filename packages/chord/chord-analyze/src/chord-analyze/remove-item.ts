c3ce3756eebf6b9f907e76993e2144256c651cf0
Summer498 <subore02@gmail.com>
2025-03-13 12:16:59 +0900

    organize directory

--- packages/chord/chord-analyze/src/chord-analyze/remove-item.ts
+++
@@ -1,8 +0,0 @@
-export const remove_item = <T>(array: T[], will_removed: (item: T) => boolean) => {
-  const ret: T[] = [];
-  for (const e of array) {
-    if (will_removed(e)) { continue; }
-    ret.push(e);
-  }
-  return ret;
-};
