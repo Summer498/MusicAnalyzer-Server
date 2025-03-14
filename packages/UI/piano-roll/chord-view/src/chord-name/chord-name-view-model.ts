d119a46d782488cab5076a2c6b44971b4118d8e2
Summer498 <subore02@gmail.com>
2025-03-14 22:42:41 +0900

    separate responsibility (calculate/setAttribute)

--- packages/UI/piano-roll/chord-view/src/chord-name/chord-name-view-model.ts
+++ packages/UI/piano-roll/chord-view/src/chord-name/chord-name-view-model.ts
@@ -3,13 +3,23 @@ import { MVVM_ViewModel } from "@music-analyzer/view";
 import { ChordNameModel } from "./chord-name-model";
 import { ChordNameView } from "./chord-name-view";
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
+import { NoteSize, PianoRollHeight } from "@music-analyzer/view-parameters";
+import { chord_text_size } from "../chord-view-params";
+
+const scaled = (e: number) => e * NoteSize.get();
 
 export class ChordNameVM extends MVVM_ViewModel<ChordNameModel, ChordNameView> {
+  #y: number;
   constructor(e: TimeAndRomanAnalysis) {
     const model = new ChordNameModel(e);
     super(model, new ChordNameView(model));
+    this.updateX();
+    this.updateY();
+    this.#y = PianoRollHeight.get() + chord_text_size;
   }
+  updateX() { this.view.updateX(scaled(this.model.time.begin)) }
+  updateY() { this.view.updateY(this.#y) }
   onWindowResized() {
-    this.view.onWindowResized()
+    this.updateX();
   }
 }
--- packages/UI/piano-roll/chord-view/src/chord-name/chord-name-view.ts
+++ packages/UI/piano-roll/chord-view/src/chord-name/chord-name-view.ts
@@ -1,15 +1,10 @@
-import { _Chord } from "@music-analyzer/tonal-objects";
-import { NoteSize, PianoRollHeight } from "@music-analyzer/view-parameters";
 import { fifthToColor } from "@music-analyzer/color";
 import { MVVM_View } from "@music-analyzer/view";
 import { shortenChord } from "../shorten";
 import { ChordNameModel } from "./chord-name-model";
-import { chord_text_em, chord_text_size } from "../chord-view-params";
-
-const scaled = (e: number) => e * NoteSize.get();
+import { chord_text_em } from "../chord-view-params";
 
 export class ChordNameView extends MVVM_View<ChordNameModel, "text"> {
-  readonly y: number;
   constructor(model: ChordNameModel) {
     super(model, "text");
     this.svg.textContent = shortenChord(this.model.name);
@@ -17,13 +12,7 @@ export class ChordNameView extends MVVM_View<ChordNameModel, "text"> {
     this.svg.style.fontFamily = "Times New Roman";
     this.svg.style.fontSize = `${chord_text_em}em`;
     this.svg.style.fill = fifthToColor(this.model.tonic, 1, 0.75) || "rgb(0, 0, 0)";
-    this.y = PianoRollHeight.get() + chord_text_size;
-    this.updateX();
-    this.updateY();
   }
-  updateX() { this.svg.setAttribute("x", String(scaled(this.model.time.begin))); }
-  updateY() { this.svg.setAttribute("y", String(this.y)); }
-  onWindowResized() {
-    this.updateX();
-  }
+  updateX(x: number) { this.svg.setAttribute("x", String(x)); }
+  updateY(y: number) { this.svg.setAttribute("y", String(y)); }
 }
