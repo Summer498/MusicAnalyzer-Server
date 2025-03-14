fd8d511ef0d5875175d61ad1a98b04c2c8721b4d
Summer498 <subore02@gmail.com>
2025-03-14 13:33:10 +0900

    move generator function into constructor function

--- packages/application/music-analyzer-application/src/initialize-application.ts
+++ packages/application/music-analyzer-application/src/initialize-application.ts
@@ -7,26 +7,4 @@ import { AnalyzedMusicData } from "./MusicAnalyzerWindow";
 
 export const initializeApplication = (
   analyzed: AnalyzedMusicData,
-) => {
-  const d_romans: TimeAndRomanAnalysis[] = analyzed.roman.map(e => e);
-  const d_melodies: TimeAndAnalyzedMelody[] = analyzed.melody.map(e => e);
-  const romans = d_romans.map(e => e);
-  const melodies = d_melodies.map(e => e)
-    .filter((e, i) => i + 1 >= d_melodies.length || 60 / (d_melodies[i + 1].time.begin - d_melodies[i].time.begin) < 300 * 4);
-
-  // テンポの計算
-  const beat_info = calcTempo(melodies, romans);
-
-  // SVG -->
-  const highest_pitch = analyzed.melody.reduce((p, c) => isNaN(p.note) ? c : isNaN(c.note) ? p : p.note > c.note ? p : c).note || 0;
-  const lowest_pitch = analyzed.melody.reduce((p, c) => isNaN(p.note) ? c : isNaN(c.note) ? p : p.note < c.note ? p : c).note || 0;
-  PianoRollBegin.set(highest_pitch + Math.floor(analyzed.hierarchical_melody.length * bracket_height / 12) * 12 + 12);
-  PianoRollEnd.set(lowest_pitch - 3);
-  return new AnalyzedDataContainer(
-    beat_info,
-    romans,
-    analyzed.hierarchical_melody,
-    melodies,
-    d_melodies,
-  );
-};
+) => { new AnalyzedDataContainer(analyzed) };
