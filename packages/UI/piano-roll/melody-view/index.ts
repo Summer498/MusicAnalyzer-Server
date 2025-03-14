315343f1f3ce9cbc3fe7adcbe71e7a409d71070e
Summer498 <subore02@gmail.com>
2025-03-14 16:01:51 +0900

    remove unused export

--- packages/UI/piano-roll/melody-view/index.ts
+++ packages/UI/piano-roll/melody-view/index.ts
@@ -2,5 +2,5 @@ export { DMelodyGroup } from "./src/d-melody";
 export { ReductionHierarchy } from "./src/reduction";
 export { IRSymbolHierarchy } from "./src/ir-symbol";
 export { IRPlotHierarchy } from "./src/ir-plot";
-export { MelodyHierarchy, MelodyVM } from "./src/melody";
+export { MelodyHierarchy } from "./src/melody";
 export { GravityHierarchy } from "./src/gravity";
