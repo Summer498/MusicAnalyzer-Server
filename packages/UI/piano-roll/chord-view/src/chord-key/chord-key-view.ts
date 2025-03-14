0e41be92576ec710fb6cdaad66e3caedaee62cba
Summer498 <subore02@gmail.com>
2025-03-14 22:38:14 +0900

    separate responsibility (calculate/setAttribute)

--- packages/UI/piano-roll/chord-view/src/chord-key/chord-key-view.ts
+++ packages/UI/piano-roll/chord-view/src/chord-key/chord-key-view.ts
@@ -1,15 +1,11 @@
 import { _Scale } from "@music-analyzer/tonal-objects";
-import { NoteSize, PianoRollHeight } from "@music-analyzer/view-parameters";
 import { fifthToColor } from "@music-analyzer/color";
 import { MVVM_View } from "@music-analyzer/view";
 import { oneLetterKey } from "../shorten";
 import { ChordKeyModel } from "./chord-key-model";
-import { chord_name_margin, chord_text_em, chord_text_size } from "../chord-view-params";
-
-const scaled = (e: number) => e * NoteSize.get();
+import { chord_text_em } from "../chord-view-params";
 
 export class ChordKeyView extends MVVM_View<ChordKeyModel, "text"> {
-  readonly y: number;
   constructor(model: ChordKeyModel,) {
     super(model, "text");
     this.svg.textContent = oneLetterKey(_Scale.get(this.model.scale)) + ': ';
@@ -18,14 +14,8 @@ export class ChordKeyView extends MVVM_View<ChordKeyModel, "text"> {
     this.svg.style.fontSize = `${chord_text_em}em`;
     this.svg.style.textAnchor = "end";
     this.svg.style.fill = fifthToColor(this.model.tonic, 1, 0.75) || "rgb(0, 0, 0)";
-    this.y = PianoRollHeight.get() + chord_text_size * 2 + chord_name_margin;
-    this.updateX();
-    this.updateY();
   }
-  updateX() { this.svg.setAttribute("x", String(scaled(this.model.time.begin))); }
-  updateY() { this.svg.setAttribute("y", String(this.y)); }
-  onWindowResized() {
-    this.updateX();
-  }
+  updateX(x:number) { this.svg.setAttribute("x", String(x)); }
+  updateY(y:number) { this.svg.setAttribute("y", String(y)); }
 }
 
