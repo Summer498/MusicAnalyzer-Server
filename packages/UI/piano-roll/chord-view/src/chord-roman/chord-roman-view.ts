340ca6ea1a27228212fe4f3c85a7418661d2943a
Summer498 <subore02@gmail.com>
2025-03-14 22:54:50 +0900

    separate responsibility (calculate/setAttribute)

--- packages/UI/piano-roll/chord-view/src/chord-roman/chord-roman-view.ts
+++ packages/UI/piano-roll/chord-view/src/chord-roman/chord-roman-view.ts
@@ -1,14 +1,10 @@
 import { fifthToColor } from "@music-analyzer/color";
-import { NoteSize, PianoRollHeight } from "@music-analyzer/view-parameters";
 import { MVVM_View } from "@music-analyzer/view";
-import { chord_name_margin, chord_text_em, chord_text_size } from "../chord-view-params";
 import { shortenChord } from "../shorten";
 import { ChordRomanModel } from "./chord-roman-model";
-
-const scaled = (e: number) => e * NoteSize.get();
+import { chord_text_em } from "../chord-view-params";
 
 export class ChordRomanView extends MVVM_View<ChordRomanModel, "text"> {
-  readonly y: number;
   constructor(model: ChordRomanModel) {
     super(model, "text");
     this.svg.textContent = shortenChord(this.model.roman);
@@ -16,13 +12,7 @@ export class ChordRomanView extends MVVM_View<ChordRomanModel, "text"> {
     this.svg.style.fontFamily = "Times New Roman";
     this.svg.style.fontSize = `${chord_text_em}em`;
     this.svg.style.fill = fifthToColor(this.model.tonic, 1, 0.75) || "rgb(0, 0, 0)";
-    this.y = PianoRollHeight.get() + chord_text_size * 2 + chord_name_margin;
-    this.updateX();
-    this.updateY();
   }
-  updateX() { this.svg.setAttribute("x", String(scaled(this.model.time.begin))); }
-  updateY() { this.svg.setAttribute("y", String(this.y)); }
-  onWindowResized() {
-    this.updateX();
-  }
+  updateX(x: number) { this.svg.setAttribute("x", String(x)); }
+  updateY(y: number) { this.svg.setAttribute("y", String(y)); }
 }
