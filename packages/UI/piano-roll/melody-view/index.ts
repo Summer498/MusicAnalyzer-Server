54e0dab81a1d2080d62450904133317ceaad2bb5
Summer498 <subore02@gmail.com>
2025-03-14 16:00:39 +0900

    unify export

--- packages/UI/piano-roll/melody-view/index.ts
+++ packages/UI/piano-roll/melody-view/index.ts
@@ -2,6 +2,5 @@ export { DMelodyGroup } from "./src/d-melody";
 export { ReductionHierarchy } from "./src/reduction";
 export { IRSymbolHierarchy } from "./src/ir-symbol";
 export { IRPlotHierarchy } from "./src/ir-plot";
-export { MelodyHierarchy } from "./src/melody";
-export { MelodyVM } from "./src/melody";
+export { MelodyHierarchy, MelodyVM } from "./src/melody";
 export { GravityHierarchy } from "./src/gravity";
