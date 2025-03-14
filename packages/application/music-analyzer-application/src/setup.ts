048e43d246093f288db5f87066daddfefe481283
Summer498 <subore02@gmail.com>
2025-03-14 13:34:26 +0900

    replace generator to class constructor

--- packages/application/music-analyzer-application/src/setup.ts
+++ packages/application/music-analyzer-application/src/setup.ts
@@ -1,16 +1,16 @@
 import { AnalyzedMusicData, getMusicAnalyzerWindow } from "./MusicAnalyzerWindow";
-import { initializeApplication } from "./initialize-application";
 import { setupUI } from "./setup-ui";
 import { EventLoop } from "./EventLoop";
 import { HTMLsContainer } from "./HTMLs-container";
 import { TitleInfo } from "./tune-info";
+import { AnalyzedDataContainer } from "./analyzed-data-container";
 
 export const setup = (
   window: Window,
   html: HTMLsContainer,
   title: TitleInfo,
 ) => (raw_analyzed_data: AnalyzedMusicData) => {
-  const app_manager = setupUI(title, html, initializeApplication(raw_analyzed_data));
+  const app_manager = setupUI(title, html, new AnalyzedDataContainer(raw_analyzed_data));
   new EventLoop(app_manager.audio, html.audio_player).update();
   getMusicAnalyzerWindow(window, raw_analyzed_data).onresize = _ => app_manager.window.onUpdate();
   app_manager.window.onUpdate();
