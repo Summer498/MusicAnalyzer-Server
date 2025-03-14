36383eb5abc03ca89fd31d65c2f5f799bc334c83
Summer498 <subore02@gmail.com>
2025-03-14 15:46:48 +0900

    remove unused generator

--- packages/UI/piano-roll/melody-view/src/ir-plot/ir-plot.ts
+++ packages/UI/piano-roll/melody-view/src/ir-plot/ir-plot.ts
@@ -1,15 +1,6 @@
 import { AudioReflectable, WindowReflectable } from "@music-analyzer/view";
 import { IRPlotHierarchy } from "./ir-plot-hierarchy";
 
-export const getIRPlot = (g: IRPlotHierarchy) => {
-  const ir_plot = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  ir_plot.appendChild(g.svg);
-  ir_plot.id = "IR-plot";
-  ir_plot.setAttribute("width", String(g.width));
-  ir_plot.setAttribute("height", String(g.height));
-  return ir_plot;
-};
-
 export class IRPlot implements AudioReflectable, WindowReflectable {
   readonly svg: SVGSVGElement;
   readonly children: [IRPlotHierarchy];
