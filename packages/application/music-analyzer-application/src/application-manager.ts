29160d64b0eb048f6fc48800df741b78a888a4da
Summer498 <subore02@gmail.com>
2025-03-16 08:34:28 +0900

    use children from children member

--- packages/application/music-analyzer-application/src/application-manager.ts
+++ packages/application/music-analyzer-application/src/application-manager.ts
@@ -18,26 +18,17 @@ class Controllers {
       readonly melody_color: MelodyColorController,
     }
   ) {
-    const {
-      d_melody,
-      hierarchy,
-      time_range,
-      gravity,
-      melody_beep,
-      melody_color
-    } = this.children
-
     this.div = document.createElement("div");
     this.div.id = "controllers";
     this.div.style = "margin-top:20px";
-    this.div.appendChild(d_melody.view);
-    this.div.appendChild(hierarchy.view);
-    this.div.appendChild(time_range.view);
+    this.div.appendChild(this.children.d_melody.view);
+    this.div.appendChild(this.children.hierarchy.view);
+    this.div.appendChild(this.children.time_range.view);
     if (!NO_CHORD) {
-      this.div.appendChild(gravity.view);
+      this.div.appendChild(this.children.gravity.view);
     }
-    this.div.appendChild(melody_beep.view);
-    this.div.appendChild(melody_color.view);  // NOTE: 色選択は未実装なので消しておく
+    this.div.appendChild(this.children.melody_beep.view);
+    this.div.appendChild(this.children.melody_color.view);  // NOTE: 色選択は未実装なので消しておく
   }
 }
 
