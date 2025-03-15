f25a64bdba03879a813311469bee9994244c0154
Summer498 <subore02@gmail.com>
2025-03-15 23:15:51 +0900

    use window

--- packages/UI/spectrogram/src/audio-analyzer/get-fft.ts
+++ packages/UI/spectrogram/src/audio-analyzer/get-fft.ts
@@ -1,12 +1,23 @@
 import { Complex, fft } from "@music-analyzer/math";
-import { getByteTimeDomainData } from "./get-data-on-buffer";
+import { getByteTimeDomainData, getFloatTimeDomainData } from "./get-data-on-buffer";
+
+const blackManWindow = (x: Float32Array<ArrayBuffer>) => {
+  const c = 0.16;
+  const a = [(1 - c) / 2, 1 / 2, c / 2]
+  const N = x.length;
+return  x.map((e, i) =>
+  (a[0]
+    - a[1] * Math.cos(2 * Math.PI * i / N
+      + a[2] * Math.cos(4 * Math.PI * i / N)
+    ) * e))
+}
 
 export const getFFT = (analyser: AnalyserNode) => {
-  const buff = getByteTimeDomainData(analyser);
+  const buff = blackManWindow(getFloatTimeDomainData((analyser)));
   const amplitude: number[] = []
   for (let i = 0; i < buff.length; i++) {
     amplitude.push(buff[i])
   }
-  
+
   return fft(amplitude.map(e => new Complex(e, 0)))
 }
