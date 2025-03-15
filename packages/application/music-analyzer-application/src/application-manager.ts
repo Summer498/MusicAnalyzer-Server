8f1dcb037c5d0979723a20d68d89e22f45b001f3
Summer498 <subore02@gmail.com>
2025-03-16 08:08:39 +0900

    aunify controller class file

--- packages/application/music-analyzer-application/src/application-manager.ts
+++ packages/application/music-analyzer-application/src/application-manager.ts
@@ -1,9 +1,58 @@
 import { AudioReflectableRegistry, WindowReflectableRegistry } from "@music-analyzer/view";
 import { BeatElements, ChordElements, MelodyElements, MusicStructureElements } from "./piano-roll";
-import { Controllers } from "./setup-ui";
 import { AnalyzedDataContainer } from "./containers";
 import { ChordGravityMediator, DMelodyMediator, HierarchyLevelMediator, MelodyBeepMediator, MelodyVolumeMediator, ScaleGravityMediator, TimeRangeMediator } from "./UIMediators";
 import { ColorChangeMediator } from "./UIMediators/color-change-mediator";
+import { MelodyColorController, DMelodyController, GravityController, HierarchyLevelController, MelodyBeepController, TimeRangeController } from "@music-analyzer/controllers";
+
+class ControllerUIs {
+  constructor(
+    readonly gravity: GravityController,
+    readonly d_melody: DMelodyController,
+    readonly melody_beep: MelodyBeepController,
+    readonly melody_color: MelodyColorController,
+    readonly hierarchy: HierarchyLevelController,
+    readonly time_range: TimeRangeController,
+  ) {  }
+}
+
+class Controllers {
+  readonly div: HTMLDivElement;
+  readonly children: ControllerUIs;
+  constructor(
+    NO_CHORD: boolean
+  ) {
+    this.children = new ControllerUIs(
+      new GravityController(),
+      new DMelodyController(),
+      new MelodyBeepController(),
+      new MelodyColorController(),
+      new HierarchyLevelController(),
+      new TimeRangeController(),
+    );
+
+    const {
+      d_melody,
+      hierarchy,
+      time_range,
+      gravity,
+      melody_beep,
+      melody_color
+    } = this.children;
+
+    this.div = document.createElement("div");
+    this.div.id = "controllers";
+    this.div.style = "margin-top:20px";
+    this.div.appendChild(d_melody.view);
+    this.div.appendChild(hierarchy.view);
+    this.div.appendChild(time_range.view);
+    if (!NO_CHORD) {
+      this.div.appendChild(gravity.view);
+    }
+    this.div.appendChild(melody_beep.view);
+    this.div.appendChild(melody_color.view);  // NOTE: 色選択は未実装なので消しておく
+  }
+}
 
 export class ApplicationManager {
   readonly NO_CHORD = false;  // コード関連のものを表示しない
--- packages/application/music-analyzer-application/src/setup-ui/index.ts
+++ packages/application/music-analyzer-application/src/setup-ui/index.ts
@@ -1,3 +1,2 @@
 export { AnalysisView } from "./setup-analysis";
-export { Controllers } from "./setup-controllers";
 export { setupUI } from "./setup-ui";
