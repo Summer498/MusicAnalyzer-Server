0b2e49a80266bdd452943f5fce9d220678cffa7d
Summer498 <subore02@gmail.com>
2025-03-16 09:03:14 +0900

    remove unused class

--- packages/application/music-analyzer-application/src/UIMediators/switcher-mediator/switcher-mediator.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@music-analyzer/controllers";
-import { ControllerMediator } from "../controller-mediator";
-
-abstract class SwitcherMediator<Subscriber> extends ControllerMediator<Subscriber> {
-  constructor(controllers: Controller[], subscribers: Subscriber[]) {
-    super(controllers, subscribers);
-  }
-}
