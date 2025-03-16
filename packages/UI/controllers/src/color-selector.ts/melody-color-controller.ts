f932b81c8967e8744ecf7b8643abc6807f323662
Summer498 <subore02@gmail.com>
2025-03-16 12:50:35 +0900

    move init into controller

--- packages/UI/controllers/src/color-selector.ts/melody-color-controller.ts
+++ packages/UI/controllers/src/color-selector.ts/melody-color-controller.ts
@@ -1,7 +1,8 @@
-import { Dyad, Monad, Null_ad, Triad } from "@music-analyzer/irm";
 import { MelodyColorSelector } from "./melody-color-selector";
+import { get_color_of_implication_realization, get_color_of_Narmour_concept, get_color_on_digital_intervallic_scale, get_color_on_digital_parametric_scale, get_color_on_intervallic_angle, get_color_on_parametric_scale, get_color_on_registral_scale, Triad } from "@music-analyzer/irm";
+import { IRM_ColorNameIDs } from "./IRM-color-names";
 
-type hasArchetype = { archetype: Triad | Dyad | Monad | Null_ad }
+type hasArchetype = { archetype: Triad }
 type ColorChangeSubscriber = {
   setColor: (getColor: (e: hasArchetype) => string) => void
   updateColor: () => void
@@ -21,9 +22,29 @@ export class MelodyColorController {
   register(...subscribers: ColorChangeSubscriber[]) {
     this.subscribers.push(...subscribers);
   }
-  _update(getColor: (e: hasArchetype) => string) {
+  update(getColor: (e: hasArchetype) => string) {
     return () => {
       this.subscribers.forEach(e => e.setColor(getColor));
     };
   }
+  init() {
+    this.selector.children.forEach(s =>
+      s.input.addEventListener("input",
+        this.update.bind(this)((e: hasArchetype) => this.mapColor(s.id)(e.archetype)
+        )
+      )
+    );
+    this.update.bind(this)((e: hasArchetype) => this.mapColor("Narmour_concept")(e.archetype))();
+  };
+  mapColor(id: IRM_ColorNameIDs) {
+    switch (id) {
+      case "Narmour_concept": return get_color_of_Narmour_concept;
+      case "analog_parametric_scale": return get_color_on_parametric_scale;
+      case "implication_realization": return get_color_of_implication_realization;
+      case "digital_parametric_scale": return get_color_on_digital_parametric_scale;
+      case "digital_intervallic_scale": return get_color_on_digital_intervallic_scale;
+      case "intervallic_angle": return get_color_on_intervallic_angle;
+      case "registral_scale": return get_color_on_registral_scale;
+    }
+  };
 }
