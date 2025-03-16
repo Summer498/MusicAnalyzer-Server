1c85a4ed4673489c5ab5b502f2230a6eec821407
Summer498 <subore02@gmail.com>
2025-03-17 08:50:06 +0900

    introduce IRM_ColorSelector

--- packages/UI/controllers/src/color-selector.ts/melody-color-selector.ts
+++ packages/UI/controllers/src/color-selector.ts/melody-color-selector.ts
@@ -1,6 +1,5 @@
-import { ColorSelector } from "./color-selector";
 import { get_color_of_implication_realization, get_color_of_Narmour_concept, get_color_on_digital_intervallic_scale, get_color_on_digital_parametric_scale, get_color_on_intervallic_angle, get_color_on_parametric_scale, get_color_on_registral_scale, Triad } from "@music-analyzer/irm";
-import { IRM_ColorNameIDs } from "./IRM-color-names";
+import { IRM_ColorSelector } from "./irm-color-selector";
 
 export interface hasArchetype { archetype: Triad }
 export interface ColorChangeSubscriber {
@@ -10,16 +9,16 @@ export interface ColorChangeSubscriber {
 
 export class MelodyColorSelector {
   readonly body: HTMLSpanElement;
-  readonly children: ColorSelector<IRM_ColorNameIDs>[];
+  readonly children: IRM_ColorSelector[];
   constructor() {
     this.children = [
-      new ColorSelector("Narmour_concept", "Narmour concept color",),
-      new ColorSelector("implication_realization", "implication realization",),
-      new ColorSelector("digital_parametric_scale", "digital parametric scale color",),
-      new ColorSelector("digital_intervallic_scale", "digital intervallic scale color",),
-      new ColorSelector("registral_scale", "registral scale color",),
-      new ColorSelector("intervallic_angle", "intervallic angle color",),
-      new ColorSelector("analog_parametric_scale", "analog parametric scale color",),
+      new IRM_ColorSelector("Narmour_concept", "Narmour concept color", get_color_of_Narmour_concept),
+      new IRM_ColorSelector("implication_realization", "implication realization", get_color_of_implication_realization),
+      new IRM_ColorSelector("digital_parametric_scale", "digital parametric scale color", get_color_on_digital_parametric_scale),
+      new IRM_ColorSelector("digital_intervallic_scale", "digital intervallic scale color", get_color_on_digital_intervallic_scale),
+      new IRM_ColorSelector("registral_scale", "registral scale color", get_color_on_registral_scale),
+      new IRM_ColorSelector("intervallic_angle", "intervallic angle color", get_color_on_intervallic_angle),
+      new IRM_ColorSelector("analog_parametric_scale", "analog parametric scale color", get_color_on_parametric_scale),
     ];
     this.children.forEach(e => { e.input.name = "melody-color-selector"; });
 
@@ -29,38 +28,6 @@ export class MelodyColorSelector {
     this.body = document.createElement("div");
     this.body.id = "melody_color_selector";
     this.children.forEach(e => this.body.appendChild(e.body));
-    this.init()
   }
-
-
-  readonly subscribers: ColorChangeSubscriber[] = [];
-  register(...subscribers: ColorChangeSubscriber[]) {
-    this.subscribers.push(...subscribers);
-    this.update.bind(this)((e: hasArchetype) => this.mapColor("Narmour_concept")(e.archetype))();
-  }
-  update(getColor: (e: hasArchetype) => string) {
-    return () => {
-      this.subscribers.forEach(e => e.setColor(getColor));
-    };
-  }
-  init() {
-    this.children.forEach(s =>
-      s.input.addEventListener("input",
-        this.update.bind(this)((e: hasArchetype) => this.mapColor(s.id)(e.archetype)
-        )
-      )
-    );
-    this.update.bind(this)((e: hasArchetype) => this.mapColor("Narmour_concept")(e.archetype))();
-  };
-  mapColor(id: IRM_ColorNameIDs) {
-    switch (id) {
-      case "Narmour_concept": return get_color_of_Narmour_concept;
-      case "analog_parametric_scale": return get_color_on_parametric_scale;
-      case "implication_realization": return get_color_of_implication_realization;
-      case "digital_parametric_scale": return get_color_on_digital_parametric_scale;
-      case "digital_intervallic_scale": return get_color_on_digital_intervallic_scale;
-      case "intervallic_angle": return get_color_on_intervallic_angle;
-      case "registral_scale": return get_color_on_registral_scale;
-    }
-  };
+  register(...subscribers: ColorChangeSubscriber[]) { this.children.forEach(e => e.register(...subscribers)) }
 }
