14fc8726052282963bd55d4caec28baf85516653
Summer498 <subore02@gmail.com>
2025-03-16 09:10:43 +0900

    separate controller-view

--- packages/UI/controllers/src/controller.ts
+++ packages/UI/controllers/src/controller.ts
@@ -1,24 +1,17 @@
+import { ControllerView } from "./controller-view";
+
 type HTMLInputElementType = "button" | "checkbox" | "color" | "date" | "datetime-local" | "email" | "file" | "hidden" | "image" | "month" | "number" | "password" | "radio" | "range" | "reset" | "search" | "submit" | "tel" | "text" | "time" | "url" | "week";
 
 export abstract class Controller {
   readonly body: HTMLSpanElement;
   readonly input: HTMLInputElement;
-  readonly label: HTMLLabelElement;
-
   constructor(
     type: HTMLInputElementType,
     id: string,
     label: string,
   ) {
-    this.input = document.createElement("input");
-    this.input.type = type;
-    this.input.id = id;
-    this.input.name = id;
-    this.label = document.createElement("label");
-    this.label.textContent = label;
-    this.label.htmlFor = this.input.id;
-    this.body = document.createElement("span");
-    this.body.appendChild(this.label);
-    this.body.appendChild(this.input);
+    const e = new ControllerView(type, id, label);
+    this.body = e.body;
+    this.input = e.input
   }
 }
