987f05b75383b0c766a466fb51e7c8e05f540966
Summer498 <subore02@gmail.com>
2025-03-16 19:27:05 +0900

    use array fft

--- packages/UI/spectrogram/src/fft-viewer.ts
+++ packages/UI/spectrogram/src/fft-viewer.ts
@@ -22,19 +22,41 @@ export class FFTViewer implements AudioReflectable {
     const freqData = this.analyser.getFFT();
     console.log("freqData")
     console.log(freqData)
-    const N = freqData.length / 2;
+    const N = freqData[0].length / 2;
     const width = this.svg.clientWidth;
     const height = this.svg.clientHeight;
     let pathData = "";
 
     const abs = <T extends number>(e: Complex<T>) => Math.sqrt(e.re * e.re + e.im * e.im)
+    const absV = (...c: [Float32Array<ArrayBuffer>, Float32Array<ArrayBuffer>]) =>
+      c[0].map((e, i) => Math.sqrt(e * e + c[1][i] * c[1][i]))
+
+    console.log("freqData[1]")
+    console.log(
+       absV(...freqData)
+    )
+
+    this.path.setAttribute("d", "M" +
+//      freqData.map(e => abs(e))
+        [...Array.from(absV(...freqData))]
+        .map((e, i) => {
+          if (isNaN(e * 0)) { return ``; }
+          const x = i / (N - 1) * width;
+          const y = (1 - Math.log2(1+e)) * height;
+//          const y = (1 - Math.log2(1 + e) / 8) * height;
+          return `L ${x},${y}`;
+        })
+        .join()
+        .slice(1))
+
+    /*
     for (let i = 0; i < N; i++) {
       if (isNaN(freqData[i].re * 0)) { continue; }
       const x = i / (N - 1) * width;
       const y = (1 - Math.log2(1 + abs(freqData[i])) / 8) * height;
-      // const y = (1 - Math.log2(1 + freqData[i]) / 8) * height;
       pathData += `L ${x},${y}`;
     }
     this.path.setAttribute("d", "M" + pathData.slice(1));
+    */
   }
 }
