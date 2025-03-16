114d6162e9dfbd9510c34d005666bccc99e5c2a7
Summer498 <subore02@gmail.com>
2025-03-17 08:48:54 +0900

    colorselector extends controller

--- packages/UI/controllers/src/color-selector.ts/color-selector.ts
+++ packages/UI/controllers/src/color-selector.ts/color-selector.ts
@@ -1,24 +1,10 @@
-export class ColorSelector<ID extends string> {
-  input: HTMLInputElement;
-  label: HTMLLabelElement;
-  body: HTMLSpanElement;
+import { Controller } from "../controller";
+
+export abstract class ColorSelector<T> extends Controller<T> {
   constructor(
-    readonly id: ID,
+    readonly id: string,
     text: string
   ) {
-    this.input = document.createElement("input");
-    this.input.id = this.id;
-    this.input.type = "radio";
-    this.input.value = this.id;
-
-    this.label = document.createElement("label");
-    this.label.textContent = text;
-    this.label.htmlFor = this.input.id;
-    this.label.style.whiteSpace = "nowrap";  //     white-space: nowrap;
-
-    this.body = document.createElement("span");
-    this.body.style.whiteSpace = "nowrap";   //     white-space: nowrap;
-    this.body.appendChild(this.input);
-    this.body.appendChild(this.label);
+    super("radio", id, text);
   };
 }
