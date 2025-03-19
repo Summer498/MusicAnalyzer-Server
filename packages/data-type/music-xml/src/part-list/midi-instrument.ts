38197381b69f1fb6601e94cc097f0ab9ed992437
Summer498 <subore02@gmail.com>
2025-03-19 19:13:49 +0900

    format: newline before extends

--- packages/data-type/music-xml/src/part-list/midi-instrument.ts
+++ packages/data-type/music-xml/src/part-list/midi-instrument.ts
@@ -1,6 +1,7 @@
 import { HasID } from "../part/has-id";
 
-export interface MidiInstrument extends HasID {
+export interface MidiInstrument 
+extends HasID {
   readonly "midi-channel": number,
   readonly "midi-program": number,
   readonly volume: number,
