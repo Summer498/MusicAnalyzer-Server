6bf921a21ca1461768c5c940928a8a3bb1ffbcda
Summer498 <subore02@gmail.com>
2025-03-19 19:22:37 +0900

    format: indent "extends"

--- packages/data-type/music-xml/src/part/part.ts
+++ packages/data-type/music-xml/src/part/part.ts
@@ -3,6 +3,6 @@ import { HasID } from "./has-id";
 import { Measure } from "../measure";
 
 export interface Part 
-extends HasID {
+  extends HasID {
   readonly measure: (Measure | ExtendedMeasure)[]
 }
