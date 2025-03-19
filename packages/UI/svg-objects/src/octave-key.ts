4a0198db4334a1655ba71f215c7bb8d0bcb32bcd
Summer498 <subore02@gmail.com>
2025-03-19 20:37:13 +0900

    separate files by class

---
+++ packages/UI/svg-objects/src/octave-key.ts
@@ -0,0 +1,24 @@
+import { WindowReflectable } from "@music-analyzer/view";
+import { OctaveBlackKey } from "./octave-black-key";
+import { OctaveWhiteKey } from "./octave-white-key";
+
+export class OctaveKey 
+  implements WindowReflectable {
+  readonly svg: SVGGElement;
+  readonly white_keys: OctaveWhiteKey;
+  readonly black_keys: OctaveBlackKey;
+  constructor(
+    readonly oct: number,
+  ) {
+    this.white_keys = new OctaveWhiteKey(oct);
+    this.black_keys = new OctaveBlackKey(oct);
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    this.svg.id = `octave-key-${oct}`;
+    this.svg.appendChild(this.white_keys.svg);
+    this.svg.appendChild(this.black_keys.svg);
+  }
+  onWindowResized() {
+    this.white_keys.onWindowResized();
+    this.black_keys.onWindowResized();
+  }
+}
