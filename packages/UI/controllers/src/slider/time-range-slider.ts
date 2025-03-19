fe61984561af4a101b1ffc8635a7c2a719c24975
Summer498 <subore02@gmail.com>
2025-03-19 19:15:00 +0900

    format: newline before extends

--- packages/UI/controllers/src/slider/time-range-slider.ts
+++ packages/UI/controllers/src/slider/time-range-slider.ts
@@ -1,7 +1,8 @@
 import { PianoRollRatio } from "@music-analyzer/view-parameters";
 import { Slider } from "./abstract-slider";
 
-class TimeRangeSlider extends Slider<TimeRangeSubscriber> {
+class TimeRangeSlider 
+extends Slider<TimeRangeSubscriber> {
   constructor() {
     super("time_range_slider", "Time Range", 1, 10, 0.1, 10);
   };
