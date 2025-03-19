4cdd4c144f4c42f28f39f167481619cfcd5cb318
Summer498 <subore02@gmail.com>
2025-03-19 19:13:37 +0900

    format: newline before extends

--- packages/data-type/music-xml/src/part/part.ts
+++ packages/data-type/music-xml/src/part/part.ts
@@ -2,6 +2,7 @@ import { ExtendedMeasure } from "../measure/extended-measure";
 import { HasID } from "./has-id";
 import { Measure } from "../measure";
 
-export interface Part extends HasID {
+export interface Part 
+extends HasID {
   readonly measure: (Measure | ExtendedMeasure)[]
 }
