593334314f1b502dabc07e570fe5cf80603528da
Summer498 <subore02@gmail.com>
2025-03-19 19:25:36 +0900

    format: indent "extends"

--- packages/UI/view-parameters/src/piano-roll/piano-roll-constants.ts
+++ packages/UI/view-parameters/src/piano-roll/piano-roll-constants.ts
@@ -6,28 +6,28 @@ export const size = 2;
 export const octave_height = size * 84;  // 7 白鍵と 12 半音をきれいに描画するには 7 * 12 の倍数が良い
 
 export class WhiteKeyPrm 
-extends RectParameters {
+  extends RectParameters {
   static readonly fill = "rgb(255, 255, 255)";
   static readonly stroke = "rgb(0, 0, 0)";
   static readonly width = 36;
   static readonly height = octave_height / 7;
 };
 export class BlackKeyPrm 
-extends RectParameters {
+  extends RectParameters {
   static readonly fill = "rgb(64, 64, 64)";
   static readonly stroke = "rgb(0, 0, 0)";
   static readonly width = WhiteKeyPrm.width * 2 / 3;
   static readonly height = octave_height / 12;
 }
 export class WhiteBGsPrm 
-extends RectParameters {
+  extends RectParameters {
   static readonly fill = "rgb(242, 242, 242)";
   static readonly stroke = "rgb(0, 0, 0)";
   static readonly width = PianoRollWidth.get();
   static readonly height = octave_height / 12;
 }
 export class BlackBGsPrm 
-extends RectParameters {
+  extends RectParameters {
   static readonly fill = "rgb(192, 192, 192)";
   static readonly stroke = "rgb(0, 0, 0)";
   static readonly width = PianoRollWidth.get();
