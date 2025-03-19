2d6f25e5c925184956d158f6f8ab52af4ed5703c
Summer498 <subore02@gmail.com>
2025-03-19 18:46:28 +0900

    format: newline before implements

--- packages/cognitive-theory-of-music/gttm/src/analysis-result/TSR/time-span-tree.ts
+++ packages/cognitive-theory-of-music/gttm/src/analysis-result/TSR/time-span-tree.ts
@@ -4,7 +4,8 @@ export interface ITimeSpanTree {
   readonly ts: ITimeSpan
 }
 
-export class TimeSpanTree implements ITimeSpanTree {
+export class TimeSpanTree 
+implements ITimeSpanTree {
   readonly ts: TimeSpan;
   constructor(ts_tree: ITimeSpanTree) {
     this.ts = new TimeSpan(ts_tree.ts);
