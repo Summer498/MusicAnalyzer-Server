8f2e519fab0554295ad1169038f86a17a975f653
Summer498 <subore02@gmail.com>
2025-03-19 18:48:37 +0900

    format: newline before implements

--- packages/UI/view/src/collection-hierarchy.ts
+++ packages/UI/view/src/collection-hierarchy.ts
@@ -4,7 +4,8 @@ import { AudioReflectable, WindowReflectable } from "./reflectable";
 
 export abstract class CollectionHierarchy<L extends I_CollectionLayer & WindowReflectable>
   extends MVVM_Collection<L>
-  implements I_MVVM_Collection, AudioReflectable {
+  
+implements I_MVVM_Collection, AudioReflectable {
   protected _show: L[];
   get show() { return this._show; }
   constructor(id: string, readonly children: L[]) {
