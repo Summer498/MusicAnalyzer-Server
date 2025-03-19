0af3a2cdac5cf6c4c8617c5ee1dd2a0820ccb008
Summer498 <subore02@gmail.com>
2025-03-19 19:12:53 +0900

    format: newline before extends

--- packages/cognitive-theory-of-music/gttm/src/analysis-result/MTR/applied.ts
+++ packages/cognitive-theory-of-music/gttm/src/analysis-result/MTR/applied.ts
@@ -1,6 +1,7 @@
 import { MPR } from "./metric-preference-rule";
 import { Applied as BApplied } from "../common";
 
-export interface Applied extends BApplied<MPR> {
+export interface Applied 
+extends BApplied<MPR> {
   readonly level: number,
 }
