4757f0454f4e0128405ab906c4fe3c3e48ffe5f0
Summer498 <subore02@gmail.com>
2025-03-20 12:08:29 +0900

    format

--- packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-hierarchy.ts
+++ packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-hierarchy.ts
@@ -7,10 +7,7 @@ export class IRSymbolHierarchy
   extends CollectionHierarchy<IRSymbolLayer> {
   constructor(
     hierarchical_melodies: TimeAndAnalyzedMelody[][],
-    controllers: [
-      HierarchyLevelController,
-      MelodyColorController,
-    ]
+    controllers: [HierarchyLevelController, MelodyColorController]
   ) {
     super("implication-realization archetype", hierarchical_melodies.map((e, l) => new IRSymbolLayer(e, l, [controllers[1]])));
     controllers[0].register(this);
