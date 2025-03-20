d312d999eb3e0896f5e46119405e63b20bb50e43
Summer498 <subore02@gmail.com>
2025-03-20 14:43:43 +0900

    open analysis, use beat, chord, and melody directly

--- packages/application/music-analyzer-application/src/setup-ui/analysis-view.ts
+++ packages/application/music-analyzer-application/src/setup-ui/analysis-view.ts
@@ -8,20 +8,20 @@ export class AnalysisView
   constructor(
     analysis: MusicStructureElements,
   ) {
-    const e = analysis;
-    this.children = [e.beat, e.chord, e.melody];
+    const { beat, chord, melody } = analysis;
+    this.children = [beat, chord, melody];
     this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
     // this.svg.appendChild(beat.beat_bars.svg);
-    this.svg.appendChild(e.chord.chord_notes.svg);
-    this.svg.appendChild(e.chord.chord_names.svg);
-    this.svg.appendChild(e.chord.chord_romans.svg);
-    this.svg.appendChild(e.chord.chord_keys.svg);
-    this.svg.appendChild(e.melody.d_melody_collection.svg);
-    this.svg.appendChild(e.melody.melody_hierarchy.svg);
-    this.svg.appendChild(e.melody.ir_hierarchy.svg);
-    this.svg.appendChild(e.melody.chord_gravities.svg);
-    this.svg.appendChild(e.melody.scale_gravities.svg);
-    this.svg.appendChild(e.melody.time_span_tree.svg);
+    this.svg.appendChild(chord.chord_notes.svg);
+    this.svg.appendChild(chord.chord_names.svg);
+    this.svg.appendChild(chord.chord_romans.svg);
+    this.svg.appendChild(chord.chord_keys.svg);
+    this.svg.appendChild(melody.d_melody_collection.svg);
+    this.svg.appendChild(melody.melody_hierarchy.svg);
+    this.svg.appendChild(melody.ir_hierarchy.svg);
+    this.svg.appendChild(melody.chord_gravities.svg);
+    this.svg.appendChild(melody.scale_gravities.svg);
+    this.svg.appendChild(melody.time_span_tree.svg);
   }
   onAudioUpdate() { this.children.forEach(e => e.onAudioUpdate()); }
   onWindowResized() { this.children.forEach(e => e.onWindowResized()); }
