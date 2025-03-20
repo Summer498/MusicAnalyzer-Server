1724fd2561c9ce7c15951a256a717e4ec24cfde8
Summer498 <subore02@gmail.com>
2025-03-20 11:46:09 +0900

    format

--- packages/UI/view/src/collection-layer.ts
+++ packages/UI/view/src/collection-layer.ts
@@ -1,13 +1,11 @@
 import { I_ReflectableTimeAndMVCControllerCollection, I_TimeAndVM, ReflectableTimeAndMVCControllerCollection } from "./svg-collection";
 
 export interface I_CollectionLayer
-  
   extends I_ReflectableTimeAndMVCControllerCollection {
   readonly layer: number
 }
 
 export class CollectionLayer<VM extends I_TimeAndVM>
-  
   extends ReflectableTimeAndMVCControllerCollection<VM>
   
   implements I_CollectionLayer {
