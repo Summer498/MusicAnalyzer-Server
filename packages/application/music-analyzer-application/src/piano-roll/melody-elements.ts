6b49f40ac3edd8e3140fb3c745024da9cf65be7d
Summer498 <subore02@gmail.com>
2025-03-20 13:38:42 +0900

    append registry in argument

--- packages/application/music-analyzer-application/src/piano-roll/melody-elements.ts
+++ packages/application/music-analyzer-application/src/piano-roll/melody-elements.ts
@@ -1,6 +1,6 @@
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { GravityHierarchy, DMelodySeries, IRSymbolHierarchy, MelodyHierarchy, ReductionHierarchy, IRPlot } from "@music-analyzer/melody-view";
-import { AudioReflectable, WindowReflectable } from "@music-analyzer/view";
+import { AudioReflectable, AudioReflectableRegistry, WindowReflectable, WindowReflectableRegistry } from "@music-analyzer/view";
 import { Controllers } from "../controllers";
 
 export class MelodyElements
@@ -17,16 +17,18 @@ export class MelodyElements
     hierarchical_melody: TimeAndAnalyzedMelody[][],
     d_melodies: TimeAndAnalyzedMelody[],
     controllers: Controllers,
+    publisher: [AudioReflectableRegistry, WindowReflectableRegistry]
   ) {
     const { d_melody, hierarchy, time_range, gravity, melody_color, melody_beep } = controllers;
+    const [audio, window] = publisher;
 
-    this.d_melody_collection = new DMelodySeries(d_melodies, [d_melody]);
-    this.melody_hierarchy = new MelodyHierarchy(hierarchical_melody, [hierarchy, melody_color, melody_beep]);
-    this.ir_hierarchy = new IRSymbolHierarchy(hierarchical_melody, [hierarchy, melody_color]);
-    this.ir_plot = new IRPlot(hierarchical_melody, [hierarchy, melody_color]);
-    this.chord_gravities = new GravityHierarchy("chord_gravity", hierarchical_melody, [gravity.chord_checkbox, hierarchy]);
-    this.scale_gravities = new GravityHierarchy("scale_gravity", hierarchical_melody, [gravity.scale_checkbox, hierarchy]);
-    this.time_span_tree = new ReductionHierarchy(hierarchical_melody, [hierarchy, melody_color]);
+    this.d_melody_collection = new DMelodySeries(d_melodies, [d_melody, audio, window]);
+    this.melody_hierarchy = new MelodyHierarchy(hierarchical_melody, [hierarchy, melody_color, melody_beep, audio, window]);
+    this.ir_hierarchy = new IRSymbolHierarchy(hierarchical_melody, [hierarchy, melody_color, audio, window]);
+    this.ir_plot = new IRPlot(hierarchical_melody, [hierarchy, melody_color, audio, window]);
+    this.chord_gravities = new GravityHierarchy("chord_gravity", hierarchical_melody, [gravity.chord_checkbox, hierarchy, audio, window]);
+    this.scale_gravities = new GravityHierarchy("scale_gravity", hierarchical_melody, [gravity.scale_checkbox, hierarchy, audio, window]);
+    this.time_span_tree = new ReductionHierarchy(hierarchical_melody, [hierarchy, melody_color, audio, window]);
     this.children = [
       this.d_melody_collection,
       this.melody_hierarchy,
