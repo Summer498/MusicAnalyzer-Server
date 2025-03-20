dcb13649f481dcf6493495efa53ee64f50f68c16
Summer498 <subore02@gmail.com>
2025-03-20 12:15:55 +0900

    delegate controller into children

--- packages/UI/piano-roll/melody-view/src/reduction/reduction-layer.ts
+++ packages/UI/piano-roll/melody-view/src/reduction/reduction-layer.ts
@@ -1,21 +1,16 @@
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { CollectionLayer } from "@music-analyzer/view";
 import { Reduction } from "./reduction";
-import { ColorChangeSubscriber, hasArchetype, MelodyColorController } from "@music-analyzer/controllers";
+import { MelodyColorController } from "@music-analyzer/controllers";
 
 export class ReductionLayer
-  extends CollectionLayer<Reduction>
-  implements
-  ColorChangeSubscriber {
+  extends CollectionLayer<Reduction> {
   constructor(
     melody: TimeAndAnalyzedMelody[],
     layer: number,
     controllers: [MelodyColorController]
   ) {
-    super(layer, melody.map(e => new Reduction(e, layer)));
-    controllers[0].register(this);
+    super(layer, melody.map(e => new Reduction(e, layer, controllers)));
   }
-  setColor(getColor: (e: hasArchetype) => string) { this.children.forEach(e => e.setColor(getColor)); }
-  updateColor() { this.children.forEach(e => e.updateColor()); }
   renewStrong(layer: number) { this.children.forEach(e => e.renewStrong(layer === this.layer)); }
 }
--- packages/UI/piano-roll/melody-view/src/reduction/reduction/reduction.ts
+++ packages/UI/piano-roll/melody-view/src/reduction/reduction/reduction.ts
@@ -2,16 +2,20 @@ import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { MVVM_ViewModel } from "@music-analyzer/view";
 import { ReductionModel } from "./reduction-model";
 import { ReductionView } from "../reduction-view";
-import { hasArchetype } from "@music-analyzer/controllers";
+import { ColorChangeSubscriber, hasArchetype, MelodyColorController } from "@music-analyzer/controllers";
 
 export class Reduction
-  extends MVVM_ViewModel<ReductionModel, ReductionView> {
+  extends MVVM_ViewModel<ReductionModel, ReductionView>
+  implements
+  ColorChangeSubscriber {
   constructor(
     melody: TimeAndAnalyzedMelody,
     layer: number,
+    controllers: [MelodyColorController]
   ) {
     const model = new ReductionModel(melody, layer);
     super(model, new ReductionView(model));
+    controllers[0].register(this);
   }
   renewStrong(strong: boolean) { this.view.strong = strong; }
   setColor(getColor: (e: hasArchetype) => string) { this.view.setColor(getColor); }
