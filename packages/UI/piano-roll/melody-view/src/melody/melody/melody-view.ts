1eaaa46a8214e89cad5f5aecd572b3b0e044b29d
Summer498 <subore02@gmail.com>
2025-03-21 05:56:30 +0900

    remove has archetype

--- packages/UI/piano-roll/melody-view/src/melody/melody/melody-view.ts
+++ packages/UI/piano-roll/melody-view/src/melody/melody/melody-view.ts
@@ -1,5 +1,5 @@
-import { ColorChangeSubscriber, hasArchetype, MelodyColorController } from "@music-analyzer/controllers";
-import { get_color_of_Narmour_concept } from "@music-analyzer/irm";
+import { ColorChangeSubscriber, MelodyColorController } from "@music-analyzer/controllers";
+import { get_color_of_Narmour_concept, Triad } from "@music-analyzer/irm";
 import { MVVM_View } from "@music-analyzer/view";
 import { deleteMelody } from "../../melody-editor-function";
 import { MelodyModel } from "./melody-model";
@@ -8,7 +8,7 @@ export class MelodyView
   extends MVVM_View<MelodyModel, "rect">
   implements
   ColorChangeSubscriber {
-  #getColor: (e: hasArchetype) => string;
+  #getColor: (e: Triad) => string;
   constructor(
     model: MelodyModel,
     controllers: [MelodyColorController]
@@ -18,16 +18,16 @@ export class MelodyView
     this.svg.style.stroke = "rgb(64, 64, 64)";
     this.svg.onclick = deleteMelody;
     this.svg.style.fill = "rgb(0, 192, 0)";
-    this.#getColor = e => get_color_of_Narmour_concept(e.archetype);
+    this.#getColor = get_color_of_Narmour_concept;
     controllers[0].register(this);
   }
-  setColor(getColor: (e: hasArchetype) => string) {
+  setColor(getColor: (e: Triad) => string) {
     this.#getColor = getColor;
-    this.svg.style.fill = this.#getColor(this.model) || "rgb(0, 0, 0)";
+    this.svg.style.fill = this.#getColor(this.model.archetype) || "rgb(0, 0, 0)";
     this.svg.style.fill = "rgb(0, 192, 0)";
   }
   updateColor() {
-    this.#getColor(this.model) || "rgb(0, 0, 0)";
+    this.#getColor(this.model.archetype) || "rgb(0, 0, 0)";
   }
   updateX(x:number) { this.svg.setAttribute("x", String(x)); }
   updateY(y:number) { this.svg.setAttribute("y", String(y)); }
