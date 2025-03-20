6a6a20c214d32c69c1ff0941352b35230303224a
Summer498 <subore02@gmail.com>
2025-03-20 21:03:14 +0900

    remove reflectable (delegate to children)

--- packages/application/music-analyzer-application/src/setup-ui/analysis-view.ts
+++ packages/application/music-analyzer-application/src/setup-ui/analysis-view.ts
@@ -1,15 +1,12 @@
 import { AudioReflectable, AudioReflectableRegistry, WindowReflectable, WindowReflectableRegistry } from "@music-analyzer/view";
 import { MusicStructureElements } from "../piano-roll";
 
-export class AnalysisView
-  implements AudioReflectable, WindowReflectable {
+export class AnalysisView {
   readonly svg: SVGGElement;
-  readonly children: (AudioReflectable & WindowReflectable)[];
   constructor(
     analysis: MusicStructureElements,
   ) {
     const { beat, chord, melody } = analysis;
-    this.children = [beat, chord, melody];
     this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
     // this.svg.appendChild(beat.beat_bars.svg);
     this.svg.appendChild(chord.chord_notes.svg);
@@ -23,6 +20,4 @@ export class AnalysisView
     this.svg.appendChild(melody.scale_gravities.svg);
     this.svg.appendChild(melody.time_span_tree.svg);
   }
-  onAudioUpdate() { this.children.forEach(e => e.onAudioUpdate()); }
-  onWindowResized() { this.children.forEach(e => e.onWindowResized()); }
 }
