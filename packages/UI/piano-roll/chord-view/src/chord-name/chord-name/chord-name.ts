7a7eb27f8cc6e16f804a18547e6320fc1ffbaacd
Summer498 <subore02@gmail.com>
2025-03-21 06:29:52 +0900

    implement TimeRangeSubscriber

--- packages/UI/piano-roll/chord-view/src/chord-name/chord-name/chord-name.ts
+++ packages/UI/piano-roll/chord-view/src/chord-name/chord-name/chord-name.ts
@@ -5,11 +5,13 @@ import { ChordNameView } from "./chord-name-view";
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
 import { NoteSize, PianoRollHeight } from "@music-analyzer/view-parameters";
 import { chord_text_size } from "../../chord-view-params";
+import { TimeRangeSubscriber } from "@music-analyzer/controllers";
 
 const scaled = (e: number) => e * NoteSize.get();
 
-export class ChordName 
-  extends MVVM_ViewModel<ChordNameModel, ChordNameView> {
+export class ChordName
+  extends MVVM_ViewModel<ChordNameModel, ChordNameView>
+  implements TimeRangeSubscriber {
   #y: number;
   constructor(
     e: TimeAndRomanAnalysis,
@@ -20,11 +22,12 @@ export class ChordName
     this.#y = PianoRollHeight.get() + chord_text_size;
     this.updateX();
     this.updateY();
-    publisher.forEach(e=>e.register(this));
+    publisher.forEach(e => e.register(this));
   }
   updateX() { this.view.updateX(scaled(this.model.time.begin)) }
   updateY() { this.view.updateY(this.#y) }
   onWindowResized() {
     this.updateX();
   }
+  onTimeRangeChanged = this.onWindowResized
 }
