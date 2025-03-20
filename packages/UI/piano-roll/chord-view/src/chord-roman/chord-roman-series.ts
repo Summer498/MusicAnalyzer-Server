5577297f35df807973f0915d4ff7a3c1327a3390
Summer498 <subore02@gmail.com>
2025-03-20 09:55:29 +0900

    rename class

--- packages/UI/piano-roll/chord-view/src/chord-roman/chord-roman-series.ts
+++ packages/UI/piano-roll/chord-view/src/chord-roman/chord-roman-series.ts
@@ -1,10 +1,10 @@
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
 import { ReflectableTimeAndMVCControllerCollection } from "@music-analyzer/view";
-import { ChordRomanVM } from "./chord-roman-view-model";
+import { ChordRoman } from "./chord-roman-view-model";
 
 export class ChordRomanSeries 
-  extends ReflectableTimeAndMVCControllerCollection<ChordRomanVM> {
+  extends ReflectableTimeAndMVCControllerCollection<ChordRoman> {
   constructor(romans: TimeAndRomanAnalysis[]) {
-    super("roman-names", romans.map(e => new ChordRomanVM(e)));
+    super("roman-names", romans.map(e => new ChordRoman(e)));
   }
 } 
