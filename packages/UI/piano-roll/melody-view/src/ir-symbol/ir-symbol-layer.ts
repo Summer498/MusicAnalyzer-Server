5684b159e4d59c88c0566a885954416a6f050900
Summer498 <subore02@gmail.com>
2025-03-20 11:57:58 +0900

    delegate controller into children

--- packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-layer.ts
+++ packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-layer.ts
@@ -1,22 +1,15 @@
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { CollectionLayer } from "@music-analyzer/view";
 import { IRSymbol } from "./ir-symbol";
-import { ColorChangeSubscriber, hasArchetype, MelodyColorController } from "@music-analyzer/controllers";
+import { MelodyColorController } from "@music-analyzer/controllers";
 
 export class IRSymbolLayer
-  extends CollectionLayer<IRSymbol>
-  implements
-  ColorChangeSubscriber {
+  extends CollectionLayer<IRSymbol> {
   constructor(
     melodies: TimeAndAnalyzedMelody[],
     layer: number,
-    controllers: [
-      MelodyColorController,
-    ]
+    controllers: [MelodyColorController]
   ) {
-    super(layer, melodies.map(e => new IRSymbol(e, layer)));
-    controllers[0].register(this)
+    super(layer, melodies.map(e => new IRSymbol(e, layer, controllers)));
   }
-  setColor(getColor: (e: hasArchetype) => string) { this.children.forEach(e => e.setColor(getColor)); }
-  updateColor() { this.children.forEach(e => e.updateColor()); }
 }
--- packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol/ir-symbol.ts
+++ packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol/ir-symbol.ts
@@ -3,24 +3,28 @@ import { MVVM_ViewModel } from "@music-analyzer/view";
 import { IRSymbolModel } from "./ir-symbol-model";
 import { IRSymbolView } from "./ir-symbol-view";
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
-import { hasArchetype } from "@music-analyzer/controllers";
+import { ColorChangeSubscriber, hasArchetype, MelodyColorController } from "@music-analyzer/controllers";
 
 const transposed = (e: number) => e - PianoRollBegin.get()
 const scaled = (e: number) => e * NoteSize.get();
 const convertToCoordinate = (e: number) => e * BlackKeyPrm.height;
 
 export class IRSymbol 
-  extends MVVM_ViewModel<IRSymbolModel, IRSymbolView> {
+  extends MVVM_ViewModel<IRSymbolModel, IRSymbolView>
+  implements
+  ColorChangeSubscriber {
   #y: number;
   constructor(
     melody: TimeAndAnalyzedMelody,
     layer: number,
+    controllers: [MelodyColorController]
   ) {
     const model = new IRSymbolModel(melody, layer);
     super(model, new IRSymbolView(model));
     this.#y = isNaN(this.model.note) ? -99 : -convertToCoordinate(transposed(this.model.note));
     this.updateX();
     this.updateY();
+    controllers[0].register(this)
   }
   setColor(getColor: (e: hasArchetype) => string) {
     this.view.setColor(getColor);
