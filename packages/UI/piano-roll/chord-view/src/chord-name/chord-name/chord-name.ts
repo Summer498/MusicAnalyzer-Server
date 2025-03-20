834f9a37ada16304543f2f2f031ef3bcb41b0926
Summer498 <subore02@gmail.com>
2025-03-20 21:52:03 +0900

    remove unused import

--- packages/UI/piano-roll/chord-view/src/chord-name/chord-name/chord-name.ts
+++ packages/UI/piano-roll/chord-view/src/chord-name/chord-name/chord-name.ts
@@ -1,5 +1,5 @@
 import { _Chord } from "@music-analyzer/tonal-objects";
-import { AudioReflectableRegistry, MVVM_ViewModel, WindowReflectableRegistry } from "@music-analyzer/view";
+import { MVVM_ViewModel, WindowReflectableRegistry } from "@music-analyzer/view";
 import { ChordNameModel } from "./chord-name-model";
 import { ChordNameView } from "./chord-name-view";
 import { TimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
