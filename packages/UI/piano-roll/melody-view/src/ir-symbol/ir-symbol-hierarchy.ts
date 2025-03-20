0bf7dd635a2161fd278321598e4d043d9b0358e8
Summer498 <subore02@gmail.com>
2025-03-20 13:37:46 +0900

    append registry in argument

--- packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-hierarchy.ts
+++ packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-hierarchy.ts
@@ -1,13 +1,13 @@
-import { HierarchyLevelController, HierarchyLevelSubscriber, MelodyColorController } from "@music-analyzer/controllers";
+import { HierarchyLevelController, MelodyColorController } from "@music-analyzer/controllers";
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
-import { CollectionHierarchy } from "@music-analyzer/view";
+import { AudioReflectableRegistry, CollectionHierarchy, WindowReflectableRegistry } from "@music-analyzer/view";
 import { IRSymbolLayer } from "./ir-symbol-layer";
 
 export class IRSymbolHierarchy
   extends CollectionHierarchy<IRSymbolLayer> {
   constructor(
     hierarchical_melodies: TimeAndAnalyzedMelody[][],
-    controllers: [HierarchyLevelController, MelodyColorController]
+    controllers: [HierarchyLevelController, MelodyColorController, AudioReflectableRegistry, WindowReflectableRegistry]
   ) {
     super("implication-realization archetype", hierarchical_melodies.map((e, l) => new IRSymbolLayer(e, l, [controllers[1]])));
     controllers[0].register(this);
