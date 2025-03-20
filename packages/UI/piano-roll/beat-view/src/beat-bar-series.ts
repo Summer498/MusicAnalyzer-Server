ee05636de7489b46eaedb403cf9a68ea3bfdb75f
Summer498 <subore02@gmail.com>
2025-03-20 21:00:31 +0900

    rename argument

--- packages/UI/piano-roll/beat-view/src/beat-bar-series.ts
+++ packages/UI/piano-roll/beat-view/src/beat-bar-series.ts
@@ -8,11 +8,11 @@ export class BeatBarsSeries
   constructor(
     beat_info: BeatInfo,
     melodies: TimeAndAnalyzedMelody[],
-    publisher: [AudioReflectableRegistry, WindowReflectableRegistry]
+    controllers: [AudioReflectableRegistry, WindowReflectableRegistry]
   ) {
     const N = Math.ceil(beat_info.tempo * melodies[melodies.length - 1].time.end) + beat_info.phase;
     const seed = [...Array(N)];
     super("beat-bars", seed.map((_, i) => new BeatBar(beat_info, i)));
-    publisher.forEach(e=>e.register(this))
+    controllers.forEach(e=>e.register(this))
   }
 }
