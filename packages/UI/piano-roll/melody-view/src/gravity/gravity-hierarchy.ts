7dfa58f0fd031d1cc0faa0d964a8fd8d817ac497
Summer498 <subore02@gmail.com>
2025-03-20 12:05:59 +0900

    remove unnecessary implement

--- packages/UI/piano-roll/melody-view/src/gravity/gravity-hierarchy.ts
+++ packages/UI/piano-roll/melody-view/src/gravity/gravity-hierarchy.ts
@@ -1,14 +1,13 @@
 import { CollectionHierarchy } from "@music-analyzer/view";
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { GravityLayer } from "./gravity-layer";
-import { GravitySwitcherSubscriber, HierarchyLevelController, HierarchyLevelSubscriber } from "@music-analyzer/controllers";
+import { GravitySwitcherSubscriber, HierarchyLevelController } from "@music-analyzer/controllers";
 import { GravitySwitcher } from "@music-analyzer/controllers/src/switcher/gravity-switcher";
 
 export class GravityHierarchy
   extends CollectionHierarchy<GravityLayer>
   implements
-  GravitySwitcherSubscriber,
-  HierarchyLevelSubscriber {
+  GravitySwitcherSubscriber {
   constructor(
     mode: "chord_gravity" | "scale_gravity",
     hierarchical_melodies: TimeAndAnalyzedMelody[][],
