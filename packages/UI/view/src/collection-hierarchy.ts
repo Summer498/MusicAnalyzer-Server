f6b3e13b73038d5560332098de09fb3596ae272b
Summer498 <subore02@gmail.com>
2025-03-20 21:26:38 +0900

    remove reflectable

--- packages/UI/view/src/collection-hierarchy.ts
+++ packages/UI/view/src/collection-hierarchy.ts
@@ -1,15 +1,12 @@
 import { HierarchyLevelSubscriber } from "@music-analyzer/controllers"
 import { I_CollectionLayer } from "./collection-layer";
 import { I_MVVM_Collection, MVVM_Collection } from "./mvc";
-import { AudioReflectable, WindowReflectable } from "./reflectable";
 
 export abstract class CollectionHierarchy<L extends I_CollectionLayer>
   extends MVVM_Collection<L>
   implements
   I_MVVM_Collection,
-  HierarchyLevelSubscriber,
-  AudioReflectable,
-  WindowReflectable
+  HierarchyLevelSubscriber
   {
   protected _show: L[];
   get show() { return this._show; }
@@ -26,6 +23,4 @@ export abstract class CollectionHierarchy<L extends I_CollectionLayer>
     const visible_layer = this.children.filter(e => value === e.layer);
     this.setShow(visible_layer);
   }
-  onAudioUpdate() { this.children.forEach(e => e.onAudioUpdate()); }
-  onWindowResized() { this.children.forEach(e => e.onWindowResized()); }
 }
