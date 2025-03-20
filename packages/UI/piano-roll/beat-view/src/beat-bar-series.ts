c048146e9f8b89064ffb67bfde904ce7011a44b0
Summer498 <subore02@gmail.com>
2025-03-20 09:54:03 +0900

    rename file

---
+++ packages/UI/piano-roll/beat-view/src/beat-bar-series.ts
@@ -0,0 +1,16 @@
+import { ReflectableTimeAndMVCControllerCollection } from "@music-analyzer/view";
+import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
+import { BeatInfo } from "@music-analyzer/beat-estimation";
+import { BeatBar } from "./beat-bar";
+
+export class BeatBarsSeries 
+  extends ReflectableTimeAndMVCControllerCollection<BeatBar> {
+  constructor(
+    beat_info: BeatInfo,
+    melodies: TimeAndAnalyzedMelody[]
+  ) {
+    const N = Math.ceil(beat_info.tempo * melodies[melodies.length - 1].time.end) + beat_info.phase;
+    const seed = [...Array(N)];
+    super("beat-bars", seed.map((_, i) => new BeatBar(beat_info, i)));
+  }
+}
--- packages/UI/piano-roll/beat-view/src/beat-bar.ts
+++ packages/UI/piano-roll/beat-view/src/beat-bar.ts
@@ -1,16 +1,58 @@
-import { ReflectableTimeAndMVCControllerCollection } from "@music-analyzer/view";
-import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
+import { MVVM_ViewModel } from "@music-analyzer/view";
+import { play } from "@music-analyzer/synth";
+import { NoteSize, NowAt, PianoRollHeight, reservation_range } from "@music-analyzer/view-parameters";
+import { BeatBarModel } from "./beat-bar-model";
+import { BeatBarView } from "./beat-bar-view";
 import { BeatInfo } from "@music-analyzer/beat-estimation";
-import { BeatBar } from "./beat-bar-view-model";
+import { Time } from "@music-analyzer/time-and";
 
-export class BeatBarsSeries 
-  extends ReflectableTimeAndMVCControllerCollection<BeatBar> {
-  constructor(
-    beat_info: BeatInfo,
-    melodies: TimeAndAnalyzedMelody[]
-  ) {
-    const N = Math.ceil(beat_info.tempo * melodies[melodies.length - 1].time.end) + beat_info.phase;
-    const seed = [...Array(N)];
-    super("beat-bars", seed.map((_, i) => new BeatBar(beat_info, i)));
+const scaled = (e: number) => e * NoteSize.get();
+
+export class BeatBar 
+  extends MVVM_ViewModel<BeatBarModel, BeatBarView> {
+  #y1: number;
+  #y2: number;
+  sound_reserved: boolean;
+  constructor(beat_info: BeatInfo, i: number) {
+    const model = new BeatBarModel(beat_info, i);
+    super(model, new BeatBarView(model));
+    this.sound_reserved = false;
+    this.#y1 = 0;
+    this.#y2 = PianoRollHeight.get();
+    this.updateX();
+    this.updateY();
+  }
+  updateX() {
+    this.view.updateX(
+      scaled(this.model.time.begin),
+      scaled(this.model.time.begin),
+    )
+  }
+  updateY() {
+    this.view.updateY(
+      this.#y1,
+      this.#y2,
+    )
+  }
+  onWindowResized() {
+    this.updateX();
+  }
+
+  beepBeat() {
+    const model_is_in_range = new Time(0, reservation_range)
+      .map(e => e + NowAt.get())
+      .has(this.model.time.begin);
+    if (model_is_in_range) {
+      if (this.sound_reserved === false) {
+        play([220], this.model.time.begin - NowAt.get(), 0.125);
+        this.sound_reserved = true;
+        setTimeout(() => { this.sound_reserved = false; }, reservation_range * 1000);
+      }
+    }
+  }
+  onAudioUpdate() {
+    // NOTE: うるさいので停止中
+    0 && this.beepBeat();
   }
 }
+
