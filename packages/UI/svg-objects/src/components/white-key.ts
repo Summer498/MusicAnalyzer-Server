5d9f3b3adb22e136e076ace496093062871d2e2a
Summer498 <subore02@gmail.com>
2025-03-20 21:33:48 +0900

    delegate registry into children

--- packages/UI/svg-objects/src/components/white-key.ts
+++ packages/UI/svg-objects/src/components/white-key.ts
@@ -1,6 +1,6 @@
 import { BlackKeyPrm, octave_height, PianoRollBegin, WhiteKeyPrm } from "@music-analyzer/view-parameters";
 import { mod } from "@music-analyzer/math";
-import { WindowReflectable } from "@music-analyzer/view";
+import { WindowReflectable, WindowReflectableRegistry } from "@music-analyzer/view";
 
 const transposed = (e: number) => e - PianoRollBegin.get()
 const convertToCoordinate = (e: number) => e * BlackKeyPrm.height;
@@ -13,7 +13,8 @@ export class WhiteKeySVG
   readonly height: number;
   constructor(
     readonly oct: number,
-    white_index: number
+    white_index: number,
+    publisher: WindowReflectableRegistry
   ) {
     this.svg = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     this.svg.id = "white-key";
@@ -22,6 +23,7 @@ export class WhiteKeySVG
     this.y = octave_height * oct + mod(WhiteKeyPrm.height * [0, 1, 2, 3, 4, 5, 6][white_index] - convertToCoordinate(transposed(-1)), octave_height);
     this.width = WhiteKeyPrm.width;
     this.height = WhiteKeyPrm.height;
+    publisher.register(this);
   }
   onWindowResized() {
     this.svg.setAttribute("x", String(0));
--- packages/UI/svg-objects/src/octave/octave-white-key.ts
+++ packages/UI/svg-objects/src/octave/octave-white-key.ts
@@ -12,9 +12,8 @@ export class OctaveWhiteKey
     this.svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
     this.svg.id = `octave-white-keys-${oct}`;
     const white_seed = [...Array(7)];
-    this.children = white_seed.map((_, white_index) => new WhiteKeySVG(oct, white_index));
+    this.children = white_seed.map((_, white_index) => new WhiteKeySVG(oct, white_index, publisher));
     this.children.map(e => this.svg.appendChild(e.svg));
-    publisher.register(this);
   }
   onWindowResized() { this.children.forEach(e => e.onWindowResized()); }
 }
