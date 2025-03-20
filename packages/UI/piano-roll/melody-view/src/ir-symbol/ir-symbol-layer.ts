6fa0f5ac4995266d8a8b1705c97b3b6027d3393f
Summer498 <subore02@gmail.com>
2025-03-20 11:30:02 +0900

    use hasArchetype

--- packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-layer.ts
+++ packages/UI/piano-roll/melody-view/src/ir-symbol/ir-symbol-layer.ts
@@ -1,12 +1,13 @@
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { CollectionLayer } from "@music-analyzer/view";
-import { IRSymbolModel, IRSymbol } from "./ir-symbol";
+import { IRSymbol } from "./ir-symbol";
+import { hasArchetype } from "@music-analyzer/controllers";
 
 export class IRSymbolLayer
   extends CollectionLayer<IRSymbol> {
   constructor(melodies: TimeAndAnalyzedMelody[], layer: number) {
     super(layer, melodies.map(e => new IRSymbol(e, layer)));
   }
-  setColor(getColor: (e: IRSymbolModel) => string) { this.children.forEach(e => e.setColor(getColor)); }
+  setColor(getColor: (e: hasArchetype) => string) { this.children.forEach(e => e.setColor(getColor)); }
   updateColor() { this.children.forEach(e => e.updateColor()); }
 }
