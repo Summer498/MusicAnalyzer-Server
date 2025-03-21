172d1c604ca3bdad170e70abe6a0c98265ac4e93
Summer498 <subore02@gmail.com>
2025-03-21 21:06:04 +0900

    purge code chunk into class

--- packages/UI/piano-roll/melody-view/src/ir-plot/ir-plot-hierarchy/ir-plot-hierarchy.ts
+++ packages/UI/piano-roll/melody-view/src/ir-plot/ir-plot-hierarchy/ir-plot-hierarchy.ts
@@ -2,6 +2,7 @@ import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { IRPlotLayer, RequiredByIRPlotLayer } from "../ir-plot-layer";
 import { IRPlotHierarchyView } from "./ir-plot-hierarchy-view"
 import { HierarchyLevelController } from "@music-analyzer/controllers";
+import { IRPlotHierarchyModel } from "./ir-plot-hierarchy-model";
 
 export interface RequiredByIRPlotHierarchy
   extends RequiredByIRPlotLayer {
@@ -9,8 +10,7 @@ export interface RequiredByIRPlotHierarchy
 }
 export class IRPlotHierarchy {
   readonly view: IRPlotHierarchyView
-  readonly width: number;
-  readonly height: number;
+  readonly model: IRPlotHierarchyModel
   #visible_layer: number;
   readonly children: IRPlotLayer[];
   get show() { return this.view.circles.show }
@@ -19,14 +19,11 @@ export class IRPlotHierarchy {
     controllers: RequiredByIRPlotHierarchy,
   ) {
     const N = hierarchical_melody.length;
-    this.children = hierarchical_melody.map((e, l) => new IRPlotLayer(e, l, N, controllers));
-    const w = Math.max(...this.children.map(e => e.w));
-    const h = Math.max(...this.children.map(e => e.h));
-    this.width = w;
-    this.height = h;
-
     this.#visible_layer = N;
-    this.view = new IRPlotHierarchyView(w, h)
+
+    this.children = hierarchical_melody.map((e, l) => new IRPlotLayer(e, l, N, controllers));
+    this.model = new IRPlotHierarchyModel(this.children);
+    this.view = new IRPlotHierarchyView(this.model.width, this.model.height)
     controllers.hierarchy.register(this);
   }
   updateLayer() {
