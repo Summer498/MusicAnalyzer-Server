6302daea0054e43f784a2e8ebc80e1fafff20470
Summer498 <subore02@gmail.com>
2025-03-22 08:22:44 +0900

    purge function into method chain

--- packages/cli/post-f0-util/src/get-frequency.ts
+++ packages/cli/post-f0-util/src/get-frequency.ts
@@ -1,11 +1,11 @@
-import { getRange } from "@music-analyzer/math";
-
 // DATA_SAMPLING_RATE 刻みのデータをサンプリング周波数に合わせる
 export const getFrequency = (
   freq: number[],
-  N:number
+  N: number
 ) => {
-  const size = Math.floor(freq.length * N);
-  const frequency = getRange(0, size).map(i => 2 * 2 * Math.PI * freq[Math.floor(i / N)]);
-  return frequency;
+  return new Array(Math.floor(freq.length * N))
+    .map((e, i) => i / N)
+    .map(e => Math.floor(e))
+    .map(e => freq[e])
+    .map(e => 2 * 2 * Math.PI * e);
 };
