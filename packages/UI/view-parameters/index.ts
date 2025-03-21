a5461a62464f5cc5921636baf0c0aed51787ef82
Summer498 <subore02@gmail.com>
2025-03-21 20:42:58 +0900

    aggregate src export

--- packages/UI/view-parameters/index.ts
+++ packages/UI/view-parameters/index.ts
@@ -1,12 +1,8 @@
-export { CurrentTimeRatio } from "./src/current-time-ratio";
-export { CurrentTimeX } from "./src/current-time-x";
-export { NoteSize } from "./src/note-size";
-export { NowAt } from "./src/now-at";
-export { PianoRollTranslateX } from "./src/piano-roll-translate-x";
-export { OctaveCount } from "./src/octave-count";
-export { BlackPosition, WhitePosition } from "./src/position";
-export { RectParameters } from "./src/rect-parameter";
-export { SongLength } from "./src/song-length";
-export { PianoRollBegin, PianoRollEnd, PianoRollHeight, PianoRollRatio, PianoRollTimeLength, PianoRollWidth, BlackBGsPrm, BlackKeyPrm, octave_height, size, WhiteBGsPrm, WhiteKeyPrm } from "./src/piano-roll";
+export {
+  CurrentTimeRatio, CurrentTimeX, NoteSize, NowAt, PianoRollTranslateX,
+  OctaveCount, BlackPosition, WhitePosition, RectParameters, SongLength,
+  PianoRollBegin, PianoRollEnd, PianoRollHeight, PianoRollRatio,
+  PianoRollTimeLength, PianoRollWidth, BlackBGsPrm, BlackKeyPrm, octave_height, size, WhiteBGsPrm, WhiteKeyPrm
+} from "./src";
 export const bracket_height = 2;
 export const reservation_range = 1 / 15;  // play range [second]
---
+++ packages/UI/view-parameters/src/index.ts
@@ -0,0 +1,12 @@
+export { CurrentTimeRatio } from "./current-time-ratio";
+export { CurrentTimeX } from "./current-time-x";
+export { NoteSize } from "./note-size";
+export { NowAt } from "./now-at";
+export { PianoRollTranslateX } from "./piano-roll-translate-x";
+export { OctaveCount } from "./octave-count";
+export { BlackPosition, WhitePosition } from "./position";
+export { RectParameters } from "./rect-parameter";
+export { SongLength } from "./song-length";
+export { PianoRollBegin, PianoRollEnd, PianoRollHeight, PianoRollRatio, PianoRollTimeLength, PianoRollWidth, BlackBGsPrm, BlackKeyPrm, octave_height, size, WhiteBGsPrm, WhiteKeyPrm } from "./piano-roll";
+export const bracket_height = 2;
+export const reservation_range = 1 / 15;  // play range [second]
