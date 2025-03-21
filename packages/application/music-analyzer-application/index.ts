8498750ca113d44b6414e245f4a214f13d9276ce
Summer498 <subore02@gmail.com>
2025-03-21 20:18:07 +0900

    aggregate src export

--- packages/application/music-analyzer-application/index.ts
+++ packages/application/music-analyzer-application/index.ts
@@ -1,4 +1,7 @@
-export { ApplicationManager } from "./src/application-manager";
-export { setupApplication } from "./src/boot";
-export { HTMLsContainer, URLsContainer } from "./src/containers";
-export { handleGttmExampleIDs } from "./src/handle-gttm-example-IDs";
+export {
+  ApplicationManager,
+  HTMLsContainer,
+  URLsContainer,
+  setupApplication,
+  handleGttmExampleIDs
+} from "./src";
