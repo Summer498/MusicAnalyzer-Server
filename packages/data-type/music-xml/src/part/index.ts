1c03fa33b8839d9c0780b5648528ea90cde75119
Summer498 <subore02@gmail.com>
2025-03-21 20:08:27 +0900

    aggregate export

--- packages/data-type/music-xml/src/part/index.ts
+++ packages/data-type/music-xml/src/part/index.ts
@@ -1,5 +1,4 @@
-export { ExtendedMeasure } from "../measure/extended-measure";
-export { Measure } from "../measure";
+export { ExtendedMeasure, Measure } from "../measure";
 export { Beam } from "./beam";
 export { BeatPos } from "./beat-pos";
 export { HasID } from "./has-id";
