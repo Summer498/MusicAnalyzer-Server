827cb30d96d3840fe42e4f0c86e73df6adef69fa
Summer498 <subore02@gmail.com>
2025-03-21 11:26:03 +0900

    compress import

--- packages/application/music-analyzer-application/src/piano-roll/beat-elements.ts
+++ packages/application/music-analyzer-application/src/piano-roll/beat-elements.ts
@@ -1,7 +1,6 @@
 import { BeatInfo } from "@music-analyzer/beat-estimation";
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
-import { BeatBarsSeries } from "@music-analyzer/beat-view";
-import { RequiredByBeatBarsSeries } from "@music-analyzer/beat-view/src/beat-bar-series";
+import { BeatBarsSeries, RequiredByBeatBarsSeries } from "@music-analyzer/beat-view";
 
 export interface RequiredByBeatElements
   extends RequiredByBeatBarsSeries { }
