157611cdd1812393754f7318906280e90bfcd76f
Summer498 <subore02@gmail.com>
2025-03-21 21:22:44 +0900

    purge code chunk into class

--- packages/UI/piano-roll/melody-view/src/ir-plot/ir-plot/ir-plot-model.ts
+++ packages/UI/piano-roll/melody-view/src/ir-plot/ir-plot/ir-plot-model.ts
@@ -1,66 +1,21 @@
 import { Triad } from "@music-analyzer/irm";
 import { TimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
 import { MVVM_Model } from "@music-analyzer/view";
-import { NowAt } from "@music-analyzer/view-parameters";
+import { MelodiesCache } from "./melodies-cache";
 
-export class IRPlotModel 
+export class IRPlotModel
   extends MVVM_Model {
-  #index: number;
-  #cache: TimeAndAnalyzedMelody[];
-  get archetype() { return this.getCurrentNote().melody_analysis.implication_realization as Triad; }
+  readonly melody: MelodiesCache
+  get archetype() { return this.melody.getCurrentNote().melody_analysis.implication_realization as Triad; }
   constructor(
-    readonly melody_series: TimeAndAnalyzedMelody[],
+    melody_series: TimeAndAnalyzedMelody[],
   ) {
     super();
-    this.#index = 0;
-    this.#cache = [];
+    this.melody = new MelodiesCache(melody_series);
   }
-  private cacheHit() {
-    return this.#cache[1]?.time.has(NowAt.get());
-  }
-  private cacheUpdate() {
-    if (this.cacheHit()) { return this.#cache; }
-    else {
-      this.#index = this.melody_series.findIndex((value) =>
-        value.time.has(NowAt.get())
-      );
-    }
-    const i = this.#index;
-    const N = this.melody_series.length;
-    const melodies = [
-      this.melody_series[Math.max(0, i - 1)],
-      this.melody_series[Math.max(0, i)],
-      this.melody_series[Math.min(i + 1, N - 1)],
-      this.melody_series[Math.min(i + 2, N - 1)],
-    ];
-    this.#cache = melodies;
-  }
-  private getCurrentIndex() {
-    this.cacheUpdate();
-    return this.#index;
-  }
-  get is_visible() {
-    const i = this.getCurrentIndex();
-    return 1 <= i && i < this.melody_series.length - 1;
-  }
-  getRangedMelody() {
-    this.cacheUpdate();
-    return this.#cache;
-  }
-  getPositionRatio() {
-    const melodies = this.getRangedMelody();
-    const t = [melodies[1].time.begin, melodies[2].time.begin];
-    return (NowAt.get() - t[0]) / (t[1] - t[0]);
-  }
-  getInterval() {
-    const melodies = this.getRangedMelody().map(e => e.note);
-    return [
-      melodies[1] - melodies[0] || 0,
-      melodies[2] - melodies[1] || 0,
-      melodies[3] - melodies[2] || 0,
-    ];
-  }
-  getCurrentNote() {
-    return this.getRangedMelody()[1];
-  }
+  get is_visible() { return this.melody.is_visible; }
+  getRangedMelody() { return this.melody.getRangedMelody() }
+  getPositionRatio() { return this.melody.getPositionRatio() }
+  getInterval() { return this.melody.getInterval() }
+  getCurrentNote() { return this.melody.getCurrentNote() }
 }
