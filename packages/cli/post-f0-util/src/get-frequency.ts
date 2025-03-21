8231227889439f0d7754dea79a20d8b1d8b2ec67
Summer498 <subore02@gmail.com>
2025-03-22 08:15:39 +0900

    format & rename constants

--- packages/cli/post-f0-util/src/get-frequency.ts
+++ packages/cli/post-f0-util/src/get-frequency.ts
@@ -1,9 +1,13 @@
 import { getRange } from "@music-analyzer/math";
 
 // DATA_SAMPLING_RATE 刻みのデータをサンプリング周波数に合わせる
-export const getFrequency = (freq_band_passed: number[], SAMPLING_RATE: number, DATA_SAMPLING_RATE:number) => {
+export const getFrequency = (
+  freq: number[],
+  SAMPLING_RATE: number,
+  DATA_SAMPLING_RATE:number
+) => {
   const N = SAMPLING_RATE / DATA_SAMPLING_RATE;
-  const size = Math.floor(freq_band_passed.length * SAMPLING_RATE / DATA_SAMPLING_RATE);
-  const frequency = getRange(0, size).map(i => 2 * 2 * Math.PI * freq_band_passed[Math.floor(i / N)]);
+  const size = Math.floor(freq.length * N);
+  const frequency = getRange(0, size).map(i => 2 * 2 * Math.PI * freq[Math.floor(i / N)]);
   return frequency;
 };
