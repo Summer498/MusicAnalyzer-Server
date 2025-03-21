95d5752290098b584d83c858aaa57a1f69f06792
Summer498 <subore02@gmail.com>
2025-03-21 20:42:18 +0900

    aggregate src export

--- packages/UI/piano-roll/chord-view/index.ts
+++ packages/UI/piano-roll/chord-view/index.ts
@@ -1,8 +1,8 @@
-export { ChordKeySeries, RequiredByChordKeySeries } from "./src/chord-key";
-export { ChordNameSeries, RequiredByChordNameSeries } from "./src/chord-name";
-export { ChordRomanSeries, RequiredByChordRomanSeries } from "./src/chord-roman";
-export { ChordNotesSeries, RequiredByChordNotesSeries } from "./src/chord-note";
-export { ChordElements, RequiredByChordElements } from "./src/chord-elements";
-
-export { chord_name_margin, chord_text_em, chord_text_size } from "./src/chord-view-params";
-
+export {
+  ChordKeySeries, RequiredByChordKeySeries,
+  ChordNameSeries, RequiredByChordNameSeries,
+  ChordRomanSeries, RequiredByChordRomanSeries ,
+  ChordNotesSeries, RequiredByChordNotesSeries,
+  ChordElements, RequiredByChordElements,
+  chord_name_margin, chord_text_em, chord_text_size,
+} from "./src";
---
+++ packages/UI/piano-roll/chord-view/src/index.ts
@@ -0,0 +1,8 @@
+export { ChordKeySeries, RequiredByChordKeySeries } from "./chord-key";
+export { ChordNameSeries, RequiredByChordNameSeries } from "./chord-name";
+export { ChordRomanSeries, RequiredByChordRomanSeries } from "./chord-roman";
+export { ChordNotesSeries, RequiredByChordNotesSeries } from "./chord-note";
+export { ChordElements, RequiredByChordElements } from "./chord-elements";
+
+export { chord_name_margin, chord_text_em, chord_text_size } from "./chord-view-params";
+
