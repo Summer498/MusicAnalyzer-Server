0a246a49a2958483a0ad328c642e0dc69e6fae7c
Summer498 <subore02@gmail.com>
2025-03-21 20:42:41 +0900

    aggregate src export

--- packages/UI/view/index.ts
+++ packages/UI/view/index.ts
@@ -1,5 +1,7 @@
-export { CollectionLayer } from "./src/collection-layer";
-export { CollectionHierarchy } from "./src/collection-hierarchy";
-export { MVVM_Collection, MVVM_Model, MVVM_View, MVVM_ViewModel } from "./src/mvc";
-export { AudioReflectable, AudioReflectableRegistry, WindowReflectable, WindowReflectableRegistry } from "./src/reflectable";
-export { ReflectableTimeAndMVCControllerCollection } from "./src/svg-collection";
+export {
+  CollectionLayer,
+  CollectionHierarchy,
+  MVVM_Collection, MVVM_Model, MVVM_View, MVVM_ViewModel,
+  AudioReflectable, AudioReflectableRegistry, WindowReflectable, WindowReflectableRegistry,
+  ReflectableTimeAndMVCControllerCollection,
+} from "./src";
---
+++ packages/UI/view/src/index.ts
@@ -0,0 +1,5 @@
+export { CollectionLayer } from "./collection-layer";
+export { CollectionHierarchy } from "./collection-hierarchy";
+export { MVVM_Collection, MVVM_Model, MVVM_View, MVVM_ViewModel } from "./mvc";
+export { AudioReflectable, AudioReflectableRegistry, WindowReflectable, WindowReflectableRegistry } from "./reflectable";
+export { ReflectableTimeAndMVCControllerCollection } from "./svg-collection";
