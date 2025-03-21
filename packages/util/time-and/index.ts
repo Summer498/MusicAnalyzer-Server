a1e4dc0be885ed7cb66556752f2848492b6c0049
Summer498 <subore02@gmail.com>
2025-03-21 20:43:09 +0900

    aggregate src export

--- packages/util/time-and/index.ts
+++ packages/util/time-and/index.ts
@@ -1,4 +1,6 @@
-export { compress } from "./src/compress";
-export { search_items_begins_in_range } from "./src/search-items-in-range";
-export { search_items_overlaps_range } from "./src/search-items-overlaps-range";
-export { Time } from "./src/time";
+export { 
+  compress,
+  search_items_begins_in_range,
+  search_items_overlaps_range,
+  Time,
+} from "./src";
---
+++ packages/util/time-and/src/index.ts
@@ -0,0 +1,4 @@
+export { compress } from "./compress";
+export { search_items_begins_in_range } from "./search-items-in-range";
+export { search_items_overlaps_range } from "./search-items-overlaps-range";
+export { Time } from "./time";
