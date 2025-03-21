843bad14b6ec3fe174d48cac672675bb7fbe7826
Summer498 <subore02@gmail.com>
2025-03-21 20:08:52 +0900

    aggregate export

--- packages/cognitive-theory-of-music/irm/src/archetype/index.ts
+++ packages/cognitive-theory-of-music/irm/src/archetype/index.ts
@@ -1,7 +1,7 @@
 export { Dyad } from "./Dyad";
 export { Monad } from "./Monad";
 export { Null_ad } from "./Null-ad";
-export { Triad } from "./triad/Triad";
+export { Triad } from "./triad";
 
 export { getArchetype } from "./archetype";
 export { ArchetypeSymbol } from "./types";
