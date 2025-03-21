20de27b43e39587f5fe00d73372515b95dee5957
Summer498 <subore02@gmail.com>
2025-03-21 20:08:14 +0900

    aggregate export

--- packages/cognitive-theory-of-music/gttm/index.ts
+++ packages/cognitive-theory-of-music/gttm/index.ts
@@ -1,3 +1,3 @@
-export { song_list } from "./sample/song-list";
+export { song_list } from "./sample";
 export { GroupingStructure, IProlongationalReduction, ITimeSpanReduction, MetricalStructure, ProlongationalReduction, ReductionElement, TimeSpan, TimeSpanReduction } from "./src/analysis-result";
 export { GTTMData } from "./src/analysis-result";
