a3aa803258003b73f96cf33895b95e2c549968a4
Summer498 <subore02@gmail.com>
2025-03-21 12:40:05 +0900

    aggregate chord estimation related class

--- packages/UI/piano-roll/chord-view/index.ts
+++ packages/UI/piano-roll/chord-view/index.ts
@@ -2,5 +2,7 @@ export { ChordKeySeries, RequiredByChordKeySeries } from "./src/chord-key";
 export { ChordNameSeries, RequiredByChordNameSeries } from "./src/chord-name";
 export { ChordRomanSeries, RequiredByChordRomanSeries } from "./src/chord-roman";
 export { ChordNotesSeries, RequiredByChordNotesSeries } from "./src/chord-note";
+export { ChordElements } from "./src/chord-elements";
 
 export { chord_name_margin, chord_text_em, chord_text_size } from "./src/chord-view-params";
+
