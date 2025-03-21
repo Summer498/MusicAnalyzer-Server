605443414c8293d01eae1df640b41b1d62d607e3
Summer498 <subore02@gmail.com>
2025-03-21 20:18:24 +0900

    aggregate src export

---
+++ packages/cognitive-theory-of-music/irm/src/index.ts
@@ -0,0 +1,11 @@
+export { Dyad, Monad, Null_ad, Triad } from "./archetype";
+export { ArchetypeSymbol } from "./archetype";
+export {
+  get_color_of_Narmour_concept,
+  get_color_of_implication_realization,
+  get_color_on_digital_parametric_scale,
+  get_color_on_digital_intervallic_scale,
+  get_color_on_registral_scale,
+  get_color_on_intervallic_angle,
+  get_color_on_parametric_scale
+} from "./colors.ts";
