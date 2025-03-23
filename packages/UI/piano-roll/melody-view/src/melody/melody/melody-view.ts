5cc5008a41108e6fe18168fb0e0e897ecb940b35
Summer498 <subore02@gmail.com>
2025-03-23 11:43:46 +0900

    use SetColor type

--- packages/UI/piano-roll/melody-view/src/melody/melody/melody-view.ts
+++ packages/UI/piano-roll/melody-view/src/melody/melody/melody-view.ts
@@ -1,8 +1,8 @@
 import { ColorChangeSubscriber, MelodyColorController } from "@music-analyzer/controllers";
-import { Triad } from "@music-analyzer/irm";
 import { MVVM_View } from "@music-analyzer/view";
 import { deleteMelody } from "../../melody-editor-function";
 import { MelodyModel } from "./melody-model";
+import { SetColor } from "@music-analyzer/controllers/src/color-selector.ts/irm-color-selector";
 
 export interface RequiredByMelodyView {
   readonly melody_color: MelodyColorController
@@ -19,7 +19,7 @@ export class MelodyView
     this.svg.style.stroke = "rgb(64, 64, 64)";
     this.svg.onclick = deleteMelody;
   }
-  setColor(getColor: (e: Triad) => string) { this.svg.style.fill = "rgb(0, 192, 0)"; }
+  readonly setColor: SetColor = getColor => this.svg.style.fill = "rgb(0, 192, 0)";
   updateX(x: number) { this.svg.setAttribute("x", String(x)); }
   updateY(y: number) { this.svg.setAttribute("y", String(y)); }
   updateWidth(w: number) { this.svg.setAttribute("width", String(w)); }
