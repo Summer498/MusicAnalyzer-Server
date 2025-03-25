2e6a1ddd7e7d3ac1025e2f5a11eecf630afa2fbb
Summer498 <subore02@gmail.com>
2025-03-26 07:56:47 +0900

    publish abstract rectangle

--- packages/UI/svg-objects/src/components/index.ts
+++ packages/UI/svg-objects/src/components/index.ts
@@ -2,3 +2,4 @@ export { BlackBG } from "./black-bg";
 export { BlackKey } from "./black-key";
 export { WhiteBG } from "./white-bg";
 export { WhiteKey } from "./white-key";
+export { Rectangle } from "./rectangle"
