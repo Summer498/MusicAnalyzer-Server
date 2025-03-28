680025e47044a37868840abeabc00a91bc589f85
Summer498 <subore02@gmail.com>
2025-03-29 06:23:18 +0900

    update dependency

--- sh/dependencyCheck.sh
+++ sh/dependencyCheck.sh
@@ -6,7 +6,6 @@ shopt -s globstar  # ** の有効化
 depCheck(){
     local -r dist="$1"
     local -r res="$(npx depcheck $(dirname $dist))"
-
     echo -e "$(dirname $dist)\r\n$res\r\n\r\n"
 }
 
@@ -23,4 +22,5 @@ depCheck ./html/package.json &
 depCheck ./html/analyze/package.json &
 depCheck ./html/hierarchical-analysis-sample/package.json &
 
+wait
 exit
