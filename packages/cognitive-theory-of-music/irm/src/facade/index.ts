522f3b9679c0cf84a929c203459a3251a6dd67c8
Summer498 <subore02@gmail.com>
2025-04-02 07:26:10 +0900

    remove facade

--- packages/cognitive-theory-of-music/irm/src/facade/index.ts
+++
@@ -1,9 +0,0 @@
-export { getNote } from "@music-analyzer/tonal-objects";
-export { getSemitones } from "@music-analyzer/tonal-objects";
-export { intervalOf } from "@music-analyzer/tonal-objects";
-export { type NoteLiteral } from "@music-analyzer/tonal-objects";
-export { type IntervalName } from "@music-analyzer/tonal-objects";
-export { type Interval } from "@music-analyzer/tonal-objects";
-export { getInterval } from "@music-analyzer/tonal-objects";
-export { hsv2rgb } from "@music-analyzer/color";
-export { rgbToString } from "@music-analyzer/color";
--- packages/music-structure/analyzed-data-container/src/facade/index.ts
+++
@@ -1,7 +0,0 @@
-export { SerializedTimeAndRomanAnalysis } from "@music-analyzer/chord-analyze";
-export { SerializedTimeAndAnalyzedMelody } from "@music-analyzer/melody-analyze";
-export { type BeatInfo } from "@music-analyzer/beat-estimation";
-export { calcTempo } from "@music-analyzer/beat-estimation";
-export { bracket_height } from "@music-analyzer/view-parameters";
-export { PianoRollBegin } from "@music-analyzer/view-parameters";
-export { PianoRollEnd } from "@music-analyzer/view-parameters";
