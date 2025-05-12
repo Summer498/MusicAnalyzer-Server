7e4a4570c9ebd0ecacc5005807daf7996f778eb7
Summer498 <subore02@gmail.com>
2025-05-12 09:03:58 +0900

    bugfix: type error

--- packages/UI/view/src/reflectable-time-and-mvc-controller-collection/time-and-mvvm.ts
+++ packages/UI/view/src/reflectable-time-and-mvc-controller-collection/time-and-mvvm.ts
@@ -6,6 +6,6 @@ export abstract class TimeAndVM<
   M extends TimeAndMVCModel,
   K extends keyof SVGElementTagNameMap
 >
-  extends MVVM_ViewModel_Impl<M, MVVM_View_Impl<K, M>> {
+  extends MVVM_ViewModel_Impl<M, MVVM_View_Impl<K>> {
   abstract readonly model: M;
 }
