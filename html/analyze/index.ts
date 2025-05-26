a4bb706d694b9320f284559948e35d1cadd20514
summer498 <subore02@gmail.com>
2025-05-26 09:23:12 +0900

    use absolute url

--- html/analyze/index.ts
+++ html/analyze/index.ts
@@ -623,7 +623,7 @@ const main = () => {
     urlParams.has("pr") ? "PR" : urlParams.has("tsr") ? "TSR" : "",
   )
   const resources = `/resources`;
-  const audio_src = `/MusicAnalyzer-Server/resources/silence.mp3`;
+  const audio_src = `https://summer498.github.io/MusicAnalyzer-Server/resources/silence.mp3`;
 
   updateTitle(titleHead, title);
   setAudioPlayer(title, resources, audio_src, audio_player);
