306cb134f4fc62754f06ba5abbb8c358365bf68f
summer498 <subore02@gmail.com>
2025-06-02 17:54:23 +0900

    remove class

--- packages/UI/piano-roll/melody-view/src/d-melody-series.ts
+++ packages/UI/piano-roll/melody-view/src/d-melody-series.ts
@@ -1,5 +1,4 @@
 import { AudioReflectableRegistry, PianoRollTranslateX, WindowReflectableRegistry } from "@music-analyzer/view";
-import { insertMelody } from "./melody-editor/insert";
 import { hsv2rgb } from "@music-analyzer/color";
 import { rgbToString } from "@music-analyzer/color";
 import { SerializedMelodyAnalysis } from "@music-analyzer/melody-analyze";
@@ -8,75 +7,34 @@ import { black_key_height, PianoRollConverter } from "@music-analyzer/view-param
 import { Time } from "@music-analyzer/time-and";
 import { DMelodyController, TimeRangeController } from "@music-analyzer/controllers";
 
-class DMelodyView {
-  constructor(
-    readonly svg: SVGRectElement
-  ) { }
-  set onclick(value: () => void) { this.svg.onclick = value; };
-  updateX(x: number) { this.svg.setAttribute("x", String(x)); }
-  updateY(y: number) { this.svg.setAttribute("y", String(y)); }
-  updateWidth(w: number) { this.svg.setAttribute("width", String(w)); }
-  updateHeight(h: number) { this.svg.setAttribute("height", String(h)); }
-}
-
-class DMelodyModel {
+interface IDMelodyModel {
   readonly time: Time;
   readonly head: Time;
   readonly note: number;
   readonly melody_analysis: SerializedMelodyAnalysis;
-  constructor(e: SerializedTimeAndAnalyzedMelody) {
-    this.time = e.time;
-    this.head = e.head;
-    this.note = e.note;
-    this.melody_analysis = e.melody_analysis;
-  }
 }
 
-class DMelody {
-  get svg() { return this.view.svg; }
-  constructor(
-    readonly model: DMelodyModel,
-    readonly view: DMelodyView,
-  ) {
-    this.onAudioUpdate();
-    this.updateX();
-    this.updateY();
-    this.updateWidth();
-    this.updateHeight();
-  }
-  updateX() { this.view.updateX(PianoRollConverter.scaled(this.model.time.begin)) }
-  updateY() { this.view.updateY(PianoRollConverter.midi2NNBlackCoordinate(this.model.note)); }
-  updateWidth() { this.view.updateWidth(PianoRollConverter.scaled(this.model.time.duration)) }
-  updateHeight() { this.view.updateHeight(black_key_height) }
-  onWindowResized() {
-    this.updateX();
-    this.updateWidth();
-    this.updateHeight();
-  }
-  onAudioUpdate() {
-    this.view.onclick = insertMelody;
-  }
-  onTimeRangeChanged = this.onWindowResized
-}
+const updateX_DMelodyView = (svg: SVGRectElement) => (x: number) => { svg.setAttribute("x", String(x)); }
+const updateY_DMelodyView = (svg: SVGRectElement) => (y: number) => { svg.setAttribute("y", String(y)); }
+const updateWidth_DMelodyView = (svg: SVGRectElement) => (w: number) => { svg.setAttribute("width", String(w)); }
+const updateHeight_DMelodyView = (svg: SVGRectElement) => (h: number) => { svg.setAttribute("height", String(h)); }
 
-class DMelodySeries {
-  readonly children_model: { readonly time: Time }[];
-  #show: DMelody[];
-  get show() { return this.#show; };
-
-  constructor(
-    readonly svg: SVGGElement,
-    readonly children: DMelody[],
-  ) {
-    this.children_model = this.children.map(e => e.model);
-    this.#show = children;
-  }
-  onDMelodyVisibilityChanged(visible: boolean) {
-    const visibility = visible ? "visible" : "hidden";
-    this.svg.style.visibility = visibility;
-  }
-  onAudioUpdate() { this.svg.setAttribute("transform", `translate(${PianoRollTranslateX.get()})`); }
+const updateX = (svg: SVGRectElement) => (begin: number) => { updateX_DMelodyView(svg)(PianoRollConverter.scaled(begin)) }
+const updateY = (svg: SVGRectElement) => (note: number) => { updateY_DMelodyView(svg)(PianoRollConverter.midi2NNBlackCoordinate(note)); }
+const updateWidth = (svg: SVGRectElement) => (duration: number) => { updateWidth_DMelodyView(svg)(PianoRollConverter.scaled(duration)) }
+const updateHeight = (svg: SVGRectElement) => { updateHeight_DMelodyView(svg)(black_key_height) }
+const onWindowResized = (svg: SVGRectElement) => (model: IDMelodyModel) => {
+  updateX(svg)(model.time.begin);
+  updateWidth(svg)(model.time.duration);
+  updateHeight(svg);
 }
+const onTimeRangeChanged = onWindowResized
+
+const onDMelodyVisibilityChanged = (svg: SVGGElement) => (visible: boolean) => {
+  const visibility = visible ? "visible" : "hidden";
+  svg.setAttribute("visibility", visibility);
+}
+const onAudioUpdate = (svg: SVGGElement) => { svg.setAttribute("transform", `translate(${PianoRollTranslateX.get()})`); }
 
 function getMelodyViewSVG() {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "rect")
@@ -95,28 +53,31 @@ function getSVGG(id: string, parts: { svg: SVGElement }[]) {
 
 export function buildDMelody(
   d_melody: SerializedTimeAndAnalyzedMelody[],
-    controllers: {
-      readonly audio: AudioReflectableRegistry,
-      readonly d_melody: DMelodyController,
-      readonly window: WindowReflectableRegistry,
-      readonly time_range: TimeRangeController,
-    }
+  controllers: {
+    readonly audio: AudioReflectableRegistry,
+    readonly d_melody: DMelodyController,
+    readonly window: WindowReflectableRegistry,
+    readonly time_range: TimeRangeController,
+  }
 ) {
-  const parts = d_melody.map(e => {
+  const children = d_melody.map(melody => {
     const svg = getMelodyViewSVG();
-    const model = new DMelodyModel(e);
-    const view = new DMelodyView(svg);
-    return new DMelody(model, view)
+    updateX(svg)(melody.time.begin);
+    updateY(svg)(melody.note);
+    updateWidth(svg)(melody.time.duration);
+    updateHeight(svg);
+
+    return { model: melody, svg }
   })
-  const svg = getSVGG("detected-melody", parts);
+  const svg = getSVGG("detected-melody", children);
 
-  const d_melody_collection = new DMelodySeries(svg, parts);
+  const d_melody_collection = { svg, children };
 
-  controllers.window.addListeners(...d_melody_collection.children.map(e => e.onWindowResized.bind(e)));
-  controllers.time_range.addListeners(...d_melody_collection.children.map(e => e.onTimeRangeChanged.bind(e)));
-  controllers.d_melody.addListeners(d_melody_collection.onDMelodyVisibilityChanged.bind(d_melody_collection));
-  controllers.audio.addListeners(...d_melody_collection.children.map(e => e.onAudioUpdate.bind(e)));
-  d_melody_collection.children.map(e => e.onAudioUpdate())
+  controllers.window.addListeners(...d_melody_collection.children.map(e => () => onWindowResized(e.svg)(e.model)));
+  controllers.time_range.addListeners(...d_melody_collection.children.map(e => () => onTimeRangeChanged(e.svg)(e.model)));
+  controllers.d_melody.addListeners(onDMelodyVisibilityChanged(d_melody_collection.svg));
+  controllers.audio.addListeners(() => onAudioUpdate(d_melody_collection.svg));
+  onAudioUpdate(d_melody_collection.svg)
 
   return d_melody_collection.svg;
 }
