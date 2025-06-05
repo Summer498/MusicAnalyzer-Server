6708e833b59c8c80bb426992e816fda2876e9cd6
summer <83847003+Summer498@users.noreply.github.com>
2025-06-05 16:17:46 +0900

    refactor median filter to factory

--- packages/cli/post-crepe/src/get-median-frequency.ts
+++ packages/cli/post-crepe/src/get-median-frequency.ts
@@ -1,9 +1,9 @@
 import { getRange } from "@music-analyzer/math";
-import { MedianFilter } from "./median-filter";
+import { createMedianFilter } from "./median-filter";
 
 export const getMedianFrequency = (freq_rounded: number[]) => {
   // 中央値を用いたフィルタ (ヘンペルフィルタ) を用いてスパイクノイズを除去する
   const WINDOW_SIZE = 25;
-  const median_filter = new MedianFilter(freq_rounded, WINDOW_SIZE);
+  const median_filter = createMedianFilter(freq_rounded, WINDOW_SIZE);
   return getRange(0, freq_rounded.length).map(i => median_filter.median(i));
 };
