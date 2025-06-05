5445f550304f7322829341e9835e657a0ee5cf27
summer <83847003+Summer498@users.noreply.github.com>
2025-06-05 16:31:48 +0900

    Add basic functional tests

--- packages/util/stdlib/index.test.ts
+++ packages/util/stdlib/index.test.ts
@@ -1,5 +1,17 @@
-describe("dummy", () => {
-  test("dummy", () => {
-    expect(1).toBe(1);
+import { unique, getCapitalCase, assertNonNullable } from "./index";
+
+describe("stdlib utilities", () => {
+  test("unique removes duplicates", () => {
+    expect(unique([1, 1, 2])).toEqual([1, 2]);
+  });
+
+  test("getCapitalCase capitalizes first letter", () => {
+    expect(getCapitalCase("hello")).toBe("Hello");
+  });
+
+  test("assertNonNullable throws on null or undefined", () => {
+    expect(() => assertNonNullable(null as any)).toThrow();
+    expect(() => assertNonNullable(undefined as any)).toThrow();
+    expect(assertNonNullable(0)).toBe(0);
   });
 });
