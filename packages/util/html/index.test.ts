972004f549fe577595912a51b8d29b2b6422a6f5
summer <83847003+Summer498@users.noreply.github.com>
2025-06-05 17:08:14 +0900

    Add list of failing and placeholder tests

--- packages/util/html/index.test.ts
+++ packages/util/html/index.test.ts
@@ -1,5 +1,9 @@
-describe("dummy", () => {
-  test("dummy", () => {
-    expect(1).toBe(1);
+import { HTML } from "./index";
+
+describe("html utilities", () => {
+  test("HTML.p creates paragraph element", () => {
+    const p = HTML.p({}, "hello");
+    expect(p.tagName.toLowerCase()).toBe("p");
+    expect(p.textContent).toBe("hello");
   });
 });
