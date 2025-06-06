a9ed6648931d713350f1348f0cb588488ea0345a
summer <83847003+Summer498@users.noreply.github.com>
2025-06-06 17:08:47 +0900

    Merge main into work

--- packages/UI/controllers/index.ts
+++ packages/UI/controllers/index.ts
@@ -1,10 +1,19 @@
-export { SetColor, MelodyColorController, createMelodyColorController } from "./src/color-selector";
+export { SetColor } from "./src/color-selector";
+export { MelodyColorController, createMelodyColorController } from "./src/color-selector";
 export { ControllerView } from "./src/controller";
-export { MelodyBeepController } from "./src/melody-beep-controller";
+export type { MelodyBeepVolume, MelodyBeepSwitcher, MelodyBeepController } from "./src/melody-beep-controller";
+export { createMelodyBeepVolume, createMelodyBeepSwitcher, createMelodyBeepController } from "./src/melody-beep-controller";
 export { HierarchyLevelController } from "./src/slider";
 export { TimeRangeController } from "./src/slider";
 export { Controller } from "./src/controller";
-export { DMelodyController } from "./src/switcher";
+export {
+  createCheckbox,
+  Checkbox,
+  createDMelodyController,
+  DMelodyController,
+  createGravityController,
+  GravityController,
+  createImplicationDisplayController,
+  ImplicationDisplayController,
+} from "./src/switcher"
 export { Slider } from "./src/slider";
-export { GravityController } from "./src/switcher";
-export { Checkbox } from "./src/switcher";
